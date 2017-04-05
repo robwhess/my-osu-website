f30ebd51ce9e99f2c2c000c680c8114812ac4012
Rob Hess <robwhess@gmail.com>
2017-04-05 09:23:07 -0700

    Add coming soon message to assignments.

--- src/courses/sp17/cs290/index.js
+++ src/courses/sp17/cs290/index.js
@@ -63,7 +63,12 @@ var courseDetails = {
   assignments: {
     preamble: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
     assignments: [
-
+      {
+        link : '',
+        notes: [
+          'Coming soon'
+        ]
+      }
     ]
   }
 };
