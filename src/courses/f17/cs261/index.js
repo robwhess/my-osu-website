4ea186a44484e1ce42e77b0a90f7b5c6147105ea
Rob Hess <robwhess@gmail.com>
2017-09-19 10:59:56 -0700

    Add Assignment 0 to CS 261.

--- src/courses/f17/cs261/index.js
+++ src/courses/f17/cs261/index.js
@@ -100,12 +100,11 @@ var courseDetails = {
     preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
     assignments: [
       {
-        link : '#',
-        title: 'TBA',
+        link : 'https://classroom.github.com/a/QtuA_vy_',
+        title: 'Assignment 0 - Git and GitHub',
         notesHTML: [
-          // 'Assignment due by 11:59pm, Monday 4/17/2017',
-          // 'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 4/17/2017',
-          // 'Demo due by 11:59pm on Monday, 5/1/2017'
+          'Assignment due by 11:59pm, Wednesday 9/27/2017',
+          '<b>No demo for this assignment</b>'
         ]
       }
     ]
