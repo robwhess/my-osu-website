61c0386c60d72b3dffffc565b7c2f13dd8f1edbc
Rob Hess <robwhess@gmail.com>
2017-09-27 10:36:37 -0700

    Update Xinyao's office/grading hours location and add his grading hours link.

--- src/courses/f17/cs290/taInfo.js
+++ src/courses/f17/cs290/taInfo.js
@@ -12,12 +12,12 @@ var taInfo = {
     {
       name: "Xinyao Wang",
       email: "wangxiny@oregonstate.edu",
-      officeHours: [ "Tu 4:00-6:00pm (KEC Atrium)" ],
+      officeHours: [ "Tu 4:00-6:00pm (KEC 2087)" ],
       gradingHours: [
-        "Tu 8:30am-12:30pm (KEC Atrium)",
-        "W 8:30am-12:30pm (KEC Atrium)"
+        "Tu 8:30am-12:30pm (KEC 2087)",
+        "W 8:30am-12:30pm (KEC 2087)"
       ],
-      gradingHoursPollLink: "#"
+      gradingHoursPollLink: "https://calendar.google.com/calendar/selfsched?sstoken=UUlmZ0ZnZVYtZmJXfGRlZmF1bHR8MjQ5NWVjZjFhNGQ1NjNmNjkxNmI4MzNhZTlkNDI2M2U"
     }
   ]
 };
