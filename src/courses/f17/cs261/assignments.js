43043a7bbb22cf50ef70161d67085a63e60f2707
Rob Hess <robwhess@gmail.com>
2017-09-28 16:29:54 -0700

    Add assignment 1 for CS 261.

--- src/courses/f17/cs261/assignments.js
+++ src/courses/f17/cs261/assignments.js
@@ -6,6 +6,14 @@ var assignments = [
       'Assignment due by 11:59pm, Wednesday 9/27/2017',
       '<b>No demo for this assignment</b>'
     ]
+  },
+  {
+    link : 'https://classroom.github.com/a/JSwSehjS',
+    title: 'Assignment 1 - Practicing C and pointers',
+    notesHTML: [
+      'Assignment due by 11:59pm, Monday 10/9/2017',
+      'Demo due by 11:59pm, Monday 10/23/2017'
+    ]
   }
 ];
 
