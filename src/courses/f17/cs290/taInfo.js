c489b4ed34387a225c470889eeb7cccc2f548747
Rob Hess <robwhess@gmail.com>
2017-10-03 10:38:11 -0700

    Update Sudhanshu's grading hours.

--- src/courses/f17/cs290/taInfo.js
+++ src/courses/f17/cs290/taInfo.js
@@ -5,7 +5,8 @@ var taInfo = {
       email: "pathaks@oregonstate.edu",
       officeHours: [ "M 11:00am-12:00pm (KEC 2087)" ],
       gradingHours: [
-        "M 12:00-4:00pm (KEC 2087)"
+        "M 9:00-10:00am (KEC 2087)",
+        "M 12:00-3:00pm (KEC 2087)"
       ],
       gradingHoursPollLink: "https://calendar.google.com/calendar/selfsched?sstoken=UURnZnI4UXBVNWExfGRlZmF1bHR8NzU1NGQ4NGU0Yjk3MWY0NTM0ZTk2NzNhY2M0ZWI4YWM"
     },
