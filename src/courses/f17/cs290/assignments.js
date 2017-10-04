2ae38d2f6daaf420e1b1c725968b82831defcee9
Rob Hess <robwhess@gmail.com>
2017-10-04 14:39:17 -0700

    Add CS 290 Assignment 2.

--- src/courses/f17/cs290/assignments.js
+++ src/courses/f17/cs290/assignments.js
@@ -7,6 +7,15 @@ var assignments = [
       'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/2/2017',
       'Demo due by 11:59pm on Monday, 10/16/2017'
     ]
+  },
+  {
+    link : 'https://classroom.github.com/a/Y1Yqc7UO',
+    title: 'Assignment 2 - CSS',
+    notesHTML: [
+      'Assignment due by 11:59pm, Monday 10/16/2017',
+      'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/16/2017',
+      'Demo due by 11:59pm on Monday, 10/30/2017'
+    ]
   }
 ];
 
