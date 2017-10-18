d3e9db6f75a43db2ebc3acc0599008f435b2e1db
Rob Hess <robwhess@gmail.com>
2017-10-18 14:41:47 -0700

    Add assignment 3 to CS 290.

--- src/courses/f17/cs290/assignments.js
+++ src/courses/f17/cs290/assignments.js
@@ -16,6 +16,15 @@ var assignments = [
       'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/16/2017',
       'Demo due by 11:59pm on Monday, 10/30/2017'
     ]
+  },
+  {
+    link : 'https://classroom.github.com/a/prGm3Bvv',
+    title: 'Assignment 3 - Client-side JS',
+    notesHTML: [
+      'Assignment due by 11:59pm, Monday 11/6/2017',
+      'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 11/6/2017',
+      'Demo due by 11:59pm on Monday, 11/20/2017'
+    ]
   }
 ];
 
