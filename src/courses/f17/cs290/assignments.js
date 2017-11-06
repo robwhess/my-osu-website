04092405260e3e00afe97a274f9ecafa0f450c58
Rob Hess <robwhess@gmail.com>
2017-11-06 10:15:38 -0800

    Add assignment 4 to CS 290.

--- src/courses/f17/cs290/assignments.js
+++ src/courses/f17/cs290/assignments.js
@@ -25,6 +25,15 @@ var assignments = [
       'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 11/6/2017',
       'Demo due by 11:59pm on Monday, 11/20/2017'
     ]
+  },
+  {
+    link : 'https://classroom.github.com/a/tmpIaB80',
+    title: 'Assignment 4 - Native Node.js file serving',
+    notesHTML: [
+      'Assignment due by 11:59pm, Monday 11/13/2017',
+      'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 11/13/2017',
+      '<b>No demo for this assignment</b>'
+    ]
   }
 ];
 
