de2357ccfbba4a054b76948736972b32fd46cf34
Rob Hess <robwhess@gmail.com>
2017-11-27 10:04:31 -0800

    Add final project demo signup link to CS 290.

--- src/courses/f17/cs290/finalProject.js
+++ src/courses/f17/cs290/finalProject.js
@@ -3,15 +3,15 @@ var finalProject = [
     title: 'Final Project Description',
     link: 'https://docs.google.com/document/d/1x0GulHsdlpbo1S5fTW6ipF8Hpw90EdRXOX_OIbKrzJA/edit?usp=sharing'
   },
-  // {
-  //   link : 'https://doodle.com/poll/2tcxb25td4shqac5',
-  //   title: 'Final Project Demo Signup',
-  //   notesHTML: [
-  //     'Sign up for a Final Project demo using the link above',
-  //     'Sign up for one slot per team',
-  //     'Code is due on GitHub when when you do your demo'
-  //   ]
-  // }
+  {
+    link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
+    title: 'Final Project Demo Signup',
+    notesHTML: [
+      'Sign up for a Final Project demo using the link above',
+      'Sign up for one slot per team',
+      'Code is due on GitHub when when you do your demo'
+    ]
+  }
 ];
 
 export default finalProject;
