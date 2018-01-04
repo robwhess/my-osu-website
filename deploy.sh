ee565816cbd92eac08edaa8fd3cbdac04eceb874
Rob Hess <robwhess@gmail.com>
2018-01-04 16:21:53 -0500

    Specify username in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DEPLOY_USERNAME="hessro"
 DEPLOY_HOST="flip.engr.oregonstate.edu"
-DEPLOY_DIR="public_html"
+DEPLOY_DEST_DIR="public_html"
+DEPLOY_SRC_DIR="build"
 
 STASH_SUCCEEDED=false
 if git stash --include-untracked; then
   STASH_SUCCEEDED=true
 fi
 npm run build
-rsync -avz --delete build/ ${DEPLOY_HOST}:${DEPLOY_DIR}/
+rsync -avz --delete ${DEPLOY_SRC_DIR}/ ${DEPLOY_USERNAME}@${DEPLOY_HOST}:${DEPLOY_DEST_DIR}/
 if [[ ${STASH_SUCCEEDED} == true ]]; then
   git stash pop
 fi
