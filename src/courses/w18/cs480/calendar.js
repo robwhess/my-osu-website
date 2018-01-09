c9a2089857aded7d605a1b4ed0889ffa6ed0ad7c
Rob Hess <robwhess@gmail.com>
2018-01-08 16:30:17 -0800

    Update topics and readings for CS 480.

--- src/courses/w18/cs480/calendar.js
+++ src/courses/w18/cs480/calendar.js
@@ -6,13 +6,15 @@ var calendar = [
         heading: 'Topics',
         entriesHTML: [
           'Course Intro',
-          'Overview of Compilers &ndash; <a href="https://docs.google.com/document/d/1TVQFQlMrP0RpSTYAI9P-xnM_5iA45rswqCuI0yADEfw/edit?usp=sharing">notes</a>'
+          'Overview of Compilers &ndash; <a href="https://docs.google.com/document/d/1TVQFQlMrP0RpSTYAI9P-xnM_5iA45rswqCuI0yADEfw/edit?usp=sharing">notes</a>',
+          'Scanning: Finite Automata and Regular Expressions'
         ]
       },
       {
         heading: 'Readings',
         entriesHTML: [
-          'Chapter 1 of Cooper and Torczon',
+          'Chapter 1 &ndash; Cooper and Torczon',
+          'Chapter 2 (2.1&ndash;2.3) &ndash; Cooper and Torczon',
           '<a href="https://git-scm.com/book/en/v2">Chapters 1 and 2</a> &ndash; Pro Git (optional, for those without much git/GitHub experience)'
         ]
       }
@@ -31,7 +33,7 @@ var calendar = [
       {
         heading: 'Readings',
         entriesHTML: [
-          'Chapter 2 of Cooper and Torczon',
+          'Chapter 2 &ndash; Cooper and Torczon',
         ]
       }
     ]
