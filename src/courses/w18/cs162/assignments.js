ba811e2745d2f0babea59c29d4817764bb8ca035
Rob Hess <robwhess@gmail.com>
2018-01-10 11:50:46 -0800

    Update CS 162 assignments preamble to include course style guidelines.

--- src/courses/w18/cs162/assignments.js
+++ src/courses/w18/cs162/assignments.js
@@ -1,9 +1,15 @@
-var assignments = [
-  {
-    link : '#',
-    title: 'TBD',
-    notesHTML: []
-  },
-];
+import CourseStyleGuidelines from '../../../files/162_style_guideline.pdf';
+
+var assignments = {
+  preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>" +
+    "<p>Note that all of your assignments will have a component of their grade based on your code style and commenting. Using descriptive comments and good, consistent code style is very important to ensure the code you write is readable, understandable, and maintainable. For more on why good, consistent style is important, check out the background and goals of <a href='https://google.github.io/styleguide/cppguide.html#Background'>Google's C++ style guide</a>.  Please read the <a href='" + CourseStyleGuidelines + "'>style guidelines for this course</a> and try to follow them when writing your assignments.</p>",
+  assignments: [
+    {
+      link : '#',
+      title: 'TBD',
+      notesHTML: []
+    }
+  ]
+};
 
 export default assignments;
--- src/courses/w18/cs162/index.js
+++ src/courses/w18/cs162/index.js
@@ -55,10 +55,7 @@ var courseDetails = {
 
   calendar: calendar,
 
-  assignments: {
-    preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
-    assignments: assignments
-  },
+  assignments: assignments,
 
   taInfo: taInfo,
 
