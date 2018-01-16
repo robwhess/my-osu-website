1d17f3017980e22def0405a85af29ad5a7e9d724
Rob Hess <robwhess@gmail.com>
2018-01-16 13:43:18 -0800

    Add assignment 1 to CS 492.

--- src/courses/w18/cs492/assignments.js
+++ src/courses/w18/cs492/assignments.js
@@ -2,9 +2,12 @@ var assignments = {
   preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
   assignments: [
     {
-      link : '#',
-      title: 'TBD',
-      notesHTML: []
+      link : 'https://classroom.github.com/a/4CO1EmX0',
+      title: 'Assignment 1 – A simple weather app using RecyclerView',
+      notesHTML: [
+        'Assignment due by 11:59pm, Monday 1/29/2018',
+        'Demo due by 11:59pm, Monday 2/12/2018'
+      ]
     },
   ]
 };
