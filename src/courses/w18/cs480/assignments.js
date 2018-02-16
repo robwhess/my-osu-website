da47beace0c6e3813be8f820343c44dd64d416a0
Rob Hess <robwhess@gmail.com>
2018-02-16 15:13:49 -0800

    Extend CS 480 assignment 2 deadline and post midterm solutions.

--- src/courses/w18/cs480/assignments.js
+++ src/courses/w18/cs480/assignments.js
@@ -13,8 +13,8 @@ var assignments = {
       link : 'https://classroom.github.com/a/Jd54l1S3',
       title: 'Assignment 2 - Parsing and syntax-directed translation',
       notesHTML: [
-        'Assignment due by 11:59pm, Monday 2/19/2018',
-        'Demo due by 11:59pm, Monday 3/5/2018'
+        'Assignment due by 11:59pm, Monday <span class="strike">2/19/2018</span> 2/26/2018',
+        'Demo due by 11:59pm, Monday <span class="strike">3/5/2018</span> 3/12/2018'
       ]
     }
   ]
