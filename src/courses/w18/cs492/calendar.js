7fbda72f19f440fcc62ddd040df358dde24d2548
Rob Hess <robwhess@gmail.com>
2018-02-19 14:49:58 -0800

    Add git/GitHub notes to CS 492.

--- src/courses/w18/cs492/calendar.js
+++ src/courses/w18/cs492/calendar.js
@@ -6,6 +6,7 @@ var calendar = [
         heading: 'Topics',
         entriesHTML: [
           'Course Intro &ndash; <a href="https://docs.google.com/document/d/1UTWRygJFwM_4rh3qziFHHlRDwMST5hDcYe9ABZSSmyA/edit?usp=sharing">notes</a>',
+          'Git and GitHub &ndash; <a href="https://docs.google.com/document/d/1AdrFnQTwCLeB5dCphfRj3itg6cTcaeePm2WCtVHlLGA/edit?usp=sharing">notes</a>',
           'Java &ndash; <a href="https://docs.google.com/document/d/10-d9kS41MD5JXYK0vbuJBtNXP6yAZQzmTjYnsL8WxGs/edit?usp=sharing">notes</a> &ndash; <a href="https://github.com/OSU-CS492-W18/java">example code</a>'
         ]
       },
