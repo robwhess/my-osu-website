5c215b8219cc191909391bb548036c7e124ad4df
Rob Hess <robwhess@gmail.com>
2018-04-02 15:53:23 -0700

    Add TA hours to CS 290.

--- src/courses/sp18/cs290/taInfo.js
+++ src/courses/sp18/cs290/taInfo.js
@@ -3,29 +3,35 @@ var taInfo = {
     {
       name: "Arash Shahbaz Badr",
       email: "shahbaar@oregonstate.edu",
-      officeHours: [ "TBD" ],
-      gradingHours: [ "TBD" ],
+      officeHours: [ "Tu 12:00-2:00pm (Location TBD)" ],
+      gradingHours: [
+        "Tu 2:00-5:00pm (Location TBD)",
+        "Th 2:00-5:00pm (Location TBD)"
+      ],
       gradingHoursPollLink: "#"
     },
     {
       name: "Fengfei Zheng",
       email: "zhengfen@oregonstate.edu",
-      officeHours: [ "TBD" ],
+      officeHours: [
+        "Tu 12:50-1:50pm (Location TBD)",
+        "W 11:00am-12:00pm (Location TBD)"
+      ],
       gradingHours: [ "TBD" ],
       gradingHoursPollLink: "#"
     },
     {
       name: "Prashant Kumar",
       email: "kumarp@oregonstate.edu",
-      officeHours: [ "TBD" ],
-      gradingHours: [ "TBD" ],
+      officeHours: [ "M 2:00-3:00pm (Location TBD)" ],
+      gradingHours: [ "M 2:00-6:00pm (Location TBD)" ],
       gradingHoursPollLink: "#"
     },
     {
       name: "Purbasha Chatterjee",
       email: "chattepu@oregonstate.edu",
-      officeHours: [ "TBD" ],
-      gradingHours: [ "TBD" ],
+      officeHours: [ "M 10:00-11:00am (Location TBD)" ],
+      gradingHours: [ "Tu 9:00am-12:00pm (Location TBD)" ],
       gradingHoursPollLink: "#"
     },
   ]
