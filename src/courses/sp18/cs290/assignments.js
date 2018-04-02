ed2152fa4cdf7e6ef331566207fa59ad1133c101
Rob Hess <robwhess@gmail.com>
2018-04-02 12:00:13 -0700

    Add assignment 1 to CS 290.

--- src/courses/sp18/cs290/assignments.js
+++ src/courses/sp18/cs290/assignments.js
@@ -1,17 +1,17 @@
 var assignments = {
   preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
   assignments: [
+    {
+      link : 'https://classroom.github.com/a/sZp-Auwf',
+      title: 'Assignment 1 - HTML and web publishing',
+      notesHTML: [
+        'Assignment due by 11:59pm, Monday 4/9/2018',
+        'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 4/9/2018',
+        'Demo due by 11:59pm on Monday, 4/23/2018'
+      ]
+    },
     // {
-    //   link : 'https://classroom.github.com/a/llQf7q8s',
-    //   title: 'Assignment 1 - HTML, Git, and GitHub',
-    //   notesHTML: [
-    //     'Assignment due by 11:59pm, Monday 10/2/2017',
-    //     'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/2/2017',
-    //     'Demo due by 11:59pm on Monday, 10/16/2017'
-    //   ]
-    // },
-    // {
-    //   link : 'https://classroom.github.com/a/Y1Yqc7UO',
+    //   link : '',
     //   title: 'Assignment 2 - CSS',
     //   notesHTML: [
     //     'Assignment due by 11:59pm, Monday 10/16/2017',
@@ -20,7 +20,7 @@ var assignments = {
     //   ]
     // },
     // {
-    //   link : 'https://classroom.github.com/a/prGm3Bvv',
+    //   link : '',
     //   title: 'Assignment 3 - Client-side JS',
     //   notesHTML: [
     //     'Assignment due by 11:59pm, Monday 11/6/2017',
@@ -29,7 +29,7 @@ var assignments = {
     //   ]
     // },
     // {
-    //   link : 'https://classroom.github.com/a/tmpIaB80',
+    //   link : '',
     //   title: 'Assignment 4 - Native Node.js file serving',
     //   notesHTML: [
     //     'Assignment due by 11:59pm, Monday 11/13/2017',
@@ -38,7 +38,7 @@ var assignments = {
     //   ]
     // },
     // {
-    //   link : 'https://classroom.github.com/a/yFR6VblN',
+    //   link : '',
     //   title: 'Assignment 5 - Handlebars templating',
     //   notesHTML: [
     //     'Assignment due by 11:59pm, Wednesday 11/29/2017',
