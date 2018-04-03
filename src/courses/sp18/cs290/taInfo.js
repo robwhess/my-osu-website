79ab15ef9ec6cacc7446ab5690c83ceef78c8c05
Rob Hess <robwhess@gmail.com>
2018-04-03 10:45:01 -0700

    Update CS 290 office hours.

--- src/courses/sp18/cs290/taInfo.js
+++ src/courses/sp18/cs290/taInfo.js
@@ -17,14 +17,18 @@ var taInfo = {
         "Tu 12:50-1:50pm (Location TBD)",
         "W 11:00am-12:00pm (Location TBD)"
       ],
-      gradingHours: [ "TBD" ],
+      gradingHours: [
+        "M 8:00-9:00am (Location TBD)",
+        "M 10:00am-12:00pm (Location TBD)",
+        "M 3:50-6:50pm (Location TBD)"
+      ],
       gradingHoursPollLink: "#"
     },
     {
       name: "Prashant Kumar",
       email: "kumarp@oregonstate.edu",
       officeHours: [ "M 2:00-3:00pm (Location TBD)" ],
-      gradingHours: [ "M 2:00-6:00pm (Location TBD)" ],
+      gradingHours: [ "M 3:00-6:00pm (Location TBD)" ],
       gradingHoursPollLink: "#"
     },
     {
