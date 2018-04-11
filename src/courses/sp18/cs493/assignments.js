af4ed4ba67dc2ca5dc857923eae8b8be4adaa738
Rob Hess <robwhess@gmail.com>
2018-04-10 17:59:52 -0700

    Post assignment 1 to CS 493.

--- src/courses/sp18/cs493/assignments.js
+++ src/courses/sp18/cs493/assignments.js
@@ -1,15 +1,14 @@
 var assignments = {
   preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
   assignments: [
-    // {
-    //   link : 'https://classroom.github.com/a/llQf7q8s',
-    //   title: 'Assignment 1 - HTML, Git, and GitHub',
-    //   notesHTML: [
-    //     'Assignment due by 11:59pm, Monday 10/2/2017',
-    //     'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/2/2017',
-    //     'Demo due by 11:59pm on Monday, 10/16/2017'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/lOS-TXPe',
+      title: 'Assignment 1 - API design & implementation and OpenAPI specification',
+      notesHTML: [
+        'Assignment due by 11:59pm, Monday 4/23/2018',
+        'Demo due by 11:59pm on Monday, 5/7/2018'
+      ]
+    },
   ]
 };
 
