1512404d3da12d71c60e6c51b31228a49dbf33b7
Rob Hess <robwhess@gmail.com>
2018-04-11 10:49:18 -0700

    Update Arash's office/grading hours.

--- src/courses/sp18/cs290/taInfo.js
+++ src/courses/sp18/cs290/taInfo.js
@@ -3,10 +3,10 @@ var taInfo = {
     {
       name: "Arash Shahbaz Badr",
       email: "shahbaar@oregonstate.edu",
-      officeHours: [ "Tu 12:00-2:00pm (JOHN 123)" ],
+      officeHours: [ "Th 3:00-5:00pm (JOHN 123)" ],
       gradingHours: [
-        "Tu 2:00-5:00pm (JOHN 123)",
-        "Th 2:00-5:00pm (JOHN 123)"
+        "Tu 12:00-5:00pm (JOHN 123)",
+        "Th 2:00-3:00pm (JOHN 123)"
       ],
       gradingHoursPollLink: "https://calendar.google.com/calendar/selfsched?sstoken=UU1TZS1ucWlQMFN5fGRlZmF1bHR8ZWRmNzgzN2UzZWUwOGNhYmMzZmY3MGJlYTExNmRlNjE"
     },
