708f4b326a733ccdf98c93336028b4f59487258b
Rob Hess <robwhess@gmail.com>
2018-04-24 15:23:21 -0700

    Add assignment 3 to CS 290.

--- src/courses/sp18/cs290/assignments.js
+++ src/courses/sp18/cs290/assignments.js
@@ -19,15 +19,15 @@ var assignments = {
         'Demo due by 11:59pm on Monday, 5/7/2018'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 3 - Client-side JS',
-    //   notesHTML: [
-    //     'Assignment due by 11:59pm, Monday 11/6/2017',
-    //     'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 11/6/2017',
-    //     'Demo due by 11:59pm on Monday, 11/20/2017'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/Wz0jjtvW',
+      title: 'Assignment 3 - Client-side JS',
+      notesHTML: [
+        'Assignment due by 11:59pm, Monday 5/7/2018',
+        'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 5/7/2018',
+        'Demo due by 11:59pm on Monday, 5/21/2018'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 4 - Native Node.js file serving',
