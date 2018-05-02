91fd10c20d85857d5f2c4f8bea19b281c8bf43dd
Rob Hess <robwhess@gmail.com>
2018-05-02 16:53:04 -0700

    Extend CS 493 assignment 2.

--- src/courses/sp18/cs493/assignments.js
+++ src/courses/sp18/cs493/assignments.js
@@ -13,8 +13,8 @@ var assignments = {
       link : 'https://classroom.github.com/a/9ar_8SwW',
       title: 'Assignment 2 - Docker and MySQL',
       notesHTML: [
-        'Assignment due by 11:59pm, Monday 5/7/2018',
-        'Demo due by 11:59pm on Monday, 5/21/2018'
+        'Assignment due by 11:59pm, Monday <span class="strike">5/7/2018</span> 5/14/2018',
+        'Demo due by 11:59pm on Monday, <span class="strike">5/21/2018</span> 5/28/2018'
       ]
     }
   ]
