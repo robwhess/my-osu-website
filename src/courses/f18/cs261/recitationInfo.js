09fbbccb174b4f5f19669ba9f1922283aaa763f8
Rob Hess <robwhess@gmail.com>
2018-09-21 17:37:16 -0700

    Assign TAs to recitations and add week 1 recitation worksheet to CS 261.

--- src/courses/f18/cs261/recitationInfo.js
+++ src/courses/f18/cs261/recitationInfo.js
@@ -12,62 +12,56 @@ var recitationInfo = {
       number: '010',
       dayTime: 'Tu 3:00-3:50pm',
       location: 'Weniger 287',
-      ta: 'TBA'
+      ta: 'Zahir Al-Sulaimawi'
     },
     {
       number: '011',
       dayTime: 'W 3:00-3:50pm',
       location: 'KEC 1001',
-      ta: 'TBA'
+      ta: 'Zahir Al-Sulaimawi'
     },
     {
       number: '012',
       dayTime: 'W 6:00-6:50pm',
       location: 'KEC 1003',
-      ta: 'TBA'
+      ta: 'Aayam Shrestha'
     },
     {
       number: '013',
       dayTime: 'Th 8:00-8:50am',
       location: 'KEC 1003',
-      ta: 'TBA'
+      ta: 'Prashant Kumar'
     },
     {
       number: '014',
       dayTime: 'Th 6:00-6:50pm',
       location: 'KEC 1003',
-      ta: 'TBA'
+      ta: 'Chloe Fleming'
     },
     {
       number: '015',
       dayTime: 'Tu 6:00-6:50pm',
       location: 'KEC 1003',
-      ta: 'TBA'
+      ta: 'Chloe Fleming'
     },
     {
       number: '016',
       dayTime: 'Tu 10:00-10:50am',
       location: 'STAG 160',
-      ta: 'TBA'
+      ta: 'Andrew Emmott'
     },
     {
       number: '017',
       dayTime: 'W 11:00-11:50am',
       location: 'Milam 033',
-      ta: 'TBA'
+      ta: 'Prashant Kumar'
     },
     {
       number: '018',
       dayTime: 'M 6:00-6:50pm',
       location: 'KEC 1003',
-      ta: 'TBA'
-    },
-    {
-      number: '020',
-      dayTime: 'M 6:00-6:50pm',
-      location: 'Batcheller 144',
-      ta: 'TBA'
-    },
+      ta: 'Aayam Shrestha'
+    }
   ],
 
   calendar: [
@@ -93,7 +87,8 @@ var recitationInfo = {
         {
           heading: 'Recitation worksheet',
           entriesHTML: [
-            'TBA'
+            '<a href="https://docs.google.com/document/d/1VIeZ-OQujN4vbrlEM2V72VtaRr7Tj63tssBLtS47_JQ/edit?usp=sharing">Week 1 Recitation Worksheet</a> &ndash; print this and bring it with you to recitation',
+            '<a href="https://gist.githubusercontent.com/robwhess/a55e1360b474b910ab19b3af24cd5ae2/raw/301eb52186fa405d80311b5827f9dfc1d15bce31/test.c">Here\'s a link</a> to the C program you\'ll have to download as part of the week\'s recitation exercise.'
           ]
         }
       ]
