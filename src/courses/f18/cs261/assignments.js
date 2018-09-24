68302e64d8946b8aaded20e216e7a1065990e362
Rob Hess <robwhess@gmail.com>
2018-09-24 08:33:43 -0700

    Publish CS 261 assignment 0.

--- src/courses/f18/cs261/assignments.js
+++ src/courses/f18/cs261/assignments.js
@@ -2,19 +2,13 @@ var assignments = {
   preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
   assignments: [
     {
-      link : '#',
-      title: 'Assignment 0 - TBA',
+      link : 'https://classroom.github.com/a/y0RCxcIK',
+      title: 'Assignment 0 - Git and GitHub',
       notesHTML: [
+        'Assignment due by 11:59pm, Monday 10/1/2018',
+        '<b>No demo for this assignment</b>'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 0 - Git and GitHub',
-    //   notesHTML: [
-    //     'Assignment due by 11:59pm, Wednesday 9/27/2017',
-    //     '<b>No demo for this assignment</b>'
-    //   ]
-    // },
     // {
     //   link : '',
     //   title: 'Assignment 1 - Practicing C and pointers',
