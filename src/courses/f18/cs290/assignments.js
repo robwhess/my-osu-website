a2a6098a177e9c679cbcfb4af4a3915883b0f50f
Rob Hess <robwhess@gmail.com>
2018-09-24 10:24:23 -0700

    Publish CS 290 assignment 1.

--- src/courses/f18/cs290/assignments.js
+++ src/courses/f18/cs290/assignments.js
@@ -2,19 +2,14 @@ var assignments = {
   preambleHTML: "<p> Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a <code>README.md</code> file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.</p>",
   assignments: [
     {
-      link : '#',
-      title: 'Assignment 1 - TBA',
-      notesHTML: []
+      link : 'https://classroom.github.com/a/7zGLdJGC',
+      title: 'Assignment 1 - HTML and web publishing',
+      notesHTML: [
+        'Assignment due by 11:59pm, Monday 10/1/2018',
+        'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 10/1/2018',
+        'Demo due by 11:59pm on Monday, 10/15/2018'
+      ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 - HTML and web publishing',
-    //   notesHTML: [
-    //     'Assignment due by 11:59pm, Monday 4/9/2018',
-    //     'Code Blog due <b>ON CANVAS</b> by 11:59pm, Monday 4/9/2018',
-    //     'Demo due by 11:59pm on Monday, 4/23/2018'
-    //   ]
-    // },
     // {
     //   link : '',
     //   title: 'Assignment 2 - Styling a page with CSS',
