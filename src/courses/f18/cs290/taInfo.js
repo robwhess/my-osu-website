1943be0eb2228d8f8a7e9ba9b3185ce28125354b
Rob Hess <robwhess@gmail.com>
2018-09-25 10:51:24 -0700

    Add TA grading/office hours for CS 290.

--- src/courses/f18/cs290/taInfo.js
+++ src/courses/f18/cs290/taInfo.js
@@ -5,16 +5,22 @@ var taInfo = {
       email: "kumariau@oregonstate.edu",
       officeHours: [ "Tu 2:00-3:00pm (JOHN 123)" ],
       gradingHours: [
-        "TBA"
+        "Tu 12:00-2:00pm (JOHN 121)",
+        "Th 12:00-2:00pm (KEC Atrium)"
       ],
       gradingHoursPollLink: "#"
     },
     {
       name: "Vivswan Shitole",
       email: "shitolev@oregonstate.edu",
-      officeHours: [ "TBA" ],
+      officeHours: [
+        "F 10:00am-12:00pm (KEC Atrium)",
+      ],
       gradingHours: [
-        "TBA"
+        "M 10:00am-12:00pm (KEC Atrium)",
+        "Tu 2:00-4:00pm (KEC Atrium)",
+        "W 10:00am-12:00pm (KEC Atrium)",
+        "Th 2:00-4:00pm (KEC Atrium)"
       ],
       gradingHoursPollLink: "#"
     },
