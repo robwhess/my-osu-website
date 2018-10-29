c21318fbd0ca1b83381fbd70165b250ca0061ba8
Rob Hess <robwhess@gmail.com>
2018-10-29 12:44:54 -0700

    Publish final project info to CS 290.

--- src/courses/f18/cs290/finalProject.js
+++ src/courses/f18/cs290/finalProject.js
@@ -1,10 +1,10 @@
 var finalProject = {
   preambleHTML: "<p>For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.</p>",
   assignments: [
-    // {
-    //   title: 'Final Project Description',
-    //   link: 'https://docs.google.com/document/d/1XTnAL8cuM8n_FCZuN2Dw9LARleZT3HKg-zg2qPPsjWk/edit?usp=sharing'
-    // },
+    {
+      title: 'Final Project Description',
+      link: 'https://docs.google.com/document/d/1dMv_TcO9pJf3KbpkMrkhCeSZ0pWolj0t0k_uR6e-ufs/edit?usp=sharing'
+    },
     // {
     //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
     //   title: 'Final Project Demo Signup',
@@ -18,7 +18,5 @@ var finalProject = {
     // }
   ]
 };
-
-finalProject = null;
 
 export default finalProject;
