4762d9fd6a16ff2eab2b95ac93960204fe9eb956
Rob Hess <robwhess@gmail.com>
2018-12-14 16:09:20 -0800

    Spiff up course lists.

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -12,13 +12,62 @@ import { generateSitePath } from '../lib/SitePath';
 
 const CourseListList = styled.ul`
   margin: 0;
+  padding-left: 10px;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  a {
+    margin: 2px;
+    padding: 0;
+    border: 1px solid rgba(0, 0, 0, 0);
+    border-radius: 4px;
+    &:hover {
+      border: 1px solid rgba(0, 0, 0, 0.4);
+      text-decoration: none;
+    }
+  }
+`;
+
+const CourseItem = styled.li`
+  display: flex;
+  align-items: center;
+  font-weight: 400;
+`;
+
+const CourseNumber = styled.div`
+  padding: 5px 5px 5px 10px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  background-color: #888;
+  color: #fff;
+  ${CourseItem}:hover & {
+    background-color: #ccc;
+    color: #333;
+  }
+`;
+
+const CourseTitle = styled.div`
+  padding: 5px 10px 5px 5px;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background-color: rgba(213, 79, 30, 0.5);
+  color: #333;
+  ${CourseItem}:hover & {
+    background-color: rgba(213, 79, 30, 0.2);
+    color: #555;
+  }
 `;
 
 function CourseList(props) {
   return (
     <CourseListList>
       {Object.keys(props.courses).map((course, i) => (
-      <li key={i}><Link to={generateSitePath(`/teaching/${course}-${props.term}`)}>{props.courses[course].number} &ndash; {props.courses[course].title}</Link></li>
+        <Link key={i} to={generateSitePath(`/teaching/${course}-${props.term}`)}>
+          <CourseItem>
+            <CourseNumber>{props.courses[course].number}</CourseNumber>
+            <CourseTitle>{props.courses[course].title}</CourseTitle>
+          </CourseItem>
+        </Link>
       ))}
     </CourseListList>
   )
