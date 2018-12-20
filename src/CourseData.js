54fe87b146e465d918f258b5acabd972edb9ffe1
Rob Hess <robwhess@gmail.com>
2018-12-19 23:21:51 -0500

    Don't display old course pages.

--- src/CourseData.js
+++ src/CourseData.js
@@ -1,17 +1,17 @@
-import sp17 from './courses/sp17/';
-import f17 from './courses/f17/';
-import w18 from './courses/w18/';
-import sp18 from './courses/sp18/';
+// import sp17 from './courses/sp17/';
+// import f17 from './courses/f17/';
+// import w18 from './courses/w18/';
+// import sp18 from './courses/sp18/';
 import f18 from './courses/f18/';
 
 const currentTerm = 'f18';
 
 const courseData = {
   f18: f18,
-  sp18: sp18,
-  w18: w18,
-  f17: f17,
-  sp17: sp17
+  // sp18: sp18,
+  // w18: w18,
+  // f17: f17,
+  // sp17: sp17
 };
 
 const currentTermData = courseData[currentTerm];
