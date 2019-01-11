eeba1ea11dba8ad86999cf8085acba9444480ee6
Rob Hess <robwhess@gmail.com>
2019-01-11 14:54:56 -0800

    Add link to debate description on CS 391.

--- src/courses/w19/cs391/debate.js
+++ src/courses/w19/cs391/debate.js
@@ -8,6 +8,10 @@ const debate = {
         day: 'Wednesday, 1/9/2019',
         time: '11:59pm'
       }
+    },
+    {
+      link: 'https://docs.google.com/document/d/1b75KB20VRTYy7xkBPtcMfNsdqYnTdgC-dNO3JBh54f0/edit?usp=sharing',
+      title: 'Debate Description'
     }
   ],
   preamble: 'Starting in week 4 or 5, we\'ll hold one debate between two teams of students each week during lecture.  Each debate will focus on an issue related to the topics we discuss in class.  Assignments and information about the debate are listed below.'
