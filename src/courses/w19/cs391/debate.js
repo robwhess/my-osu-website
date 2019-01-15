86f45851b0bc98d36302b67323098652e20d7b5b
Rob Hess <robwhess@gmail.com>
2019-01-14 16:32:30 -0800

    Publish more info about CS 391 debates.

--- src/courses/w19/cs391/debate.js
+++ src/courses/w19/cs391/debate.js
@@ -12,6 +12,18 @@ const debate = {
     {
       link: 'https://docs.google.com/document/d/1b75KB20VRTYy7xkBPtcMfNsdqYnTdgC-dNO3JBh54f0/edit?usp=sharing',
       title: 'Debate Description'
+    },
+    {
+      link: 'https://docs.google.com/document/d/1ucrAW4I1J8UtezndddLMrtkGY1o7nQws1djzw0kqzAk/edit?usp=sharing',
+      title: 'Debate Rubric'
+    },
+    {
+      link: 'https://docs.google.com/document/d/1D-LL9_qi6UITz1NOjkF2vcleWHq1lo1_V6gd1BKwlT4/edit?usp=sharing',
+      title: 'Debate Team Evaluation',
+      due: {
+        day: 'Friday, 3/15/2019',
+        time: '11:59pm'
+      }
     }
   ],
   preamble: 'Starting in week 4 or 5, we\'ll hold one debate between two teams of students each week during lecture.  Each debate will focus on an issue related to the topics we discuss in class.  Assignments and information about the debate are listed below.'
