f50850d1d3744ede7ee1207334d4182f0b798d26
Rob Hess <robwhess@gmail.com>
2019-01-29 13:36:03 -0800

    Publish CS 492 assignment 2.

--- src/courses/w19/cs492/assignments.js
+++ src/courses/w19/cs492/assignments.js
@@ -11,6 +11,17 @@ const assignments = {
       notes: [
         'Demo due by 11:59pm, Monday 2/11/2019'
       ]
+    },
+    {
+      link : 'https://classroom.github.com/a/Xr1chLvX',
+      title: 'Assignment 2 &ndash; A weather app with internet connectivity and Intents',
+      due: {
+        day: 'Monday, 2/11/2019',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 2/25/2019'
+      ]
     }
   ],
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
