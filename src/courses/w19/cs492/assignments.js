3a113a9faa37510e7cdcb2099cfd7b5e6f1c0f90
Rob Hess <robwhess@gmail.com>
2019-02-12 20:47:47 -0800

    Publish CS 492 assignment 3.

--- src/courses/w19/cs492/assignments.js
+++ src/courses/w19/cs492/assignments.js
@@ -22,6 +22,17 @@ const assignments = {
       notes: [
         'Demo due by 11:59pm, Monday 2/25/2019'
       ]
+    },
+    {
+      link : 'https://classroom.github.com/a/w40DtTad',
+      title: 'Assignment 3 &ndash; A lifecycle-aware weather app with user preferences',
+      due: {
+        day: 'Monday, 2/25/2019',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 3/11/2019'
+      ]
     }
   ],
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
