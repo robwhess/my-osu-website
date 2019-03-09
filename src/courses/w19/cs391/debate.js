2156cd66e9482320a3c253a5fe12b2c273ab8547
Rob Hess <robwhess@gmail.com>
2019-03-08 16:42:01 -0800

    Post debate survey link to CS 391.

--- src/courses/w19/cs391/debate.js
+++ src/courses/w19/cs391/debate.js
@@ -33,7 +33,7 @@ const debate = {
         'Debate #3 (2/25): [Pre-debate](https://docs.google.com/forms/d/e/1FAIpQLSee7HNo1zWcGUV-2Rks8X8PFXkP7L_vlnzKzIZFWay0EKkhWQ/viewform?usp=sf_link) | [Post-debate](https://docs.google.com/forms/d/e/1FAIpQLSdvWN1kO1XPynILnHAqlUQbptyXtFztvZjqvE0I4zoZmOY_KA/viewform?usp=sf_link)',
         'Debate #4 (3/1): [Pre-debate](https://docs.google.com/forms/d/e/1FAIpQLSc7A6A3BtAuCFCaks3emikzQxzhNMEO-BCr3sXUqskezOEz5Q/viewform?usp=sf_link) | [Post-debate](https://docs.google.com/forms/d/e/1FAIpQLScSX2LocjR98Wt8WtIEbxFHk1Sog-Ldt8fiMnNmS8eHgIV4og/viewform?usp=sf_link)',
         'Debate #5 (3/6): [Pre-debate](https://docs.google.com/forms/d/e/1FAIpQLSe8R_RQXEnoipjhF2yMDeVMvSQnaF9xGx7cYa3P4xkUeraYbw/viewform?usp=sf_link) | [Post-debate](https://docs.google.com/forms/d/e/1FAIpQLSeAfzHiPHGFPwyE8sz38K6dqWn_tsDBuPy0sDf7L7k4re2HIw/viewform?usp=sf_link)',
-        'Debate #6 (3/8): [Pre-debate](https://docs.google.com/forms/d/e/1FAIpQLSfHMREKnhce-OdKXqwaF5KwUJfGIyadAZDZYsq8h80Fq_SCJw/viewform?usp=sf_link)'
+        'Debate #6 (3/8): [Pre-debate](https://docs.google.com/forms/d/e/1FAIpQLSfHMREKnhce-OdKXqwaF5KwUJfGIyadAZDZYsq8h80Fq_SCJw/viewform?usp=sf_link) | [Post-debate](https://docs.google.com/forms/d/e/1FAIpQLScsMTTsdn5o584vwrrwOKbgk366a8D-WhOjyKg8RgkWDz54mw/viewform?usp=sf_link)'
       ]
     }
   ],
