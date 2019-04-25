be7b1691193a4e4cfe4c9a34c15168f7dff44a39
Rob Hess <robwhess@gmail.com>
2019-04-25 14:46:06 -0700

    Use shrug on no match page instead of "404".

--- src/pages/NoMatchPage.js
+++ src/pages/NoMatchPage.js
@@ -9,12 +9,14 @@ import PageContent from '../components/PageContent';
 
 const ErrorCodeContainer = styled.div`
   width: 100%;
+  margin-top: 75px;
   text-align: center;
   font-size: 150px;
   font-weight: 100;
 `;
 
 const ExplanationContainer = styled(ErrorCodeContainer)`
+  margin-top: 50px;
   margin-bottom: 200px;
   font-size: 40px;
 `
@@ -23,7 +25,7 @@ function NoMatchPage() {
   return (
     <PageContent>
       <ErrorCodeContainer>
-        404
+        ¯\_(ツ)_/¯
       </ErrorCodeContainer>
       <ExplanationContainer>
         Woops! We couldn't find that page.
