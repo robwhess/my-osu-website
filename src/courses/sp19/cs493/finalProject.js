f8d31e25e1f42f55c74c315a9a6de059bd14c0b6
Rob Hess <robwhess@gmail.com>
2019-05-12 23:22:22 -0700

    Publish CS 493 final project.

--- src/courses/sp19/cs493/finalProject.js
+++ src/courses/sp19/cs493/finalProject.js
@@ -1,10 +1,10 @@
 const finalProject = {
   title: 'Final Project',
   assignments: [
-    // {
-    //   title: 'Final Project Description',
-    //   link: 'https://docs.google.com/document/d/1dMv_TcO9pJf3KbpkMrkhCeSZ0pWolj0t0k_uR6e-ufs/edit?usp=sharing'
-    // },
+    {
+      title: 'Final Project Description',
+      link: 'https://docs.google.com/document/d/1j8Xhj6f8UsyzTuzAGw_cCtk6ooa5tv-dTR5-r5MWLpI/edit?usp=sharing'
+    },
     // {
     //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
     //   title: 'Final Project Demo Signup',
@@ -16,13 +16,13 @@ const finalProject = {
     //     'Code is due on GitHub when when you do your demo.'
     //   ]
     // },
-    // {
-    //   link: 'https://docs.google.com/document/d/1xtAkfi6860veDD22_4FpwHcJyP1x9y-nXI0jJ2XRTS0/edit?usp=sharing',
-    //   title: 'Final Project Group Evaluation',
-    //   notes: [
-    //     'Due **ON CANVAS** by 5:00pm, Friday 12/7/2018'
-    //   ]
-    // }
+    {
+      link: 'https://docs.google.com/document/d/1msBghR_qZwOu814bQ4s2-NnU2MuXs4NqszqmKD6923E/edit?usp=sharing',
+      title: 'Final Project Group Evaluation',
+      notes: [
+        'Due **ON CANVAS** by 5:00pm, Friday 6/14/2019'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete RESTful API.  You can find more info below.'
