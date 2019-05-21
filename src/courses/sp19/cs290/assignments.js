ae88bbacc37ed063ff425f34a40f9a3c1ed32712
Rob Hess <robwhess@gmail.com>
2019-05-21 15:18:15 -0700

    Publish CS 290 assignment 5.

--- src/courses/sp19/cs290/assignments.js
+++ src/courses/sp19/cs290/assignments.js
@@ -49,18 +49,18 @@ const assignments = {
         '**No demo for this assignment**'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 5 &ndash; Handlebars templating',
-    //   due: {
-    //     day: 'Wednesday, 11/28/2018',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog due **ON CANVAS** by assignment due date',
-    //     '**No demo for this assignment**'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/fg2tY7kS',
+      title: 'Assignment 5 &ndash; Handlebars templating',
+      due: {
+        day: 'Monday, 6/3/2019',
+        time: '11:59pm'
+      },
+      notes: [
+        'Code Blog due **ON CANVAS** by assignment due date',
+        'Demo due by 5:00pm on Friday, 6/14/2019'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
