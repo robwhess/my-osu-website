afd2c4d29596344ec6247be13a22a714d918249e
Rob Hess <robwhess@gmail.com>
2019-06-04 14:09:29 -0700

    Publish final project demo signup links.

--- src/courses/sp19/cs290/finalProject.js
+++ src/courses/sp19/cs290/finalProject.js
@@ -5,22 +5,22 @@ const finalProject = {
       title: 'Final Project Description',
       link: 'https://docs.google.com/document/d/1yz34l-pfAUI09d-_FbM78hlveftSZXOvAsG5mMEb9g8/edit?usp=sharing'
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Final project demos will be during finals week in KEC 1109.',
-    //     'Sign up for a final project demo using the link above.',
-    //     'Sign up for one slot per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub when when you do your demo.'
-    //   ]
-    // },
     {
       link: 'https://docs.google.com/document/d/1weWNBYQITX35QqG1HpiEEf8ZkhdnJKr10SRhWSQoIA4/edit?usp=sharing',
       title: 'Final Project Team Evaluation',
       notes: [
         'Due **ON CANVAS** by 5:00pm, Friday 6/14/2019'
+      ]
+    },
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Final project demos will be during finals week in KEC 1109.',
+        'Sign up for a final project demo using the link above.',
+        'Sign up for one slot per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub when when at the time of your demo.'
       ]
     }
   ],
