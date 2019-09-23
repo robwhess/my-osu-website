cc7219aa6d42c36ab75c44b23bf6d4582c8f519a
Rob Hess <robwhess@gmail.com>
2019-09-23 09:34:51 -0700

    Add all TAs.

--- src/data/courses/f19/cs261/tas.js
+++ src/data/courses/f19/cs261/tas.js
@@ -1,152 +1,64 @@
 const taInfo = [
-  // {
-  //   name: "Zahir Al-Sulaimawi",
-  //   email: "alsulaiz@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'M',
-  //       time: '8:00 &ndash; 10:00am',
-  //       location: 'JOHN 121'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Th',
-  //       time: '4:00 &ndash; 7:00pm',
-  //       location: 'JOHN 122',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UURkY1pGQTNXb3dNfGRlZmF1bHR8NzAxNWI0NTczZjM2ZmM1NTgzYTU5MjI1NDJjNDc4YTk'
-  //     },
-  //     {
-  //       day: 'F',
-  //       time: '4:00 &ndash; 7:00pm',
-  //       location: 'JOHN 122',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UURkY1pGQTNXb3dNfGRlZmF1bHR8NzAxNWI0NTczZjM2ZmM1NTgzYTU5MjI1NDJjNDc4YTk'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Andrew Emmott",
-  //   email: "emmotta@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'W',
-  //       time: '11:00am &ndash; 4:00pm',
-  //       location: 'webex',
-  //       link: 'https://oregonstate.webex.com/oregonstate/j.php?MTID=ma0d9d6122c98cc6ea0254573b14b57f4'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Tu',
-  //       time: '12:00-2:00pm',
-  //       location: 'JOHN 123',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU9xNmlQOVNJT0tNfGRlZmF1bHR8MWU3ZDRmNDU0OWMzODFhNmZiOTEyZjE4YWExMTFmYWU'
-  //     },
-  //     {
-  //       day: 'Th',
-  //       time: '1:00-2:00pm',
-  //       location: 'JOHN 125',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU9xNmlQOVNJT0tNfGRlZmF1bHR8MWU3ZDRmNDU0OWMzODFhNmZiOTEyZjE4YWExMTFmYWU'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Chloe Fleming",
-  //   email: "flemichl@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'Tu',
-  //       time: '4:00 &ndash; 5:00pm',
-  //       location: 'Covell 140 Table 4'
-  //     },
-  //     {
-  //       day: 'Th',
-  //       time: '12:00 &ndash; 1:00pm',
-  //       location: 'Covell 140 Table 2'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'W',
-  //       time: '3:00 &ndash; 6:00pm',
-  //       location: 'Covell 140 Table 4',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUxLN1A2MzFPUVF3fGRlZmF1bHR8MTE4ODlmYzUzZjEyNTUzN2VhMzcyZWJjZjZlOTY2YzU'
-  //     },
-  //     {
-  //       day: 'Th',
-  //       time: '9:00am &ndash; 12:00pm',
-  //       location: 'Covell 140 Table 2',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUxLN1A2MzFPUVF3fGRlZmF1bHR8MTE4ODlmYzUzZjEyNTUzN2VhMzcyZWJjZjZlOTY2YzU'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Prashant Kumar",
-  //   email: "kumarp@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'W',
-  //       time: '2:00 &ndash; 3:00pm',
-  //       location: 'JOHN 121'
-  //     },
-  //     {
-  //       day: 'Th',
-  //       time: '1:00 &ndash; 2:00pm',
-  //       location: 'JOHN 121'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Tu',
-  //       time: '4:00 &ndash; 7:00pm',
-  //       location: 'JOHN 122',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU12WkRvTS1TekxvfGRlZmF1bHR8YWVhMWJiZThjOTg1MDEyZWRkNWM0N2U3M2Y0ODk3ZGI'
-  //     },
-  //     {
-  //       day: 'Th',
-  //       time: '2:00 &ndash; 5:00pm',
-  //       location: 'JOHN 121',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU12WkRvTS1TekxvfGRlZmF1bHR8YWVhMWJiZThjOTg1MDEyZWRkNWM0N2U3M2Y0ODk3ZGI'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Aayam Shrestha",
-  //   email: "shrestaa@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'M',
-  //       time: '2:00 &ndash; 4:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'M',
-  //       time: '5:00 &ndash; 7:00pm',
-  //       location: 'KEC Atrium',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUV2cnhwSURETWwzfGRlZmF1bHR8NWRiNTNlNmViOGI0ODk3MDQxNjExOGFjMWQyNmFmMDQ'
-  //     },
-  //     {
-  //       day: 'Tu',
-  //       time: '8:00 &ndash; 10:00am',
-  //       location: 'KEC Atrium',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUV2cnhwSURETWwzfGRlZmF1bHR8NWRiNTNlNmViOGI0ODk3MDQxNjExOGFjMWQyNmFmMDQ'
-  //     },
-  //     {
-  //       day: 'Tu',
-  //       time: '2:00 &ndash; 3:00pm',
-  //       location: 'KEC Atrium',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUV2cnhwSURETWwzfGRlZmF1bHR8NWRiNTNlNmViOGI0ODk3MDQxNjExOGFjMWQyNmFmMDQ'
-  //     },
-  //     {
-  //       day: 'Tu',
-  //       time: '4:00 &ndash; 5:00pm',
-  //       location: 'KEC Atrium',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUV2cnhwSURETWwzfGRlZmF1bHR8NWRiNTNlNmViOGI0ODk3MDQxNjExOGFjMWQyNmFmMDQ'
-  //     }
-  //   ]
-  // }
+  {
+    name: "Zahir Al-Sulaimawi",
+    email: "alsulaiz@oregonstate.edu",
+    officeHours: [
+      // {
+      //   day: 'M',
+      //   time: '8:00 &ndash; 10:00am',
+      //   location: 'JOHN 121'
+      // }
+    ],
+    gradingHours: []
+  },
+  {
+    name: "Seifeddine Mejri",
+    email: "mejris@oregonstate.edu",
+    officeHours: [
+      // {
+      //   day: 'M',
+      //   time: '8:00 &ndash; 10:00am',
+      //   location: 'JOHN 121'
+      // }
+    ],
+    gradingHours: []
+  },
+  {
+    name: "Gregory Blood",
+    email: "bloodg@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  },
+  {
+    name: "Aaron Didner",
+    email: "didnera@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  },
+  {
+    name: "Jianlong Huang",
+    email: "huangj2@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  },
+  {
+    name: "Ryan Kennedy",
+    email: "kennerya@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  },
+  {
+    name: "Jiawei Mo",
+    email: "moji@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  },
+  {
+    name: "Huahua Wang",
+    email: "wanghuah@oregonstate.edu",
+    officeHours: [],
+    gradingHours: []
+  }
 ];
 
 export default taInfo;
--- src/data/courses/f19/cs290/tas.js
+++ src/data/courses/f19/cs290/tas.js
@@ -1,84 +1,23 @@
 const taInfo = [
-  // {
-  //   name: "Niraj Basnet",
-  //   email: "basnetn@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'Thurs.',
-  //       time: '4:00 &ndash; 6:00pm',
-  //       location: 'Covell 140, Table 4'
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '3:00 &ndash; 5:00pm',
-  //       location: 'Covell 140, Table 4'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '1:00 &ndash; 4:00pm',
-  //       location: 'Covell 140, Table 1',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU84NHpfZXZLVVRhfGRlZmF1bHR8NDU5ZTRkYmYxMTRjYzk0ZmZkMzhjMGI1ZmZmZTU2NjM'
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '1:00 &ndash; 4:00pm',
-  //       location: 'Covell 140, Table 4',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UU84NHpfZXZLVVRhfGRlZmF1bHR8NDU5ZTRkYmYxMTRjYzk0ZmZkMzhjMGI1ZmZmZTU2NjM'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Braden Hitchcock",
-  //   email: "hitchcob@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'Thurs.',
-  //       time: '9:00 &ndash; 11:00am',
-  //       location: 'JOHN 121'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Wed.',
-  //       time: '10:00 &ndash; 11:00am',
-  //       location: 'JOHN 121',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUY5Z1hLQ1pfYkJmfGRlZmF1bHR8MTE2Mzc0MTljNmFkZTJlNWQ0MDMzN2VhYTJjYmZiNTM'
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '4:00 &ndash; 6:00pm',
-  //       location: 'JOHN 121',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUY5Z1hLQ1pfYkJmfGRlZmF1bHR8MTE2Mzc0MTljNmFkZTJlNWQ0MDMzN2VhYTJjYmZiNTM'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: "Akshay Shinde",
-  //   email: "shindea@oregonstate.edu",
-  //   officeHours: [
-  //     {
-  //       day: 'Fri.',
-  //       time: '1:00 &ndash; 5:00pm',
-  //       location: 'JOHN 123'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '1:00 &ndash; 4:00pm',
-  //       location: 'Covell 140, Table 4',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUZqRXpqMmtPZkdLfGRlZmF1bHR8YWZmOWVlMDUyOWJkNjliZTljMGI2NTdmY2IyNWU1Mjc'
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '1:00 &ndash; 4:00pm',
-  //       location: 'KEC Atrium',
-  //       link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUZqRXpqMmtPZkdLfGRlZmF1bHR8YWZmOWVlMDUyOWJkNjliZTljMGI2NTdmY2IyNWU1Mjc'
-  //     }
-  //   ]
-  // }
+  {
+    name: "Ujjval Kumaria",
+    email: "kumariau@oregonstate.edu",
+    officeHours: [
+      // {
+      //   day: 'Tu',
+      //   time: '2:00 &ndash; 3:00pm',
+      //   location: 'JOHN 123'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tu',
+      //   time: '12:00 &ndash; 2:00pm',
+      //   location: 'JOHN 121',
+      //   link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUJjZnUtYW1ZXzE1fGRlZmF1bHR8YzYxNzM2NzY3ODIxZmEyNzY3ZjQ0OTZiNWU1MTc1OTM'
+      // }
+    ]
+  },
 ];
 
 export default taInfo;
