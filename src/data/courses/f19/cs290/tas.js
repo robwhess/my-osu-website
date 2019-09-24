87f456c7b20ef81f597b24dc7332ac126618b26b
Rob Hess <robwhess@gmail.com>
2019-09-24 10:15:16 -0700

    Add Vaishnavi to CS 290.

--- src/data/courses/f19/cs290/tas.js
+++ src/data/courses/f19/cs290/tas.js
@@ -18,6 +18,25 @@ const taInfo = [
       // }
     ]
   },
+  {
+    name: "Vaishnavi Rangarajan",
+    email: "rangarav@oregonstate.edu",
+    officeHours: [
+      // {
+      //   day: 'Tu',
+      //   time: '2:00 &ndash; 3:00pm',
+      //   location: 'JOHN 123'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tu',
+      //   time: '12:00 &ndash; 2:00pm',
+      //   location: 'JOHN 121',
+      //   link: 'https://calendar.google.com/calendar/selfsched?sstoken=UUJjZnUtYW1ZXzE1fGRlZmF1bHR8YzYxNzM2NzY3ODIxZmEyNzY3ZjQ0OTZiNWU1MTc1OTM'
+      // }
+    ]
+  }
 ];
 
 export default taInfo;
