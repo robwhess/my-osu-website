c3ab5fd708cf64d6d4b8fa3e760b600838c793bc
Rob Hess <robwhess@gmail.com>
2019-09-30 14:54:50 -0700

    Update CS 261 and CS 290 TA hours.

--- src/data/courses/f19/cs261/tas.js
+++ src/data/courses/f19/cs261/tas.js
@@ -3,13 +3,19 @@ const taInfo = [
     name: 'Zahir Al-Sulaimawi',
     email: 'alsulaiz@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: 'M',
-      //   time: '8:00 &ndash; 10:00am',
-      //   location: 'JOHN 121'
-      // }
+      {
+        day: 'W',
+        time: '12:30 &ndash; 2:30pm',
+        location: 'JOHN 125'
+      }
     ],
-    gradingHours: []
+    gradingHours: [
+      {
+        day: '',
+        time: '8:00 &ndash; 9:30am',
+        location: 'Covell 140, Table 1'
+      }
+    ]
   },
   {
     name: 'Gregory Blood',
@@ -20,8 +26,25 @@ const taInfo = [
   {
     name: 'Aaron Didner',
     email: 'didnera@oregonstate.edu',
-    officeHours: [],
-    gradingHours: []
+    officeHours: [
+      {
+        day: 'W',
+        time: '2:00 &ndash; 5:00pm',
+        location: 'Covell 140'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'Tu',
+        time: '10:00am &ndash; 12:00pm',
+        location: 'Covell 140'
+      },
+      {
+        day: '',
+        time: '2:00 &ndash; 3:30pm',
+        location: 'Covell 140'
+      }
+    ]
   },
   {
     name: 'Jianlong Huang',
@@ -39,11 +62,16 @@ const taInfo = [
     name: 'Seifeddine Mejri',
     email: 'mejris@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: 'M',
-      //   time: '8:00 &ndash; 10:00am',
-      //   location: 'JOHN 121'
-      // }
+      {
+        day: 'Th',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'F',
+        time: '12:00 &ndash; 1:00pm',
+        location: 'KEC Atrium'
+      }
     ],
     gradingHours: []
   },
@@ -56,8 +84,35 @@ const taInfo = [
   {
     name: 'Huahua Wang',
     email: 'wanghuah@oregonstate.edu',
-    officeHours: [],
-    gradingHours: []
+    officeHours: [
+      {
+        day: 'M',
+        time: '9:00 &ndash; 11:00am',
+        location: 'DEAR 119'
+      },
+      {
+        day: 'M',
+        time: '4:00 &ndash; 5:00pm',
+        location: 'JOHN 125'
+      },
+      {
+        day: 'M',
+        time: '5:00 &ndash; 5:30pm',
+        location: 'JOHN 121'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'W',
+        time: '9:00 &ndash; 10:00am',
+        location: 'JOHN 121'
+      },
+      {
+        day: 'F',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'JOHN 121'
+      }
+    ]
   }
 ];
 
--- src/data/courses/f19/cs290/tas.js
+++ src/data/courses/f19/cs290/tas.js
@@ -22,11 +22,21 @@ const taInfo = [
     name: 'Vaishnavi Rangarajan',
     email: 'rangarav@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: 'Tu',
-      //   time: '2:00 &ndash; 3:00pm',
-      //   location: 'JOHN 123'
-      // }
+      {
+        day: 'Th',
+        time: '10:30 &ndash; 11:00am',
+        location: 'JOHN 121'
+      },
+      {
+        day: 'F',
+        time: '11:00am &ndash; 12:00pm',
+        location: 'JOHN 125'
+      },
+      {
+        day: 'F',
+        time: '4:30 &ndash; 6:00pm',
+        location: 'Covell 140, Table 2'
+      }
     ],
     gradingHours: [
       // {
