415136fb2a6e8af4e4316f43e8d8a1c80635c969
Rob Hess <robwhess@gmail.com>
2019-10-01 10:20:48 -0700

    Update CS 261 TA hours.

--- src/data/courses/f19/cs261/tas.js
+++ src/data/courses/f19/cs261/tas.js
@@ -40,7 +40,7 @@ const taInfo = [
         location: 'Covell 140'
       },
       {
-        day: '',
+        day: 'W',
         time: '2:00 &ndash; 3:30pm',
         location: 'Covell 140'
       }
@@ -49,14 +49,83 @@ const taInfo = [
   {
     name: 'Jianlong Huang',
     email: 'huangj2@oregonstate.edu',
-    officeHours: [],
-    gradingHours: []
+    officeHours: [
+      {
+        day: 'Tu',
+        time: '2:00 &ndash; 4:30pm',
+        location: 'DEAR 119'
+      },
+      {
+        day: 'Th',
+        time: '2:00 &ndash; 4:30pm',
+        location: 'DEAR 119'
+      },
+      {
+        day: 'F',
+        time: '2:00 &ndash; 4:30pm',
+        location: 'DEAR 119'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'Tu',
+        time: '4:30 &ndash; 5:30pm',
+        location: 'DEAR 119'
+      },
+      {
+        day: 'Th',
+        time: '4:30 &ndash; 5:30pm',
+        location: 'DEAR 119'
+      },
+      {
+        day: 'F',
+        time: '4:30 &ndash; 6:00pm',
+        location: 'DEAR 119'
+      }
+    ]
   },
   {
     name: 'Ryan Kennedy',
     email: 'kennerya@oregonstate.edu',
-     officeHours: [{day: "T", time: "10:00am - 10:30am", location: "Johnson 123"}, {day: "T", time: "10:30am - 11:30am", location: "Johnson 125"}, {day: "W", time: "9:00am - 11:00am", location: "Johnson 123"}, {day: "Th", time: "9:00am - 11:30am", location: "Johnson 123"}, {day: "F", time: "12:00pm - 3:00pm", location: "Johnson 121"}],
-    gradingHours:[{day: "M", time: "9:00am - 12:00pm", location: "Johnson 123"}, {day: "T", time: "9:00am - 9:30am", location: "Johnson 123"}]
+     officeHours: [
+       {
+         day: "T",
+         time: "10:00am - 10:30am",
+         location: "JOHN 123"
+       },
+       {
+         day: "T",
+         time: "10:30am - 11:30am",
+         location: "JOHN 125"
+       },
+       {
+         day: "W",
+         time: "9:00am - 11:00am",
+         location: "JOHN 123"
+       },
+       {
+         day: "Th",
+         time: "9:00am - 11:30am",
+         location: "JOHN 123"
+       },
+       {
+         day: "F",
+         time: "12:00pm - 3:00pm",
+         location: "JOHN 121"
+       }
+     ],
+    gradingHours:[
+      {
+        day: "M",
+        time: "9:00am - 12:00pm",
+        location: "JOHN 123"
+      },
+      {
+        day: "T",
+        time: "9:00am - 9:30am",
+        location: "JOHN 123"
+      }
+    ]
   },
   {
     name: 'Seifeddine Mejri',
@@ -113,6 +182,18 @@ const taInfo = [
     email: 'wanghuah@oregonstate.edu',
     officeHours: [
       {
+        day: 'W',
+        time: '9:00 &ndash; 10:00am',
+        location: 'JOHN 121'
+      },
+      {
+        day: 'F',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'JOHN 121'
+      }
+    ],
+    gradingHours: [
+      {
         day: 'M',
         time: '9:00 &ndash; 11:00am',
         location: 'DEAR 119'
@@ -125,18 +206,6 @@ const taInfo = [
       {
         day: 'M',
         time: '5:00 &ndash; 5:30pm',
-        location: 'JOHN 121'
-      }
-    ],
-    gradingHours: [
-      {
-        day: 'W',
-        time: '9:00 &ndash; 10:00am',
-        location: 'JOHN 121'
-      },
-      {
-        day: 'F',
-        time: '4:00 &ndash; 6:00pm',
         location: 'JOHN 121'
       }
     ]
