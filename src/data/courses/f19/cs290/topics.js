dc4227429d99d564dbefbe33a6ec63b88922a98b
Rob Hess <robwhess@gmail.com>
2019-10-21 14:06:41 -0700

    Update CS 290 topics.

--- src/data/courses/f19/cs290/topics.js
+++ src/data/courses/f19/cs290/topics.js
@@ -92,16 +92,18 @@ const topics = [
 
   {
     title: 'JavaScript Fundamentals',
+    isCurrent: true,
     weeks: [ 4 ],
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1C5fgsWQjTaPDOyqjecMZ2d554iOePCrDcZ5nzUykG_U/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-sp19/js-fundamentals'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-f19/js-fundamentals',
+        description: 'code written in lecture'
+      }
     ],
     readings: [
       {
