59bcd28c1e34567fe0f0d5a585589e0bbdc86eb3
Rob Hess <robwhess@gmail.com>
2019-12-02 10:19:11 -0800

    Post final project demo signup link to CS 290.

--- src/data/courses/f19/cs290/finalProject.js
+++ src/data/courses/f19/cs290/finalProject.js
@@ -12,17 +12,17 @@ const finalProject = {
         'Due **ON CANVAS** by 5:00pm, Thursday 12/12/2019'
       ]
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Final project demos will be during finals week in KEC 1109.',
-    //     'Sign up for a final project demo using the link above.',
-    //     'Sign up for one slot per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub when when at the time of your demo.'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Final project demos will be during finals week in KEC 1109.',
+        'Sign up for a final project demo using the link above.',
+        'Sign up for one slot per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub when when at the time of your demo.'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
