67f4e82402da6e95a4efef0f1bfa0273023205df
Rob Hess <robwhess@gmail.com>
2020-01-01 16:21:59 -0800

    Add some TA hours.

--- src/data/courses/w20/cs492/tas.js
+++ src/data/courses/w20/cs492/tas.js
@@ -23,16 +23,33 @@ const tas = [
     email: "bhuvab@oregonstate.edu",
     officeHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'TBD'
+        day: 'Mon.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'JOHN 125'
+      },
+      {
+        day: 'Fri.',
+        time: '10:00 &ndash; 11:00am',
+        location: 'JOHN 121'
       }
     ],
     gradingHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'TBD',
+        day: 'Mon.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'JOHN 121',
+        link: '#'
+      },
+      {
+        day: 'Wed.',
+        time: '10:00am &ndash; 12:00pm',
+        location: 'JOHN 123',
+        link: '#'
+      },
+      {
+        day: 'Fri.',
+        time: '11:00am &ndash; 1:00pm',
+        location: 'JOHN 123',
         link: '#'
       }
     ]
@@ -42,9 +59,14 @@ const tas = [
     email: "rangarav@oregonstate.edu",
     officeHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'TBD'
+        day: 'Tues.',
+        time: '12:00 &ndash; 1:00pm',
+        location: 'JOHN 125'
+      },
+      {
+        day: 'Fri.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'JOHN 125'
       }
     ],
     gradingHours: [
