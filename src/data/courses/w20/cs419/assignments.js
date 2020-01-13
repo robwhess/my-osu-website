bc05cfe6c06722ec3d56fca4f685fe02d731e878
Rob Hess <robwhess@gmail.com>
2020-01-13 12:31:47 -0800

    Post CS 419 assignments 0 and 1.

--- src/data/courses/w20/cs419/assignments.js
+++ src/data/courses/w20/cs419/assignments.js
@@ -1,17 +1,28 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: '',
-    //   due: {
-    //     day: '',
-    //     time: ''
-    //   },
-    //   notes: [
-    //     ''
-    //   ]
-    // }
+    {
+      link : 'https://docs.google.com/document/d/1ZTa93Vr9PyRvKatnIuaDRY3b8YNOfR1hLx2e4eq81wg/edit?usp=sharing',
+      title: 'Assignment 0 &ndash; Sign up for piazza',
+      due: {
+        day: 'Monday, 1/20/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Piazza signup will be checked off when demoing assignment 1'
+      ]
+    },
+    {
+      link : 'https://docs.google.com/document/d/1s0lwLzS7Fyqyvu2ZziQwMn1GEV_s2QGWFY1mK36KVsY/edit?usp=sharing',
+      title: 'Assignment 1 &ndash; A simple photo app',
+      due: {
+        day: 'Monday, 1/20/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 2/17/2020'
+      ]
+    },
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
