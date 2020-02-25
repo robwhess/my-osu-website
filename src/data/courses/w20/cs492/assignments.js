1f3f6256726894111796f3f3431b63e41e63e833
Rob Hess <robwhess@gmail.com>
2020-02-25 13:03:06 -0800

    Post CS 492 assignment 4.

--- src/data/courses/w20/cs492/assignments.js
+++ src/data/courses/w20/cs492/assignments.js
@@ -34,17 +34,17 @@ const assignments = {
         'Demo due by 11:59pm, Monday 3/9/2020'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; A weather app with SQLite and a navigation drawer',
-    //   due: {
-    //     day: 'Monday, 3/11/2019',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Friday 3/22/2019'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/msq5HwqS',
+      title: 'Assignment 4 &ndash; A weather app with SQLite and a navigation drawer',
+      due: {
+        day: 'Monday, 3/9/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Friday 3/20/2020'
+      ]
+    }
   ],
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
 };
