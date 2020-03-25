015ba2b82fac2613978209291e08aac78ca3d71a
Rob Hess <robwhess@gmail.com>
2020-03-24 17:16:44 -0700

    Support slugs for CollapsibleSections.  Refactor CourseTopicsList and CollapsibleSection.

--- src/components/CollapsibleSection.js
+++ src/components/CollapsibleSection.js
@@ -2,103 +2,119 @@
  * This file contains a component representing a collabsible section.
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDoubleRight, faAngleDown, faLink } from '@fortawesome/free-solid-svg-icons';
+import slugify from 'slugify';
 
 const CollapsibleSectionContainer = styled.div`
   margin-top: 20px;
 `;
 
-const CollapsibleSectionHeader = styled.button`
+const Header = styled.div`
   display: table-row;
-  padding: 0;
-  border: none;
-  text-align: left;
   font-size: 28px;
-  color: inherit;
-  background-color: inherit;
-  cursor: pointer;
 `;
 
-const CollapsibleSectionHeaderItem = styled.span`
+const HeaderItem = styled.span`
   display: table-cell;
+  vertical-align: top;
   padding: 0 2px;
 `;
 
-const CollapsibleSectionToggleIndicator = styled.span`
+const SlugLink = styled.a`
+  color: inherit;
+  font-size: 18px;
+`;
+
+const TitleButton = styled.button`
+  font-size: inherit;
+  padding: 0;
+  border: 0;
+  color: inherit;
+  background: inherit;
+  cursor: pointer;
+  text-align: left;
+  h3 {
+    display: inline;
+    font-size: inherit;
+    color: inherit;
+    text-align: left;
+  }
+`;
+
+const ToggleIndicator = styled.span`
   display: inline-block;
-  margin-left: 5px;
+  margin-left: 3px;
+  font-size: 22px;
   transform: ${props => props.collapsed ? 'rotate(-180deg)' : 'none'};
   transition: transform 150ms ease;
 `;
 
-const CollapsibleSectionChildrenContainer = styled.div`
+const ChildrenContainer = styled.div`
   height: ${props => props.collapsed ? 0 : 'auto'};
   overflow: hidden;
   padding-left: 35px;
 `;
 
-class CollapsibleSection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapsed: !!props.collapsed
-    };
-    this.toggleCollapsed = this.toggleCollapsed.bind(this);
-  }
+function CollapsibleSection({ title, children, collapsed: initCollapsed, setSlug }) {
+  const { hash } = useLocation();
+  const slug = slugify(title, { strict: true });
+  const [ collapsed, setCollapsed ] = useState(!!initCollapsed);
 
-  toggleCollapsed() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  /*
-   * Since this component is re-used across pages, we need to make sure the current
-   * state is synchronized with the incoming props before it is re-rendered.
-   */
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.collapsed !== prevProps.collapsed || this.props.collapsed !== prevState.collapsed) {
-      this.setState({
-        collapsed: this.props.collapsed
-      });
+  useEffect(() => {
+    if (setSlug && slug === hash.replace(/^#/, '')) {
+      setCollapsed(false);
     }
+  }, [ hash, setSlug, slug ]);
+
+  function toggleCollapsed() {
+    setCollapsed(prevCollapsed => !prevCollapsed);
   }
 
-  render() {
-    return (
-      <CollapsibleSectionContainer>
-        <CollapsibleSectionHeader onClick={this.toggleCollapsed}>
-          <CollapsibleSectionHeaderItem>
-            <FontAwesomeIcon icon={faAngleDoubleRight} />
-          </CollapsibleSectionHeaderItem>
-          <CollapsibleSectionHeaderItem>
-            {this.props.title}
+  return (
+    <CollapsibleSectionContainer>
+      <Header>
+        <HeaderItem>
+          <FontAwesomeIcon icon={faAngleDoubleRight} />
+        </HeaderItem>
+        {slug &&
+          <HeaderItem>
+            <SlugLink href={`#${slug}`}>
+              <FontAwesomeIcon icon={faLink} />
+            </SlugLink>
+          </HeaderItem>
+        }
+        <HeaderItem>
+          <TitleButton onClick={toggleCollapsed}>
+            <h3 id={slug}>{title}</h3>
             &nbsp;
-            <CollapsibleSectionToggleIndicator collapsed={this.state.collapsed}>
+            <ToggleIndicator collapsed={collapsed}>
               <FontAwesomeIcon icon={faAngleDown} />
-            </CollapsibleSectionToggleIndicator>
-          </CollapsibleSectionHeaderItem>
-        </CollapsibleSectionHeader>
-        <CollapsibleSectionChildrenContainer collapsed={this.state.collapsed}>
-          {this.state.collapsed ? null : this.props.children}
-        </CollapsibleSectionChildrenContainer>
-      </CollapsibleSectionContainer>
-    );
-  }
+            </ToggleIndicator>
+          </TitleButton>
+        </HeaderItem>
+      </Header>
+      <ChildrenContainer collapsed={collapsed}>
+        {collapsed || children}
+      </ChildrenContainer>
+    </CollapsibleSectionContainer>
+  );
 }
 
 CollapsibleSection.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-  collapsed: PropTypes.bool
+  collapsed: PropTypes.bool,
+  setSlug: PropTypes.bool
 };
 
 CollapsibleSection.defaultProps = {
-  collapsed: false
+  collapsed: false,
+  setSlug: false
 };
 
 export default CollapsibleSection;
