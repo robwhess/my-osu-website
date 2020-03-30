06711b0815009cbf556ef2b21eebdf12ecc55c97
Rob Hess <robwhess@gmail.com>
2020-03-30 16:31:57 -0700

    Update office hours.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,14 +2,14 @@ const officeHours = [
   {
     hours: [
       {
-        day: 'Tues.',
-        time: '11:00am&ndash;12:00pm',
+        day: 'Thurs.',
+        time: '10:00am&ndash;12:00pm',
         location: 'online via Zoom &ndash; click for details',
         details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/589990808\n\nPhone Dial-In Information\n+1 971 247 1195 US (Portland)\n+1 253 215 8782 US\n+1 301 715 8592 US\n\nMeeting ID: 589 990 808\n\nJoin by Polycom/Cisco/Other Room System\n589990808@zoomcrc.com'
       },
       {
-        day: 'Tues.',
-        time: '2:00&ndash;4:00pm',
+        day: 'Thurs.',
+        time: '2:00&ndash;3:00pm',
         location: 'online via Zoom &ndash; click for details',
         details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/589990808\n\nPhone Dial-In Information\n+1 971 247 1195 US (Portland)\n+1 253 215 8782 US\n+1 301 715 8592 US\n\nMeeting ID: 589 990 808\n\nJoin by Polycom/Cisco/Other Room System\n589990808@zoomcrc.com'
       }
