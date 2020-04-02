45f571f2f37d432f94db22dfc373f6c3bae78fd8
Rob Hess <robwhess@gmail.com>
2020-04-01 17:12:51 -0700

    Update CS 480 TA hours.

--- src/data/courses/sp20/cs480/tas.js
+++ src/data/courses/sp20/cs480/tas.js
@@ -4,16 +4,36 @@ const taInfo = [
     email: 'rangarav@oregonstate.edu',
     officeHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'online via Zoom &ndash; details TBD'
+        day: 'Tues.',
+        time: '8:00 &ndash; 9:00am',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/710134679'
+      },
+      {
+        day: 'Thurs.',
+        time: '8:00 &ndash; 9:00am',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/710134679'
+      },
+      {
+        day: 'Fri.',
+        time: '8:00 &ndash; 9:00am',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/710134679'
       }
     ],
     gradingHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'online via Zoom &ndash; details TBD'
+        day: 'Wed.',
+        time: '9:00 &ndash; 10:00am',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/146383748'
+      },
+      {
+        day: 'Thurs.',
+        time: '2:00 &ndash; 3:00pm',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/855540252'
       }
     ]
   },
@@ -22,16 +42,24 @@ const taInfo = [
     email: 'shindea@oregonstate.edu',
     officeHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'online via Zoom &ndash; details TBD'
+        day: 'Tues.',
+        time: '2:00 &ndash; 5:00pm',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/7621923704'
+      },
+      {
+        day: 'Thurs.',
+        time: '9:00am &ndash; 12:00pm',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/7621923704'
       }
     ],
     gradingHours: [
       {
-        day: 'TBD',
-        time: 'TBD',
-        location: 'online via Zoom &ndash; details TBD'
+        day: 'Tues.',
+        time: '9:00am &ndash; 1:00pm',
+        location: 'online via Zoom &ndash; click for details',
+        details: 'Join Zoom Meeting\nhttps://oregonstate.zoom.us/j/7621923704'
       }
     ]
   }
