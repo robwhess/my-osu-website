79b4726c48f06f267f31176dee45a05b79fa3878
Rob Hess <robwhess@gmail.com>
2020-04-09 14:53:52 -0700

    Post CS 480 assignment 1.

--- src/data/courses/sp20/cs480/assignments.js
+++ src/data/courses/sp20/cs480/assignments.js
@@ -1,17 +1,17 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; Scanning with flex',
-    //   due: {
-    //     day: 'Monday, 4/22/2019',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Monday 5/6/2019'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/cpVmnHJo',
+      title: 'Assignment 1 &ndash; Scanning with flex',
+      due: {
+        day: 'Monday, 4/27/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 5/11/2020'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 2 &ndash;Parsing and syntax-directed translation',
