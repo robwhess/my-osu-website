1d7b9b5c8d7827bdeb8f5c2e0b226e7e54aa0537
Rob Hess <robwhess@gmail.com>
2020-05-04 09:01:42 -0700

    Update CS 290 topics.

--- src/data/courses/sp20/cs290/topics.js
+++ src/data/courses/sp20/cs290/topics.js
@@ -180,11 +180,11 @@ const topics = [
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1KCjkNZZlYDHGcahfJQzyTsCSr21kGkXZJjOkmTJEVd4/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-f19/js-events',
-      //   description: 'code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-sp20/js-events',
+        description: 'code written in lecture'
+      },
       {
         title: 'JS Fiddle DOM manipulation example',
         link: 'https://jsfiddle.net/robwhess/kmo8ksm4/'
