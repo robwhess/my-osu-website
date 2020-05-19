3560df12122f4fc8dd569e9e8b0581cc7bfaf991
Rob Hess <robwhess@gmail.com>
2020-05-19 14:49:51 -0700

    Post CS 493 assignment 4.

--- src/data/courses/sp20/cs493/assignments.js
+++ src/data/courses/sp20/cs493/assignments.js
@@ -34,17 +34,17 @@ const assignments = {
         'Demo due by 11:59pm on Monday, 6/1/2020'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; File uploads and offline work',
-    //   due: {
-    //     day: 'Monday, 6/3/2019',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 5:00pm on Friday, 6/14/2019'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/gDZYZ6dZ',
+      title: 'Assignment 4 &ndash; File uploads and offline work',
+      due: {
+        day: 'Monday, 6/1/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 5:00pm on Friday, 6/12/2019'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
