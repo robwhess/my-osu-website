4e4b1ec1e1ef3393089199d4d67d08f63f8c2477
Rob Hess <robwhess@gmail.com>
2020-05-28 17:10:40 -0700

    Post final project info for CS 290 and CS 493.

--- src/data/courses/sp20/cs290/finalProject.js
+++ src/data/courses/sp20/cs290/finalProject.js
@@ -12,18 +12,18 @@ const finalProject = {
         'Due **ON CANVAS** by 5:00pm, Friday 6/12/2020'
       ]
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Final project demos will be during finals week in KEC 1109.',
-    //     'Sign up for a final project demo appointment using the link above.',
-    //     'Sign up for one demo appointment per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub when when at the time of your demo.',
-    //     'Plan on using one of your team\'s laptops for the demo.'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Sign up for a final project demo appointment using the link above.',
+        'Sign up for one demo appointment per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub when when at the time of your demo.',
+        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808](https://oregonstate.zoom.us/j/589990808).'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
--- src/data/courses/sp20/cs493/finalProject.js
+++ src/data/courses/sp20/cs493/finalProject.js
@@ -12,18 +12,19 @@ const finalProject = {
         'Due **ON CANVAS** by 5:00pm, Friday 6/12/2020'
       ]
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUdVZU15RFNoakRJfGRlZmF1bHR8ZmUxNjU2MDI5MTY2YjdiY2YyNzk0YjQ3OThlODc2ZDM',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Final project demos will be during finals week in KEC 1109.',
-    //     'Sign up for a final project demo using the link above.',
-    //     'Sign up for one slot per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub when when at the time of your demo.',
-    //     'Architecture/design document is due on Canvas at the time of your demo.'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Sign up for a final project demo appointment using the link above.',
+        'Sign up for one demo appointment per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub when when at the time of your demo.',
+        'Architecture/design document is due on Canvas at the time of your demo.',
+        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808](https://oregonstate.zoom.us/j/589990808).'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete RESTful API.  You can find more info below.'
