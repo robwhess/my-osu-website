18ca5b13d7b6d1669ee683447cedcabca05a4741
Rob Hess <robwhess@gmail.com>
2020-09-29 10:59:06 -0700

    Post CS 290 assignment 1.

--- src/data/courses/f20/cs290/assignments.js
+++ src/data/courses/f20/cs290/assignments.js
@@ -1,18 +1,18 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; HTML and web publishing',
-    //   due: {
-    //     day: 'Wednesday, 4/8/2020',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog due **ON CANVAS** by assignment due date',
-    //     'Demo due by 11:59pm on Monday, 4/27/2020'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/qi-RzzbW',
+      title: 'Assignment 1 &ndash; HTML and web publishing',
+      due: {
+        day: 'Monday, 10/12/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Code Blog due **ON CANVAS** by assignment due date',
+        'Demo due by 11:59pm on Monday, 10/26/2020'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 2 &ndash; Styling a page with CSS',
