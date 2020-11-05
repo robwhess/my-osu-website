f44370cc789e84cded8f4ac14f3c7530b6d02da2
Rob Hess <robwhess@gmail.com>
2020-11-05 13:09:49 -0800

    Publish CS 290 final project info.

--- src/data/courses/f20/cs290/index.js
+++ src/data/courses/f20/cs290/index.js
@@ -1,6 +1,6 @@
 import topics from './topics';
 import assignments from './assignments';
-// import finalProject from './finalProject';
+import finalProject from './finalProject';
 import tas from './tas';
 
 var courseDetails = {
@@ -48,7 +48,7 @@ var courseDetails = {
 
   topics: topics,
 
-  assignmentGroups: [ assignments ],
+  assignmentGroups: [ assignments, finalProject ],
 
   tas: tas
 };
