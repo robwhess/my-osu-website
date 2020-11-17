e1b6d9779c7f79024f07562343db2a992bbc765a
Rob Hess <robwhess@gmail.com>
2020-11-17 13:11:42 -0800

    Post CS 290 assignments 4 and 5.

--- src/data/courses/f20/cs290/assignments.js
+++ src/data/courses/f20/cs290/assignments.js
@@ -37,30 +37,29 @@ const assignments = {
         'Demo due by 11:59pm on Monday, ~~11/23/2020~~ **11/30/2020**'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; Native Node.js file serving',
-    //   due: {
-    //     day: 'Monday, 5/18/2020',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog due **ON CANVAS** by assignment due date',
-    //     '**No demo for this assignment**'
-    //   ]
-    // },
-    // {
-    //   link : '',
-    //   title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
-    //   due: {
-    //     day: '~~Monday, 6/1/2020~~ **Wednesday, 6/3/2020**',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog due **ON CANVAS** by assignment due date',
-    //     '~~Demo due by 5:00pm on Friday, 6/12/2020~~ **No demo for this assignment**'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/zWDm9lMm',
+      title: 'Assignment 4 &ndash; Native Node.js file serving',
+      due: {
+        day: '**ASSIGNMENT OPTIONAL; NO DUE DATE**'
+      },
+      notes: [
+        '**This assignment is completely optional, and you won\'t receive credit for completing it.**  Thus, there will be no grading demos for this assignment.',
+        'Since the assignment is optional, you don\'t need to write a code blog entry for it.'
+      ]
+    },
+    {
+      link : 'https://classroom.github.com/a/sLOraoPI',
+      title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
+      due: {
+        day: 'Wednesday, 12/2/2020',
+        time: '11:59pm'
+      },
+      notes: [
+        'Code Blog due **ON CANVAS** by assignment due date',
+        '**No demo for this assignment.**  It will be graded offline.'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
