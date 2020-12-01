2ee8c766c537234e2a2f0903e3687de98b5d48d9
Rob Hess <robwhess@gmail.com>
2020-12-01 13:42:21 -0800

    Post final project demo signup link to CS 290.

--- src/data/courses/f20/cs290/finalProject.js
+++ src/data/courses/f20/cs290/finalProject.js
@@ -29,18 +29,18 @@ const finalProject = {
         'Due **ON CANVAS** by 5:00pm (US/Pacific), Friday 12/11/2020'
       ]
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Sign up for a final project demo appointment using the link above.',
-    //     'Sign up for one demo appointment per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub when when at the time of your demo.',
-    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808](https://oregonstate.zoom.us/j/589990808).'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Sign up for a final project demo appointment using the link above.',
+        'Sign up for one demo appointment per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub when when at the time of your demo.',
+        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
