9841ccf58fdb751850d420b81bd3bc6ce8c75eed
Rob Hess <robwhess@gmail.com>
2020-12-28 13:58:07 -0800

    Update office hours.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,14 +2,14 @@ const officeHours = [
   {
     hours: [
       {
-        day: 'Wed.',
-        time: '2:00&ndash;3:00pm',
+        day: 'Tues.',
+        time: '10:00am&ndash;12:00pm',
         timeZone: 'US/Pacific',
         videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       },
       {
-        day: 'Thurs.',
-        time: '10:00am&ndash;12:00pm',
+        day: 'Wed.',
+        time: '11:00am&ndash;12:00pm',
         timeZone: 'US/Pacific',
         videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       }
