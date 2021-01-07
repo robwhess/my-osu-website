76f07cecdfb6bc9381b4de41aa185047f08c01d4
Rob Hess <robwhess@gmail.com>
2021-01-07 13:57:26 -0800

    Post Satyajit's hours to CS 492.

--- src/data/courses/w21/cs492/tas.js
+++ src/data/courses/w21/cs492/tas.js
@@ -47,21 +47,28 @@ const tas = [
     name: "Satyajit Kamble",
     email: "kambles@oregonstate.edu",
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '10:00am &ndash; 12:00pm',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/95640884692?pwd=cWw2SnI4YThXMlhLYkJVOWc1TXlTdz09'
-      // }
+      {
+        day: 'Wed.',
+        time: '8:30am &ndash; 12:30pm',
+        timeZone: 'US/Pacific',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/93174293710?pwd=TW05QnE1empzbndNYUJGcXhJK1Yzdz09'
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '9:00am &ndash; 12:00pm',
-      //   timeZone: 'US/Pacific',
-      //   appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UU5BVHkxMDVaTV9XfGRlZmF1bHR8NDRmMzM0MTlkMThlYmFjY2RmNDdmZTBmN2Q3ZGJlZGM',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/97740017802?pwd=bFdEUlNaamFNYVVMa2VnbjJtaGJPQT09'
-      // }
+      {
+        day: 'Mon.',
+        time: '4:00 &ndash; 7:00pm',
+        timeZone: 'US/Pacific',
+        // appointmentsLink: '',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99677315364?pwd=OTl5VmN6Z3N0Sm42QTVhSHVTekJYZz09'
+      },
+      {
+        day: 'Wed.',
+        time: '4:00 &ndash; 7:00pm',
+        timeZone: 'US/Pacific',
+        // appointmentsLink: '',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99677315364?pwd=OTl5VmN6Z3N0Sm42QTVhSHVTekJYZz09'
+      }
     ]
   }
 ];
