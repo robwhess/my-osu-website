e838ea2c46d0b13a2d1ea3c15221caae4f1d654c
Rob Hess <robwhess@gmail.com>
2021-01-15 16:53:26 -0800

    Add Saurabhkumar's grading appointments link to CS 499.

--- src/data/courses/w21/cs499/tas.js
+++ src/data/courses/w21/cs499/tas.js
@@ -21,28 +21,28 @@ const tas = [
         day: 'Mon.',
         time: '4:00 &ndash; 5:00pm',
         timeZone: 'US/Pacific',
-        // appointmentsLink: '',
+        appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UU8tZVJWQWdjVElVfGRlZmF1bHR8MjljYzc0NDQ5ZWQ2ZTk3NmNkZmFiOTc1M2NjZWM5YjE',
         // videoConferenceLink: ''
       },
       {
         day: 'Mon.',
         time: '6:30 &ndash; 7:30pm',
         timeZone: 'US/Pacific',
-        // appointmentsLink: '',
+        appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UU8tZVJWQWdjVElVfGRlZmF1bHR8MjljYzc0NDQ5ZWQ2ZTk3NmNkZmFiOTc1M2NjZWM5YjE',
         // videoConferenceLink: ''
       },
       {
         day: 'Wed.',
         time: '11:30am &ndash; 1:30pm',
         timeZone: 'US/Pacific',
-        // appointmentsLink: '',
+        appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UU8tZVJWQWdjVElVfGRlZmF1bHR8MjljYzc0NDQ5ZWQ2ZTk3NmNkZmFiOTc1M2NjZWM5YjE',
         // videoConferenceLink: ''
       },
       {
         day: 'Wed.',
         time: '4:00 &ndash; 5:00pm',
         timeZone: 'US/Pacific',
-        // appointmentsLink: '',
+        appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UU8tZVJWQWdjVElVfGRlZmF1bHR8MjljYzc0NDQ5ZWQ2ZTk3NmNkZmFiOTc1M2NjZWM5YjE',
         // videoConferenceLink: ''
       }
     ]
