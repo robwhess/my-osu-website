784af8083d5dd093c89f70ff26fca26c3c18cda7
Rob Hess <robwhess@gmail.com>
2021-02-08 13:55:45 -0800

    Update CS 499 topics.

--- src/data/courses/w21/cs499/topics.js
+++ src/data/courses/w21/cs499/topics.js
@@ -205,7 +205,6 @@ const topics = [
   {
     title: 'Communicating with an HTTP API',
     weeks: [ 5 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -249,16 +248,17 @@ const topics = [
   {
     title: 'Styling Components with Emotion',
     weeks: [ 6 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1XgCyJ3XA7lzpaT7HN5fNY65RuqN_j6vaPrWbxq0Vjso/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs419-w20/using-emotion',
-      //   description: 'GitHub repo for code written in lecture'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs499-w21/styling-with-emotion',
+        description: 'GitHub repo for code written in lecture'
+      }
     ],
     readings: [
       {
