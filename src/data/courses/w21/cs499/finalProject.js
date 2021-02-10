5e4a4a3a5ad3c6ca1785df5eb26a679542bfcb5f
Rob Hess <robwhess@gmail.com>
2021-02-10 13:42:04 -0800

    Post CS 499 final project info.

--- src/data/courses/w21/cs499/finalProject.js
+++ src/data/courses/w21/cs499/finalProject.js
@@ -2,44 +2,55 @@ const finalProject = {
   title: 'Final Project',
   assignments: [
     {
-      title: 'Final Project Proposal',
-      link: 'https://docs.google.com/document/d/19uNa7F2Dv8UVgETohwKeHsc8HHww1UaSYiaZzVXtx04/edit?usp=sharing',
+      title: 'Final Project Details',
+      link: 'https://docs.google.com/document/d/1NybjgqevMvJNx012OiZnEYUYMQrDyFNoNDHd2aLSwVs/edit?usp=sharing',
+      due: {
+        time: '5:00pm',
+        day: 'Friday, 3/19/2021'
+      }
+    },
+    {
+      title: 'Deliverable #1: Final Project Proposal',
+      link: 'https://docs.google.com/document/d/17WjO9lM0_52warRKzHgyYEPNLkgO4JJfnw9aFnZu5vE/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Thursday, 2/20/2020',
+        day: 'Monday, 2/22/2020',
         location: 'on Canvas'
       }
     },
     {
-      title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/18AfRdg7-gaL7xbn7oszkrddeOp5LPEee0AldiAGFeAM/edit?usp=sharing',
+      title: 'Deliverable #2: Code',
+      link: 'https://classroom.github.com/g/W6m2so5t',
       due: {
-        time: '5:00pm',
-        day: 'Friday, 3/20/2020'
-      }
-    },
-    {
-      link: 'https://docs.google.com/document/d/1jv4e3MxoiIt5PVYlOcXMFTV7ZrviSrue6-3TXz6KEp0/edit?usp=sharing',
-      title: 'Final Project Team Evaluation',
-      due: {
-        time: '5:00pm',
-        day: 'Friday, 3/20/2020',
-        location: 'on Canvas'
-      }
-    },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-      title: 'Final Project Demo Signup',
+        time: 'at the time of your grading demo',
+        day: 'during finals week'
+      },
       notes: [
-        'Final project demos will be during finals week in KEC 1109.',
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub when when at the time of your demo.',
-        'Plan on using one of your team\'s laptops for the demo.',
-        '**All final project demos will be held remotely via Zoom.**  The Zoom meeting number is 321-397-494, and the meeting link is: [https://oregonstate.zoom.us/j/321397494](https://oregonstate.zoom.us/j/321397494).  Please check your email or Canvas for more details.'
+        'Use the link above to create a repo on GitHub for your team to use for the final project.'
       ]
-    }
+    },
+    {
+      title: 'Deliverable #3: Final Project Team Evaluation',
+      link: 'https://docs.google.com/document/d/1viOuF-vZOMH8WJhBRwmtqVLeJfb6aGGrz9dHb5Y06t8/edit?usp=sharing',
+      due: {
+        time: '5:00pm',
+        day: 'Friday, 3/19/2020',
+        location: 'on Canvas'
+      }
+    },
+    // {
+    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Final project demos will be during finals week in KEC 1109.',
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub when when at the time of your demo.',
+    //     'Plan on using one of your team\'s laptops for the demo.',
+    //     '**All final project demos will be held remotely via Zoom.**  The Zoom meeting number is 321-397-494, and the meeting link is: [https://oregonstate.zoom.us/j/321397494](https://oregonstate.zoom.us/j/321397494).  Please check your email or Canvas for more details.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete React-based web application that serves dynamic, interactive data from an API.  You can find more info below.'
