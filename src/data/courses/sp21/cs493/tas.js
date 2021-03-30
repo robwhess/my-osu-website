17397cb1c3bee3895f691c1f5a26bf92467164a0
Rob Hess <hessro@oregonstate.edu>
2021-03-30 12:28:37 -0700

    Update CS 493 TA info.

--- src/data/courses/sp21/cs493/tas.js
+++ src/data/courses/sp21/cs493/tas.js
@@ -3,21 +3,28 @@ const taInfo = [
     name: 'Satyajit Kamble',
     email: 'kambles@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '10:00am &ndash; 12:00pm',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/95640884692?pwd=cWw2SnI4YThXMlhLYkJVOWc1TXlTdz09'
-      // },
+      {
+        day: 'Tues.',
+        time: '8:30 &ndash; 10:30am',
+        timeZone: 'US/Pacific',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/96082811676?pwd=byt4c1pybTViU0RuQTVhU0tvTDlJUT09'
+      },
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   timeZone: 'US/Pacific',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Mon',
+        time: '4:00 &ndash; 6:00pm',
+        timeZone: 'US/Pacific',
+        appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU1zTWpxTnMza1owfGRlZmF1bHR8MzI4NmQ3MDhiNWRjZjRlY2FmOTQ1MzhjYTQ3YjFkMzI',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99847221358?pwd=K1dkckN5QzB1WnM2MFFneWk4WDlEUT09'
+      },
+      {
+        day: 'Wed',
+        time: '4:00 &ndash; 6:00pm',
+        timeZone: 'US/Pacific',
+        appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU1zTWpxTnMza1owfGRlZmF1bHR8MzI4NmQ3MDhiNWRjZjRlY2FmOTQ1MzhjYTQ3YjFkMzI',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99847221358?pwd=K1dkckN5QzB1WnM2MFFneWk4WDlEUT09'
+      },
     ]
   },
   {
