4d3c544f18fea5604d2e964cfebf999b7f600e25
Rob Hess <hessro@oregonstate.edu>
2021-04-09 15:44:44 -0700

    Update Brian's links for CS 480.

--- src/data/courses/sp21/cs480/tas.js
+++ src/data/courses/sp21/cs480/tas.js
@@ -7,13 +7,13 @@ const taInfo = [
         day: 'Tues.',
         time: '1:00 &ndash; 2:30pm',
         timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/97643791988?pwd=NVZZRzZTZjl2SHBGbjIvVVhVTXlBQT09'
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92719333592?pwd=TDV5MlQ0b2kxZFZlQWVOVkMvdzFhUT09'
       },
       {
         day: 'Wed.',
         time: '3:30 &ndash; 5:00pm',
         timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/97643791988?pwd=NVZZRzZTZjl2SHBGbjIvVVhVTXlBQT09'
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92719333592?pwd=TDV5MlQ0b2kxZFZlQWVOVkMvdzFhUT09'
       }
     ],
     gradingHours: [
@@ -21,21 +21,21 @@ const taInfo = [
         day: 'Wed.',
         time: '8:00 &ndash; 9:30am',
         timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/91420899163?pwd=LzNQYUFaa0tsY2hNL3VZSjRKNDVYZz09',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/97317942095?pwd=em9SRzE4d09QLy9yclpCR0kyano2UT09',
         appointmentsLink: ''
       },
       {
         day: 'Thurs.',
         time: '8:00 &ndash; 11:30am',
         timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/91420899163?pwd=LzNQYUFaa0tsY2hNL3VZSjRKNDVYZz09',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/97317942095?pwd=em9SRzE4d09QLy9yclpCR0kyano2UT09',
         appointmentsLink: ''
       },
       {
         day: 'Fri.',
         time: '8:00 &ndash; 9:30am',
         timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/91420899163?pwd=LzNQYUFaa0tsY2hNL3VZSjRKNDVYZz09',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/97317942095?pwd=em9SRzE4d09QLy9yclpCR0kyano2UT09',
         appointmentsLink: ''
       },
     ]
