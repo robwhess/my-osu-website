75ab47e713584500e905d765ba0e166955117fc4
Rob Hess <hessro@oregonstate.edu>
2021-05-04 13:39:39 -0700

    Publish final project info for CS 290.

--- src/data/courses/sp21/cs290/finalProject.js
+++ src/data/courses/sp21/cs290/finalProject.js
@@ -2,45 +2,50 @@ const finalProject = {
   title: 'Final Project',
   assignments: [
     {
-      title: 'Final Project Description',
-      link: 'https://docs.google.com/document/d/1A4SWxG_nthjeJbuH_qhzpWbRiIxqOnv_hDsTGHwptm8/edit?usp=sharing',
+      title: 'Final Project Details',
+      link: 'https://docs.google.com/document/d/1gMCVMMLtsXF1sy4Jd3UHGUYDZ-ylTl2J73VNStpZmms/edit?usp=sharing',
+    },
+    {
+      title: 'Deliverable #1: Final Project Team Formation and Project Ideas',
+      link: 'https://docs.google.com/document/d/1dTn7_K17jujJdz-tU0ZPH-nPVMtUbOF7h-vW3qxN7Gc/edit?usp=sharing',
+      due: {
+        time: '11:59pm',
+        day: 'Wednesday, 5/19/2021',
+        location: 'on Canvas'
+      }
+    },
+    {
+      title: 'Deliverable #2: Code',
+      link: 'https://classroom.github.com/g/vzPBCgkB',
+      due: {
+        time: 'at the time of your grading demo',
+        day: 'during finals week'
+      },
       notes: [
-        'Code and grading demo due by 5:00pm (US/Pacific), Friday 12/11/2020'
+        'Use the link above to create a repo on GitHub for your team to use for the final project.'
       ]
     },
     {
-      title: 'Deliverable #1: Team Formation and Project Ideas',
-      link: 'https://docs.google.com/document/d/1GuAKGv1Im8B37Z4nTHYA2mmsM1jHFojGIiMzTfrAuVk/edit?usp=sharing',
-      notes: [
-        'Due **ON CANVAS** by 11:59pm, Monday, 11/23/2020'
-      ]
-    },
-    {
-      title: 'Final Project GitHub Classroom Link',
-      link: 'https://classroom.github.com/g/wCEwH1bi',
-      notes: [
-        'Use this link to create a GitHub repository for your team to use for the final project.'
-      ]
-    },
-    {
-      link: 'https://docs.google.com/document/d/1owMVxBPc_RJp5oZDt5u7J_auvrmsgsHhzM_gGYfR5Fw/edit?usp=sharing',
       title: 'Deliverable #3: Final Project Team Evaluation',
-      notes: [
-        'Due **ON CANVAS** by 5:00pm (US/Pacific), Friday 12/11/2020'
-      ]
+      link: 'https://docs.google.com/document/d/1M9zlk5Zamb7jCOj8XKMwIJQLy88ekVu0v36121rNdkA/edit?usp=sharing',
+      due: {
+        time: '5:00pm',
+        day: 'Friday, 6/11/2021',
+        location: 'on Canvas'
+      }
     },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
-      ]
-    }
+    // {
+    //   link : '',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is []().'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
