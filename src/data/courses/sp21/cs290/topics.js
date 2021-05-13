627cbfff9f2cf749cd1695cf3df1b5467a91b1d3
Rob Hess <hessro@oregonstate.edu>
2021-05-13 13:53:08 -0700

    Add Express code link to CS 290.

--- src/data/courses/sp21/cs290/topics.js
+++ src/data/courses/sp21/cs290/topics.js
@@ -318,11 +318,11 @@ const topics = [
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1hMtoRhCPMzuiw5uR1E92S6B0rVEQR869V2ZbDb4UQoM/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-f20/express-serving',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-sp21/express-serving',
+        description: 'GitHub repo for code written in lecture'
+      },
       // {
       //   title: 'Lecture doodles',
       //   link: 'https://drive.google.com/file/d/1Dwt98E2PRgA-GvqplRJO-5YY342wvEuD/view?usp=sharing',
