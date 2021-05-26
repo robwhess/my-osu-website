b36837b0f6fe5b7fbed9e9273ecaaf2872b551db
Rob Hess <hessro@oregonstate.edu>
2021-05-25 17:04:54 -0700

    Publish CS 480 assignment 4.

--- src/data/courses/sp21/cs480/assignments.js
+++ src/data/courses/sp21/cs480/assignments.js
@@ -34,17 +34,17 @@ const assignments = {
         'Demo due by 11:59pm, Monday 6/7/2021'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash;Code generation with LLVM',
-    //   due: {
-    //     day: 'Thursday, 6/11/2020',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     '**This assignment will not be demoed**'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/QjCufh6i',
+      title: 'Assignment 4 &ndash;Code generation with LLVM',
+      due: {
+        day: 'Monday, 6/7/2021',
+        time: '11:59pm'
+      },
+      notes: [
+        '**This assignment will not be demoed**'
+      ]
+    }
   ],
 
   preamble: "Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline."
