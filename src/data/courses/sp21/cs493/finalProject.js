7747a326960e99992d84181e95e1143f277592b0
Rob Hess <hessro@oregonstate.edu>
2021-05-28 13:45:07 -0700

    Post final project demo appointment links for CS 290 and CS 493.

--- src/data/courses/sp21/cs493/finalProject.js
+++ src/data/courses/sp21/cs493/finalProject.js
@@ -34,18 +34,18 @@ const finalProject = {
         location: 'on Canvas'
       }
     },
-    // {
-    //   link : '',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Sign up for a final project demo appointment using the link above.',
-    //     'Sign up for one demo appointment per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub at the time of your demo.',
-    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is []().'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Sign up for a final project demo appointment using the link above.',
+        'Sign up for one demo appointment per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub at the time of your demo.',
+        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete RESTful API.  You can find more info below.'
