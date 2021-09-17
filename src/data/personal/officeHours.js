9caa0bc293dfccfd216f7e01f53df5bb6bd26c7e
Rob Hess <hessro@oregonstate.edu>
2021-09-17 15:54:40 -0700

    Make office hours TBD.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,17 +2,20 @@ const officeHours = [
   {
     hours: [
       {
-        day: 'Thurs.',
-        time: '10:00am&ndash;12:00pm',
-        timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      },
-      {
-        day: 'Fri.',
-        time: '10:00&ndash;11:00am',
-        timeZone: 'US/Pacific',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+        day: 'TBD'
       }
+      // {
+      //   day: 'Thurs.',
+      //   time: '10:00am&ndash;12:00pm',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      // },
+      // {
+      //   day: 'Fri.',
+      //   time: '10:00&ndash;11:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      // }
     ]
   }
 ];
