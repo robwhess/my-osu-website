cfc479d4709369a2604ea1468f32de27e9ba2b37
Rob Hess <hessro@oregonstate.edu>
2021-09-20 14:21:43 -0700

    Add TAs to CS 261 and CS 290.

--- src/data/courses/f21/cs261/tas.js
+++ src/data/courses/f21/cs261/tas.js
@@ -1,7 +1,7 @@
 const taInfo = [
   {
-    name: 'TBA',
-    email: '',
+    name: 'Andrew Festa',
+    email: 'festaa@oregonstate.edu',
     officeHours: [
       // {
       //   day: '',
@@ -19,6 +19,106 @@ const taInfo = [
       // },
     ]
   },
+  {
+    name: 'Tieqiao Wang',
+    email: 'wangtie@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '8:00 &ndash; 9:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'Aaron Frost',
+    email: 'frostaa@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '8:00 &ndash; 9:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'Shaurya Gaur',
+    email: 'gaurs@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '8:00 &ndash; 9:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'James Taylor',
+    email: 'tayloja5@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '8:00 &ndash; 9:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'Sadie Thomas',
+    email: 'thomsadi@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '8:00 &ndash; 9:00am',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  }
 ];
 
 export default taInfo;
--- src/data/courses/f21/cs290/tas.js
+++ src/data/courses/f21/cs290/tas.js
@@ -40,6 +40,48 @@ const taInfo = [
       //   videoConferenceLink: ''
       // },
     ]
+  },
+  {
+    name: 'Alex Rash',
+    email: 'rasha@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '1:00 &ndash; 3:00pm',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: '9:00 &ndash; 11:00am',
+      //   timeZone: 'US/Pacific',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'Zach Taylor',
+    email: 'taylzach@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: '',
+      //   time: '1:00 &ndash; 3:00pm',
+      //   timeZone: 'US/Pacific',
+      //   videoConferenceLink: ''
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: '9:00 &ndash; 11:00am',
+      //   timeZone: 'US/Pacific',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
   }
 ];
 
