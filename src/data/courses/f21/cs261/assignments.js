bf079d3d0964a8245d83b4bf2bf324c311bc7f32
Rob Hess <hessro@oregonstate.edu>
2021-09-23 13:44:25 -0700

    Publish CS 261 assignment 1.

--- src/data/courses/f21/cs261/assignments.js
+++ src/data/courses/f21/cs261/assignments.js
@@ -1,21 +1,19 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
+    {
+      link : 'https://classroom.github.com/a/mjrQ7oIy',
+      title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
+      due: {
+        day: 'Monday, 10/18/2021',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 11/1/2021'
+      ]
+    },
     // {
-    //   link : 'https://classroom.github.com/a/VeT5s4qj',
-    //   title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
-    //   due: {
-    //     day: 'Monday, 10/19/2020',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Monday 11/2/2020',
-    //     '[Morning lecture doodles](https://drive.google.com/file/d/1N4abi2yjLYkIoOVJgubDyzDlLCRf9QBw/view?usp=sharing) &ndash; from section 002',
-    //     '[Afternoon lecture doodles](https://drive.google.com/file/d/1cWXf5lqmSo1joFvBlbtNJwZbshwFCqZ-/view?usp=sharing) &ndash; from section 001'
-    //   ]
-    // },
-    // {
-    //   link : 'https://classroom.github.com/a/OCGo2WY0',
+    //   link : '',
     //   title: 'Assignment 2 &ndash; Stacks and queues',
     //   due: {
     //     day: '~~Monday, 11/2/2020~~ **Wednesday, 11/4/2020**',
@@ -28,7 +26,7 @@ const assignments = {
     //   ]
     // },
     // {
-    //   link : 'https://classroom.github.com/a/17mDujPI',
+    //   link : '',
     //   title: 'Assignment 3 &ndash; Binary search trees',
     //   due: {
     //     day: 'Monday, 11/23/2020',
@@ -41,7 +39,7 @@ const assignments = {
     //   ]
     // },
     // {
-    //   link : 'https://classroom.github.com/a/8AU0ZRct',
+    //   link : '',
     //   title: 'Assignment 4 &ndash; Priority queues',
     //   due: {
     //     day: 'Monday, 12/7/2020',
