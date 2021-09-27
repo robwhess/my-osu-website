0f8c5f762e9c869296e30d526db49c2305959586
Rob Hess <hessro@oregonstate.edu>
2021-09-27 15:55:34 -0700

    Post CS 290 TA hours.

--- src/data/courses/f21/cs290/tas.js
+++ src/data/courses/f21/cs290/tas.js
@@ -2,64 +2,87 @@ const taInfo = [
   {
     name: 'Jasmin Mousavi',
     email: 'mousavij@oregonstate.edu',
+    appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNvbXM2NHpwNkg5fGRlZmF1bHR8M2Y0YjhlNTRiOTE3ZTNiNzNhNzY0NzIyNDJiMjY0ZGI',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '1:00 &ndash; 3:00pm',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Mon.',
+        time: '2:00 &ndash; 5:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: '9:00 &ndash; 11:00am',
-      //   timeZone: 'US/Pacific',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Fri.',
+        time: '9:30 &ndash; 11:30pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium',
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
     ]
   },
   {
     name: 'Emmanuel Moncada',
     email: 'moncadae@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '1:00 &ndash; 3:00pm',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues',
+        time: '12:00 &ndash; 2:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Valley Library 1812'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Thurs',
+        time: '12:00 &ndash; 1:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Valley Library 1812'
+        // videoConferenceLink: ''
+      },
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: '9:00 &ndash; 11:00am',
-      //   timeZone: 'US/Pacific',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Thurs',
+        time: '1:00 &ndash; 2:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Valley Library 1812',
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Fri',
+        time: '10:00am &ndash; 12:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Valley Library 1812',
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      }
     ]
   },
   {
     name: 'Lucas Pichette',
     email: 'pichettl@oregonstate.edu',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/94784601733?pwd=SVdKWVFla3REM1RjeitwLzZXdVJvUT09',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '1:00 &ndash; 3:00pm',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues.',
+        time: '9:15am &ndash; 12:15pm',
+        timeZone: 'US/Pacific',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94784601733?pwd=SVdKWVFla3REM1RjeitwLzZXdVJvUT09'
+      },
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: '9:00 &ndash; 11:00am',
-      //   timeZone: 'US/Pacific',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues',
+        time: '1:00 &ndash; 4:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Zoom',
+        // appointmentsLink: '',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94784601733?pwd=SVdKWVFla3REM1RjeitwLzZXdVJvUT09'
+      },
     ]
   },
   {
