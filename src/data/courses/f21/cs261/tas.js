5f432bf5f11c2f841f2f9c06822b09e0c955bd2f
Rob Hess <hessro@oregonstate.edu>
2021-09-27 16:59:20 -0700

    Post CS 261 TA hours.

--- src/data/courses/f21/cs261/tas.js
+++ src/data/courses/f21/cs261/tas.js
@@ -23,12 +23,18 @@ const taInfo = [
     name: 'Tieqiao Wang',
     email: 'wangtie@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '8:00 &ndash; 9:00am',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Mon.',
+        time: '8:00 &ndash; 10:00am',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Mon.',
+        time: '5:00 &ndash; 6:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      }
     ],
     gradingHours: [
       // {
@@ -43,20 +49,34 @@ const taInfo = [
     name: 'Aaron Frost',
     email: 'frostaa@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '8:00 &ndash; 9:00am',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '9:00am &ndash; 12:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Fri.',
+        time: '1:00 &ndash; 4:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      }
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues.',
+        time: '12:00 &ndash; 2:00pm',
+        location: 'KEC Atrium',
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Thurs.',
+        time: '9:00 &ndash; 11:00pm',
+        location: 'KEC Atrium',
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
     ]
   },
   {
@@ -103,20 +123,34 @@ const taInfo = [
     name: 'Sadie Thomas',
     email: 'thomsadi@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '8:00 &ndash; 9:00am',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '11:30am &ndash; 2:30pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Thurs.',
+        time: '2:00 &ndash; 5:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      }
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Mon.',
+        time: '11:00am &ndash; 1:00pm',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Wed.',
+        time: '11:00am &ndash; 1:00pm',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
     ]
   }
 ];
