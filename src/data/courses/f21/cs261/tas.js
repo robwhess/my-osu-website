1d95a48dd33ffe2ef65654ded1f8f998b5bc24e1
Rob Hess <hessro@oregonstate.edu>
2021-09-29 13:57:32 -0700

    Update CS 261 TA hours.

--- src/data/courses/f21/cs261/tas.js
+++ src/data/courses/f21/cs261/tas.js
@@ -5,7 +5,7 @@ const taInfo = [
     officeHours: [
       {
         day: 'Mon.',
-        time: '11:00am &ndash; 2:00pm',
+        time: '12:00am &ndash; 2:00pm',
         location: 'KEC Atrium'
         // videoConferenceLink: ''
       },
@@ -89,40 +89,80 @@ const taInfo = [
     name: 'Shaurya Gaur',
     email: 'gaurs@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '8:00 &ndash; 9:00am',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Mon.',
+        time: '3:30 &ndash; 5:30pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Tues.',
+        time: '3:30 &ndash; 5:30pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Thurs.',
+        time: '3:30 &ndash; 5:30pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Fri.',
+        time: '12:00 &ndash; 2:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      }
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues.',
+        time: '9:30 &ndash; 11:30am',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Wed.',
+        time: '3:30 &ndash; 5:30pm',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
     ]
   },
   {
     name: 'James Taylor',
     email: 'tayloja5@oregonstate.edu',
     officeHours: [
-      // {
-      //   day: '',
-      //   time: '8:00 &ndash; 9:00am',
-      //   timeZone: 'US/Pacific',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '9:00 &ndash; 11:50am',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Wed.',
+        time: '2:00 &ndash; 5:00pm',
+        location: 'KEC Atrium'
+        // videoConferenceLink: ''
+      }
     ],
     gradingHours: [
-      // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   appointmentsLink: '',
-      //   videoConferenceLink: ''
-      // },
+      {
+        day: 'Tues.',
+        time: '2:00 &ndash; 4:00pm',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      },
+      {
+        day: 'Thurs.',
+        time: '2:00 &ndash; 4:00pm',
+        location: 'KEC Atrium'
+        // appointmentsLink: '',
+        // videoConferenceLink: ''
+      }
     ]
   },
   {
