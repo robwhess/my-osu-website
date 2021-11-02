395f69df735d813f8d6ce45a022aa86260a30ed9
Rob Hess <robwhess@gmail.com>
2021-11-02 11:10:37 -0700

    Publish info about CS 290 final project.

--- src/data/courses/f21/cs290/finalProject.js
+++ src/data/courses/f21/cs290/finalProject.js
@@ -3,20 +3,20 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1gMCVMMLtsXF1sy4Jd3UHGUYDZ-ylTl2J73VNStpZmms/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/154CyPojTMeP9EbXd6oE5HSICsNqVAaCAZxNqOw4QiKI/edit?usp=sharing',
     },
     {
       title: 'Deliverable #1: Final Project Team Formation and Project Ideas',
-      link: 'https://docs.google.com/document/d/1dTn7_K17jujJdz-tU0ZPH-nPVMtUbOF7h-vW3qxN7Gc/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1jd5ReoMlUl4txgoi6UryDG-IXs2-SidAw2x5dlmOxGo/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Wednesday, 5/19/2021',
+        day: 'Wednesday, 11/17/2021',
         location: 'on Canvas'
       }
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/g/vzPBCgkB',
+      link: 'https://classroom.github.com/a/IKL52O1F',
       due: {
         time: 'at the time of your grading demo',
         day: 'during finals week'
@@ -27,25 +27,25 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/1M9zlk5Zamb7jCOj8XKMwIJQLy88ekVu0v36121rNdkA/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1eJLQU2IKq-H91FTYvSz5esYOEhFdb4_Zwz4rjlJkqLA/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 6/11/2021',
+        day: 'Friday, 12/10/2021',
         location: 'on Canvas'
       }
     },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
-      ]
-    }
+    // {
+    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
