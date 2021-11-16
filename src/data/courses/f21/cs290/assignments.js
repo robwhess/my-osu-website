67de06f0fd802653ab586a9728638e5ab03472e8
Rob Hess <robwhess@gmail.com>
2021-11-16 10:06:30 -0800

    Post CS 290 assignment 5.

--- src/data/courses/f21/cs290/assignments.js
+++ src/data/courses/f21/cs290/assignments.js
@@ -49,18 +49,18 @@ const assignments = {
         '**This assignment will not be demoed.  It will be graded offline.**'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
-    //   due: {
-    //     day: '~~Tuesday, 6/1/2021~~ **Wednesday, 6/2/2021**',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog link due **ON CANVAS** by assignment due date',
-    //     'Demo due by 5:00pm on Friday, 6/11/2021'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/vx0NOo63',
+      title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
+      due: {
+        day: 'Wednesday, 12/1/2021',
+        time: '11:59pm'
+      },
+      notes: [
+        'Code Blog link due **ON CANVAS** by assignment due date',
+        'Demo due by 11:59pm on Friday, 12/10/2021'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
