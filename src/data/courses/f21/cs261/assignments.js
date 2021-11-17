6f0d309f4900530fd58db139a2814fbdf95f0239
Rob Hess <robwhess@gmail.com>
2021-11-16 17:45:32 -0800

    Post CS 261 assignment 4.

--- src/data/courses/f21/cs261/assignments.js
+++ src/data/courses/f21/cs261/assignments.js
@@ -34,17 +34,17 @@ const assignments = {
         'Demo due by 11:59pm, Monday 11/29/2021',
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; Priority queues',
-    //   due: {
-    //     day: 'Monday, 12/7/2020',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     '**No demo for this assignment**'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/KYOPnCPm',
+      title: 'Assignment 4 &ndash; Priority queues',
+      due: {
+        day: 'Monday, 11/29/2021',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Friday 12/10/2021',
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
