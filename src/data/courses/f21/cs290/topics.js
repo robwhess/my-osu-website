089610ea2c16647bb4ab79cffa857e538304721f
Rob Hess <hessro@oregonstate.edu>
2021-11-30 13:49:47 -0800

    Update CS 290 topics.  Add async communication code link.

--- src/data/courses/f21/cs290/topics.js
+++ src/data/courses/f21/cs290/topics.js
@@ -270,7 +270,6 @@ const topics = [
   {
     title: 'Serving with Express',
     weeks: [ 8 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -314,7 +313,6 @@ const topics = [
   {
     title: 'Dynamically Rendering Content with Handlebars',
     weeks: [ 8, 9 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -368,16 +366,17 @@ const topics = [
   {
     title: 'Asynchronous Client-Server Communication',
     weeks: [ 10 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1mG9PAN18vuKIXPZXE0P4EVkOBtU9zjz4UqaIv6DnDfY/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-sp21/async-communication',
-      //   description: 'GitHub repo for code written in lecture'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-f21/async-communication',
+        description: 'GitHub repo for code written in lecture'
+      }
     ],
     readings: [
       {
