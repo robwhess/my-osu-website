eae7b88dd7931c7b552cc5481fdc0920c09d13af
Rob Hess <robwhess@gmail.com>
2021-12-16 13:48:33 -0800

    Update HoF data.  Recreate Pictogram GIF.

--- src/data/courses/sp21/cs290/hof.js
+++ src/data/courses/sp21/cs290/hof.js
@@ -10,8 +10,8 @@ const hof = [
       "Matthew Attebery"
     ],
     description: "This site allows you to create your own incredibly complex works of art and share them with the world. Simply draw your masterpiece, enter a name for the artist, and post away! If you see someone else’s drawing you really like, feel free to remix it and make it your own. You can also compare the drawings two at a time to help create the global ranking of whose drawings are the best. Although roles were shared, each member largely focused on just one section of the site. Ethan created the paint program, Elijah created the frontend, and Matthew created the backend.  A live version of the site that you can participate in can be found at https://pictogram-cs290.herokuapp.com",
-    image: pictogramImage,
-    screencap: pictogramScreencap
+    imageUrl: pictogramImage,
+    screencapUrl: pictogramScreencap
   }
 ];
 
