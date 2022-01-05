96d0b8048487456a6ed2fbee9d7a1acda25abc88
Rob Hess <hessro@oregonstate.edu>
2022-01-05 11:23:51 -0800

    Add Jasmin's hours for CS 499.

--- src/data/courses/w22/cs499/tas.js
+++ src/data/courses/w22/cs499/tas.js
@@ -2,25 +2,40 @@ const tas = [
   {
     name: 'Jasmin Mousavi',
     email: 'mousavij@oregonstate.edu',
-    appointmentsLink: '',
+    appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNvbXM2NHpwNkg5fGRlZmF1bHR8M2Y0YjhlNTRiOTE3ZTNiNzNhNzY0NzIyNDJiMjY0ZGI',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '2:00 &ndash; 5:00pm',
-      //   location: 'KEC Atrium'
-      // },
-      // {
-      //   day: 'Wed.',
-      //   time: '2:00 &ndash; 5:00pm',
-      //   location: 'KEC Atrium'
-      // },
+      {
+        day: 'Mon.',
+        time: '9:30 &ndash; 11:30am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed.',
+        time: '9:30 &ndash; 11:30am',
+        location: 'KEC Atrium'
+      },
     ],
     gradingHours: [
-      // {
-      //   day: 'Fri.',
-      //   time: '9:30am &ndash; 12:40pm',
-      //   location: 'KEC Atrium',
-      // },
+      {
+        day: 'Mon.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Tue.',
+        time: '6:00 &ndash; 7:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thur.',
+        time: '6:00 &ndash; 7:00pm',
+        location: 'KEC Atrium'
+      }
     ]
   }
 ];
