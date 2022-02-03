b34bf5852542b25b63999dd008b56c34f4855b6d
Rob Hess <hessro@oregonstate.edu>
2022-02-03 12:58:15 -0800

    Post final project info to CS 492.

--- src/data/courses/w22/cs492/finalProject.js
+++ src/data/courses/w22/cs492/finalProject.js
@@ -3,24 +3,24 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1Qq8Dzb2VnQJONWxm9SNipdQFGniIlFNFEnykTejHSyo/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1TjWslr95JiObggv2f1axTE9GrpRwrlVFZJ-kHK-SZ3c/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/19/2021'
+        day: 'Friday, 3/18/2022'
       }
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: 'https://docs.google.com/document/d/1E_iE2GOzNp_-a6idA9qI_XEmVBNPwPFTa_5m9846WBE/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1IBu8mcIHSQ48WZb5MAzbIMqFjJBtSHCByniOkdV7jpw/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Thursday, 2/25/2021',
+        day: 'Wednesday, 2/16/2022',
         location: 'on Canvas'
       }
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/g/_SNEhboY',
+      link: 'https://classroom.github.com/a/2ZhkiBYa',
       due: {
         time: 'at the time of your grading demo',
         day: 'during finals week'
@@ -31,25 +31,28 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/1UCcfVA25fQvCkkJaKn_nXaBUZ1rB26ZuprMgHRVIOSg/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1VgGKUd1HEq_PUhUL6yCuO_S8xO5G16DkTmPPtfG4tzY/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/19/2021',
+        day: 'Friday, 3/18/2022',
         location: 'on Canvas'
-      }
-    },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-      title: 'Final Project Demo Signup',
+      },
       notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+        'Note that this must be completed individually by every member of your team.'
       ]
-    }
+    },
+    // {
+    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete Android app that gets data from a third-party API.  The project will have three deliverables: a proposal, where you outline the app you\'d like to write; an implementation of your app; and a brief evaluation of your project team.  Follow the links below to find out more about each of these deliverables.'
