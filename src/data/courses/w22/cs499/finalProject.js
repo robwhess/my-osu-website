dcdbabe5ef6cf8fdb4009d486633fe1eb6dd1d9e
Rob Hess <hessro@oregonstate.edu>
2022-02-09 10:42:30 -0800

    Post CS 499 final project info.

--- src/data/courses/w22/cs499/finalProject.js
+++ src/data/courses/w22/cs499/finalProject.js
@@ -3,24 +3,24 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1NybjgqevMvJNx012OiZnEYUYMQrDyFNoNDHd2aLSwVs/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1_CSropzmWfqrHT8nlzS7A7f3XnC0fSmwgkFxvi9ftmU/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/19/2021'
+        day: 'Friday, 3/18/2022'
       }
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: 'https://docs.google.com/document/d/17WjO9lM0_52warRKzHgyYEPNLkgO4JJfnw9aFnZu5vE/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1y-yhTSDaUin_-epihdovibSqqRnbSgvOVvdUx4KJsU4/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Monday, 2/22/2021',
+        day: 'Wednesday, 2/23/2022',
         location: 'on Canvas'
       }
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/g/W6m2so5t',
+      link: 'https://classroom.github.com/a/5-sDtCjz',
       due: {
         time: 'at the time of your grading demo',
         day: 'during finals week'
@@ -31,25 +31,28 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/1viOuF-vZOMH8WJhBRwmtqVLeJfb6aGGrz9dHb5Y06t8/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1KuYc-epAuCxaxGh-3hQJS3LeqzoZfQCtzDE3lg6v6oU/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/19/2021',
+        day: 'Friday, 3/18/2022',
         location: 'on Canvas'
-      }
-    },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
-      title: 'Final Project Demo Signup',
+      },
       notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-        'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+        'Note that this must be completed individually by every member of your team.'
       ]
-    }
+    },
+    // {
+    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
+    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete React-based web application that serves dynamic, interactive data from an API.  You can find more info below.'
