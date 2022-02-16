72cd28599457f4dbd1960da03141d59bdc0eb4ce
Rob Hess <hessro@oregonstate.edu>
2022-02-15 18:26:19 -0800

    Post CS 499 assignment 4.

--- src/data/courses/w22/cs499/assignments.js
+++ src/data/courses/w22/cs499/assignments.js
@@ -45,17 +45,17 @@ const assignments = {
         'Demo due by 11:59pm, Monday 3/7/2022'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; A Redux-powered shopping app',
-    //   due: {
-    //     day: 'Monday, 3/1/2021',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 5:00pm, Friday 3/19/2021'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/KEbPy6Ag',
+      title: 'Assignment 4 &ndash; A Redux-powered shopping app',
+      due: {
+        day: 'Monday, 2/28/2022',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 5:00pm, Friday 3/18/2022'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
