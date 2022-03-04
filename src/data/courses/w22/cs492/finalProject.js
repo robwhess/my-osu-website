34b77bc3195de4a985c90771c7623047faca7a76
Rob Hess <hessro@oregonstate.edu>
2022-03-04 11:50:42 -0800

    Publish final project demo info for CS 492 and CS 499.

--- src/data/courses/w22/cs492/finalProject.js
+++ src/data/courses/w22/cs492/finalProject.js
@@ -41,18 +41,19 @@ const finalProject = {
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    // {
-    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
-    //   title: 'Final Project Demo Signup',
-    //   notes: [
-    //     'Sign up for a final project demo appointment using the link above.',
-    //     'Sign up for one demo appointment per team.',
-    //     'All team members must attend your demo.',
-    //     'Code is due on GitHub at the time of your demo.',
-    //     'Come to the demo prepared to screen share your project from one of your team\'s computers.',
-    //     'All final project demos will be held remotely via Zoom.  The meeting link for all demos is [https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09](https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09).'
-    //   ]
-    // }
+    {
+      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UUo3U1pDaXF5WHUyfGRlZmF1bHR8MWI2NTk2NDg3NTBmNDA5NTdmY2RmZTFkOWFiZWY5NGI',
+      title: 'Final Project Demo Signup',
+      notes: [
+        'Sign up for a final project demo appointment using the link above.',
+        'Sign up for one demo appointment per team.',
+        'All team members must attend your demo.',
+        'Code is due on GitHub at the time of your demo.',
+        'Come to the demo prepared to run your project from one of your team\'s computers.',
+        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
+      ]
+    }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete Android app that gets data from a third-party API.  The project will have three deliverables: a proposal, where you outline the app you\'d like to write; an implementation of your app; and a brief evaluation of your project team.  Follow the links below to find out more about each of these deliverables.'
