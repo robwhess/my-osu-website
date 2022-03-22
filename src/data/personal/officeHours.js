321e5a600963ef84a23b614f0eb0bc157d0ae3e3
Rob Hess <hessro@oregonstate.edu>
2022-03-22 13:54:01 -0700

    Update my office hours.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,18 +2,18 @@ const officeHours = [
   {
     hours: [
       {
-        day: 'Wed.',
-        time: '11:30am&ndash;12:30pm',
-        timeZone: 'US/Pacific',
-        location: 'KEC 1109'
-        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      },
-      {
         day: 'Thurs.',
         time: '10:30am&ndash;12:30pm',
         timeZone: 'US/Pacific',
         location: 'KEC 1109'
         // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Fri.',
+        time: '2:00&ndash;3:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       }
     ]
   }
