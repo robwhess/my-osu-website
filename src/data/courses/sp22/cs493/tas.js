ed5455f707eebc8f1cf1e565b15ae8038a16549d
Rob Hess <hessro@oregonstate.edu>
2022-03-30 13:43:38 -0700

    Post CS 493 TA hours.

--- src/data/courses/sp22/cs493/tas.js
+++ src/data/courses/sp22/cs493/tas.js
@@ -2,37 +2,94 @@ const taInfo = [
   {
     name: 'Jasmin Mousavi',
     email: 'mousavij@oregonstate.edu',
+    appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNvbXM2NHpwNkg5fGRlZmF1bHR8M2Y0YjhlNTRiOTE3ZTNiNzNhNzY0NzIyNDJiMjY0ZGI',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '8:30 &ndash; 10:30am',
-      //   timeZone: 'US/Pacific'
-      // },
+      {
+        day: 'Fri.',
+        time: '10:00am &ndash; 1:00pm',
+        timeZone: 'US/Pacific',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
+        details: 'Use the camera icon next to these office hours to connect to the Zoom meeting.'
+      },
     ],
     gradingHours: [
-      // {
-      //   day: 'Mon',
-      //   time: '4:00 &ndash; 6:00pm',
-      //   timeZone: 'US/Pacific'
-      // },
+      {
+        day: 'Mon',
+        time: '12:00 &ndash; 2:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Tues',
+        time: '3:30 &ndash; 5:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed',
+        time: '1:00 &ndash; 2:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '3:30 &ndash; 4:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '4:30 &ndash; 5:30pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },{
+        day: 'Fri.',
+        time: '9:00 &ndash; 10:00am',
+        timeZone: 'US/Pacific',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
+        details: 'Use the camera icon next to these grading hours to connect to the Zoom meeting.'
+      },
     ]
   },
   {
     name: "Sumer Patel",
     email: "patelsum@oregonstate.edu",
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '1:00 &ndash; 2:00pm',
-      //   timeZone: 'US/Pacific'
-      // },
+      {
+        day: 'Mon.',
+        time: '10:00 &ndash; 11:30am',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed.',
+        time: '10:00 &ndash; 11:30am',
+        timeZone: 'US/Pacific',
+        location: 'Zoom'
+      },
     ],
     gradingHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '7:00 &ndash; 9:00pm',
-      //   timeZone: 'US/Pacific'
-      // },
+      {
+        day: 'Tues.',
+        time: '2:00 &ndash; 4:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '2:00 &ndash; 4:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Fri.',
+        time: '10:00am &ndash; 1:00pm',
+        timeZone: 'US/Pacific',
+        location: 'KEC Atrium'
+      },
     ]
   }
 ];
