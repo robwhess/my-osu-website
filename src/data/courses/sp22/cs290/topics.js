134728813d594589f633a1d00328393383688bbe
Rob Hess <hessro@oregonstate.edu>
2022-04-05 16:16:38 -0700

    Add CSS code link to CS 290.

--- src/data/courses/sp22/cs290/topics.js
+++ src/data/courses/sp22/cs290/topics.js
@@ -51,7 +51,6 @@ const topics = [
   {
     title: 'HTML',
     weeks: [ 1, 2 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -86,11 +85,11 @@ const topics = [
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1DTpjDB2K4EF2sz4MwhPOYfC8CUhpZn9ykEVkwNZY8wM/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-f21/css',
-      //   description: 'GitHub repo for code written in lecture'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-sp22/css',
+        description: 'GitHub repo for code written in lecture'
+      }
     ],
     readings: [
       {
