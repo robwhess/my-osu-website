2f11f9fcdeb70e473f05ba9e6926542d6a37cf4b
Rob Hess <hessro@oregonstate.edu>
2022-04-07 12:23:11 -0700

    Update Jasmin's hours for CS 493.

--- src/data/courses/sp22/cs493/tas.js
+++ src/data/courses/sp22/cs493/tas.js
@@ -6,9 +6,13 @@ const taInfo = [
     videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
     officeHours: [
       {
+        day: 'Wed',
+        time: '1:00 &ndash; 2:00pm',
+        location: 'KEC Atrium'
+      },
+      {
         day: 'Fri.',
-        time: '10:00am &ndash; 1:00pm',
-        timeZone: 'US/Pacific',
+        time: '10:00am &ndash; 12:00pm',
         location: 'Zoom',
         videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
         details: 'Use the camera icon next to these office hours to connect to the Zoom meeting.'
@@ -18,39 +22,17 @@ const taInfo = [
       {
         day: 'Mon',
         time: '12:00 &ndash; 2:00pm',
-        timeZone: 'US/Pacific',
         location: 'KEC Atrium'
       },
       {
-        day: 'Tues',
-        time: '3:30 &ndash; 5:00pm',
-        timeZone: 'US/Pacific',
-        location: 'KEC Atrium'
-      },
-      {
-        day: 'Wed',
-        time: '1:00 &ndash; 2:00pm',
-        timeZone: 'US/Pacific',
+        day: 'Tues.',
+        time: '11:30am &ndash; 2:00pm',
         location: 'KEC Atrium'
       },
       {
         day: 'Thurs.',
-        time: '3:30 &ndash; 4:00pm',
-        timeZone: 'US/Pacific',
+        time: '11:30am &ndash; 2:00pm',
         location: 'KEC Atrium'
-      },
-      {
-        day: 'Thurs.',
-        time: '4:30 &ndash; 5:30pm',
-        timeZone: 'US/Pacific',
-        location: 'KEC Atrium'
-      },{
-        day: 'Fri.',
-        time: '9:00 &ndash; 10:00am',
-        timeZone: 'US/Pacific',
-        location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/94053640120?pwd=M1hXUUZUSnE1MGlYNHltMWJBcHVaUT09',
-        details: 'Use the camera icon next to these grading hours to connect to the Zoom meeting.'
       },
     ]
   },
@@ -63,13 +45,11 @@ const taInfo = [
       {
         day: 'Mon.',
         time: '10:00 &ndash; 11:30am',
-        timeZone: 'US/Pacific',
         location: 'KEC Atrium'
       },
       {
         day: 'Wed.',
         time: '10:00 &ndash; 11:30am',
-        timeZone: 'US/Pacific',
         location: 'Zoom',
         videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
         details: 'Use the camera icon next to these grading hours to connect to the Zoom meeting.'
@@ -79,13 +59,11 @@ const taInfo = [
       {
         day: 'Tues.',
         time: '10:00am &ndash; 1:00pm',
-        timeZone: 'US/Pacific',
         location: 'KEC Atrium'
       },
       {
         day: 'Thurs.',
         time: '12:00 &ndash; 4:00pm',
-        timeZone: 'US/Pacific',
         location: 'KEC Atrium'
       }
     ]
