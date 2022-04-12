e013b9d0b01f878eea70fbceacdfe7802d20d834
Rob Hess <hessro@oregonstate.edu>
2022-04-12 11:08:04 -0700

    Post Brian's TA hours for CS 480.

--- src/data/courses/sp22/cs480/tas.js
+++ src/data/courses/sp22/cs480/tas.js
@@ -2,19 +2,34 @@ const taInfo = [
   {
     name: 'Brian Mulanda',
     email: 'mulandab@oregonstate.edu',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '1:00 &ndash; 2:30pm',
-      //   timeZone: 'US/Pacific',
-      // }
+      {
+        day: 'Tues.',
+        time: '8:30 &ndash; 10:30am',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      },
+      {
+        day: 'Tues.',
+        time: '11:30am &ndash; 1:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Wed.',
-      //   time: '8:00 &ndash; 9:30am',
-      //   timeZone: 'US/Pacific'
-      // }
+      {
+        day: 'Thurs.',
+        time: '11:30am &ndash; 1:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      },
+      {
+        day: 'Fri.',
+        time: '8:30 &ndash; 10:30am',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      }
     ]
   },
   {
