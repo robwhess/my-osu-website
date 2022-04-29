ccca77946e0c58594912856d0b6a21d41f337675
Rob Hess <hessro@oregonstate.edu>
2022-04-29 15:46:52 -0700

    Publish CS 493 final project.

--- src/data/courses/sp22/cs493/finalProject.js
+++ src/data/courses/sp22/cs493/finalProject.js
@@ -1,39 +1,42 @@
 const finalProject = {
   title: 'Final Project',
   assignments: [
-    // {
-    //   title: 'Final Project Details',
-    //   link: 'https://docs.google.com/document/d/1kkFw90RkUdX2sLiU8yJb2pbxxRQWx4a_E-3vqPGaqZw/edit?usp=sharing',
-    // },
-    // {
-    //   title: 'Deliverable #1: Final Project Proposal',
-    //   link: 'https://docs.google.com/document/d/17MHG6ZvPpQwOaibX6YygvduN3YZmR2HhWiE250BGYqc/edit?usp=sharing',
-    //   due: {
-    //     time: '11:59pm',
-    //     day: 'Wednesday, 5/12/2021',
-    //     location: 'on Canvas'
-    //   }
-    // },
-    // {
-    //   title: 'Deliverable #2: Code',
-    //   link: 'https://classroom.github.com/g/6Wm12we6',
-    //   due: {
-    //     time: 'at the time of your grading demo',
-    //     day: 'during finals week'
-    //   },
-    //   notes: [
-    //     'Use the link above to create a repo on GitHub for your team to use for the final project.'
-    //   ]
-    // },
-    // {
-    //   title: 'Deliverable #3: Final Project Team Evaluation',
-    //   link: 'https://docs.google.com/document/d/1RAN3Xj5UEE3h1qlWVTNN2QWFMQbifjM2_DcM5iU9ZT0/edit?usp=sharing',
-    //   due: {
-    //     time: '5:00pm',
-    //     day: 'Friday, 6/11/2021',
-    //     location: 'on Canvas'
-    //   }
-    // },
+    {
+      title: 'Final Project Details',
+      link: 'https://docs.google.com/document/d/1wWgiGouapjrf3Jw8j6tD1XJ2NzJDhfMrDLTrDfMwDWg/edit?usp=sharing',
+    },
+    {
+      title: 'Deliverable #1: Final Project Proposal',
+      link: 'https://docs.google.com/document/d/1dWXr_vJyXkq1Gc0nCpB__C5M8Hz8wqn8JwqDY3oerf4/edit?usp=sharing',
+      due: {
+        time: '11:59pm',
+        day: 'Wednesday, 5/11/2022',
+        location: 'on Canvas'
+      }
+    },
+    {
+      title: 'Deliverable #2: Code',
+      link: 'https://classroom.github.com/a/bfBqhMbH',
+      due: {
+        time: 'at the time of your grading demo',
+        day: 'during finals week'
+      },
+      notes: [
+        'Use the link above to create a repo on GitHub for your team to use for the final project.'
+      ]
+    },
+    {
+      title: 'Deliverable #3: Final Project Team Evaluation',
+      link: 'https://docs.google.com/document/d/1Vdlsi8qLSPLA_jQsvtkK4udcSGKwug5ogjz9-0cg0RU/edit?usp=sharing',
+      due: {
+        time: '5:00pm',
+        day: 'Friday, 6/10/2022',
+        location: 'on Canvas'
+      },
+      notes: [
+        'Note that this must be completed individually by every member of your team.'
+      ]
+    },
     // {
     //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
     //   title: 'Final Project Demo Signup',
