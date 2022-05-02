f87406cf789efd6a96ee053f7f710e4ff7ae6a10
Rob Hess <hessro@oregonstate.edu>
2022-05-02 13:08:57 -0700

    Add Auth code link to CS 493.

--- src/data/courses/sp22/cs493/topics.js
+++ src/data/courses/sp22/cs493/topics.js
@@ -267,7 +267,6 @@ const topics = [
   {
     title: 'Using MySQL with Sequelize to Store API Data',
     weeks: [ 5 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -326,16 +325,17 @@ const topics = [
   {
     title: 'API Authentication and Authorization',
     weeks: [ 6 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1uQjeMG2GtLFSdB0yBdkZroKojnuaCykt_6Tl4c09E2s/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs493-sp21/auth',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs493-sp22/auth',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
