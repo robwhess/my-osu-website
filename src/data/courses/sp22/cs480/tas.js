66319266c43ea90f559d2946381f8e6ec17012af
Rob Hess <hessro@oregonstate.edu>
2022-05-17 15:37:07 -0700

    Unlist Brian's hours from CS 480.

--- src/data/courses/sp22/cs480/tas.js
+++ src/data/courses/sp22/cs480/tas.js
@@ -2,35 +2,35 @@ const taInfo = [
   {
     name: 'Brian Mulanda',
     email: 'mulandab@oregonstate.edu',
-    videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
-    appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZlQnNKTXN2eFZsfGRlZmF1bHR8MWYwNzJkMTVkMmQzMjZlZjVlODQ2YWRkMzQ2YzI0YzI',
+    // videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZlQnNKTXN2eFZsfGRlZmF1bHR8MWYwNzJkMTVkMmQzMjZlZjVlODQ2YWRkMzQ2YzI0YzI',
     officeHours: [
-      {
-        day: 'Tues.',
-        time: '8:30 &ndash; 10:30am',
-        location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
-      },
-      {
-        day: 'Tues.',
-        time: '11:30am &ndash; 1:30pm',
-        location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
-      }
+      // {
+      //   day: 'Tues.',
+      //   time: '8:30 &ndash; 10:30am',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      // },
+      // {
+      //   day: 'Tues.',
+      //   time: '11:30am &ndash; 1:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      // }
     ],
     gradingHours: [
-      {
-        day: 'Thurs.',
-        time: '11:30am &ndash; 1:30pm',
-        location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
-      },
-      {
-        day: 'Fri.',
-        time: '8:30 &ndash; 10:30am',
-        location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
-      }
+      // {
+      //   day: 'Thurs.',
+      //   time: '11:30am &ndash; 1:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      // },
+      // {
+      //   day: 'Fri.',
+      //   time: '8:30 &ndash; 10:30am',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94892492441?pwd=bFhFNGFDWlpuemVtb3o5M0lJemRyZz09',
+      // }
     ]
   },
   {
