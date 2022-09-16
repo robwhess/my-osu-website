1058073678fc9486b895599aa55b170a05368985
Rob Hess <hessro@oregonstate.edu>
2022-09-16 14:34:41 -0700

    Add TAs for f22 courses.

--- src/data/courses/f22/cs261/tas.js
+++ src/data/courses/f22/cs261/tas.js
@@ -1,178 +1,118 @@
 const taInfo = [
-  // {
-  //   name: 'Andrew Festa',
-  //   email: 'festaa@oregonstate.edu',
-  //   officeHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '12:00am &ndash; 2:00pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '11:00am &ndash; 2:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     // {
-  //     //   day: '',
-  //     //   time: ' &ndash; ',
-  //     //   appointmentsLink: '',
-  //     //   videoConferenceLink: ''
-  //     // },
-  //   ]
-  // },
-  // {
-  //   name: 'Tieqiao Wang',
-  //   email: 'wangtie@oregonstate.edu',
-  //   officeHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '8:00 &ndash; 10:00am',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Mon.',
-  //       time: '5:00 &ndash; 6:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     // {
-  //     //   day: '',
-  //     //   time: ' &ndash; ',
-  //     //   appointmentsLink: '',
-  //     //   videoConferenceLink: ''
-  //     // },
-  //   ]
-  // },
-  // {
-  //   name: 'Aaron Frost',
-  //   email: 'frostaa@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBUUmE4dkkwcE9yfGRlZmF1bHR8MjM5ZWUxNTE4NDEwNjdmYThmYWMzYjUyODFjYzc5MzM',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '9:00am &ndash; 12:00pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '1:00 &ndash; 4:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '12:00 &ndash; 2:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Thurs.',
-  //       time: '9:00 &ndash; 11:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //   ]
-  // },
-  // {
-  //   name: 'Shaurya Gaur',
-  //   email: 'gaurs@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUlsc0toVW1tV2hafGRlZmF1bHR8YjAzYTYzNWEwZTI3MDRjMGQ2N2E3NzZmMjkyZjU4NGE',
-  //   officeHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '3:30 &ndash; 5:30pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Tues.',
-  //       time: '3:30 &ndash; 5:30pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Thurs.',
-  //       time: '3:30 &ndash; 5:30pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '12:00 &ndash; 2:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '9:30 &ndash; 11:30am',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '3:30 &ndash; 5:30pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //   ]
-  // },
-  // {
-  //   name: 'James Taylor',
-  //   email: 'tayloja5@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBCbEFLUkRkRUROfGRlZmF1bHR8MzY4ZmY3MzIyMDE1N2U1ZDA3ZjM5NmE4Y2MwYWJiMTQ',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '9:00 &ndash; 11:50am',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '2:00 &ndash; 5:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '2:00 &ndash; 4:00pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Thurs.',
-  //       time: '2:00 &ndash; 4:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Sadie Thomas',
-  //   email: 'thomsadi@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9pNXQwc1o5WnZmfGRlZmF1bHR8YjM4NGM1YzgyN2RjNjVhZThlOGZmNTYyZTg3ZWI1ZmQ',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '11:30am &ndash; 2:30pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Thurs.',
-  //       time: '2:00 &ndash; 5:00pm',
-  //       location: 'KEC Atrium',
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '11:00am &ndash; 1:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '11:00am &ndash; 1:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //   ]
-  // }
+  {
+    name: 'Sagar Karki',
+    email: 'karkisa@oregonstate.edu',
+    officeHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '12:00am &ndash; 2:00pm',
+      //   location: 'KEC Atrium'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: '',
+      //   time: ' &ndash; ',
+      //   appointmentsLink: '',
+      //   videoConferenceLink: ''
+      // },
+    ]
+  },
+  {
+    name: 'Ella Daugherty',
+    email: 'daugheel@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBCbEFLUkRkRUROfGRlZmF1bHR8MzY4ZmY3MzIyMDE1N2U1ZDA3ZjM5NmE4Y2MwYWJiMTQ',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '9:00 &ndash; 11:50am',
+      //   location: 'KEC Atrium'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '2:00 &ndash; 4:00pm',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  },
+  {
+    name: 'Shaurya Gaur',
+    email: 'gaurs@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUlsc0toVW1tV2hafGRlZmF1bHR8YjAzYTYzNWEwZTI3MDRjMGQ2N2E3NzZmMjkyZjU4NGE',
+    officeHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '3:30 &ndash; 5:30pm',
+      //   location: 'KEC Atrium'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '9:30 &ndash; 11:30am',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  },
+  {
+    name: 'Sergiy Greblov',
+    email: 'greblovs@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBCbEFLUkRkRUROfGRlZmF1bHR8MzY4ZmY3MzIyMDE1N2U1ZDA3ZjM5NmE4Y2MwYWJiMTQ',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '9:00 &ndash; 11:50am',
+      //   location: 'KEC Atrium'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '2:00 &ndash; 4:00pm',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  },
+  {
+    name: 'Anh Nguyen',
+    email: 'nguyanh2@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBCbEFLUkRkRUROfGRlZmF1bHR8MzY4ZmY3MzIyMDE1N2U1ZDA3ZjM5NmE4Y2MwYWJiMTQ',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '9:00 &ndash; 11:50am',
+      //   location: 'KEC Atrium'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '2:00 &ndash; 4:00pm',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  },
+  {
+    name: 'Sadie Thomas',
+    email: 'thomsadi@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9pNXQwc1o5WnZmfGRlZmF1bHR8YjM4NGM1YzgyN2RjNjVhZThlOGZmNTYyZTg3ZWI1ZmQ',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '11:30am &ndash; 2:30pm',
+      //   location: 'KEC Atrium',
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 1:00pm',
+      //   location: 'KEC Atrium',
+      // }
+    ]
+  }
 ];
 
 export default taInfo;
--- src/data/courses/f22/cs290/tas.js
+++ src/data/courses/f22/cs290/tas.js
@@ -23,73 +23,65 @@ const taInfo = [
   //     },
   //   ]
   // },
-  // {
-  //   name: 'Emmanuel Moncada',
-  //   email: 'moncadae@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UUdoOGhFQTU3VjJxfGRlZmF1bHR8N2RhODUzN2NmNDQzNGFmMDQzMjVmOTAxZjAzMmY0OTA',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues',
-  //       time: '12:00 &ndash; 2:00pm',
-  //       location: 'Valley Library 1812',
-  //       details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
-  //     },
-  //     {
-  //       day: 'Thurs',
-  //       time: '12:00 &ndash; 1:00pm',
-  //       location: 'Valley Library 1812',
-  //       details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
-  //     },
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Thurs',
-  //       time: '1:00 &ndash; 2:00pm',
-  //       location: 'Valley Library 1812',
-  //       details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
-  //     },
-  //     {
-  //       day: 'Fri',
-  //       time: '10:00am &ndash; 12:00pm',
-  //       location: 'Valley Library 1812',
-  //       details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Zach Taylor',
-  //   email: 'taylzach@oregonstate.edu',
-  //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNpRlRYWkpQVUpGfGRlZmF1bHR8ZjNlMDg2OWZmZTI0Nzk4ZDIxNTc1YjY3M2RjNGQ3OGU',
-  //   officeHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '11:00am &ndash; 12:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '11:00am &ndash; 12:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Fri.',
-  //       time: '11:00am &ndash; 12:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Mon',
-  //       time: '12:00 &ndash; 2:00pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //     {
-  //       day: 'Fri',
-  //       time: '12:30 &ndash; 1:30pm',
-  //       location: 'KEC Atrium',
-  //     },
-  //   ]
-  // }
+  {
+    name: 'Emmanuel Moncada',
+    email: 'moncadae@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/selfsched?sstoken=UUdoOGhFQTU3VjJxfGRlZmF1bHR8N2RhODUzN2NmNDQzNGFmMDQzMjVmOTAxZjAzMmY0OTA',
+    officeHours: [
+      // {
+      //   day: 'Tues',
+      //   time: '12:00 &ndash; 2:00pm',
+      //   location: 'Valley Library 1812',
+      //   details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Thurs',
+      //   time: '1:00 &ndash; 2:00pm',
+      //   location: 'Valley Library 1812',
+      //   details: 'Here\'s a map of the first floor of the Valley Library: [https://library.oregonstate.edu/floormaps/first-floor](https://library.oregonstate.edu/floormaps/first-floor)'
+      // }
+    ]
+  },
+  {
+    name: 'Amanda Sinha',
+    email: 'sinhaam@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNpRlRYWkpQVUpGfGRlZmF1bHR8ZjNlMDg2OWZmZTI0Nzk4ZDIxNTc1YjY3M2RjNGQ3OGU',
+    officeHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 12:00pm',
+      //   location: 'KEC Atrium',
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon',
+      //   time: '12:00 &ndash; 2:00pm',
+      //   location: 'KEC Atrium',
+      // }
+    ]
+  },
+  {
+    name: 'Zach Taylor',
+    email: 'taylzach@oregonstate.edu',
+    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNpRlRYWkpQVUpGfGRlZmF1bHR8ZjNlMDg2OWZmZTI0Nzk4ZDIxNTc1YjY3M2RjNGQ3OGU',
+    officeHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 12:00pm',
+      //   location: 'KEC Atrium',
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon',
+      //   time: '12:00 &ndash; 2:00pm',
+      //   location: 'KEC Atrium',
+      // }
+    ]
+  }
 ];
 
 export default taInfo;
