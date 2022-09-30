14f5241e73e13ae1782e7c0c28cbaf32e80377e0
Rob Hess <hessro@oregonstate.edu>
2022-09-30 15:24:09 -0700

    Post some TA hours.

--- src/data/courses/f22/cs261/tas.js
+++ src/data/courses/f22/cs261/tas.js
@@ -23,18 +23,28 @@ const taInfo = [
     email: 'berlineg@oregonstate.edu',
     // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UVBCbEFLUkRkRUROfGRlZmF1bHR8MzY4ZmY3MzIyMDE1N2U1ZDA3ZjM5NmE4Y2MwYWJiMTQ',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '9:00 &ndash; 11:50am',
-      //   location: 'KEC Atrium'
-      // }
+      {
+        day: 'Wed.',
+        time: '3:00 &ndash; 4:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '1:00 &ndash; 3:00pm',
+        location: 'KEC Atrium'
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '2:00 &ndash; 4:00pm',
-      //   location: 'KEC Atrium'
-      // }
+      {
+        day: 'Mon.',
+        time: '3:00 &ndash; 4:30pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Fri.',
+        time: '8:30 &ndash; 9:45am',
+        location: 'KEC Atrium'
+      }
     ]
   },
   {
@@ -97,20 +107,25 @@ const taInfo = [
   {
     name: 'Sadie Thomas',
     email: 'thomsadi@oregonstate.edu',
-    // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UU9pNXQwc1o5WnZmfGRlZmF1bHR8YjM4NGM1YzgyN2RjNjVhZThlOGZmNTYyZTg3ZWI1ZmQ',
+    appointmentsLink: 'https://calendar.app.google/PbPamdUFLZBgimni9',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '11:30am &ndash; 2:30pm',
-      //   location: 'KEC Atrium',
-      // }
+      {
+        day: 'Mon.',
+        time: '11:00 &ndash; 11:50am',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Wed.',
+        time: '2:00 &ndash; 3:50pm',
+        location: 'KEC Atrium',
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '11:00am &ndash; 1:00pm',
-      //   location: 'KEC Atrium',
-      // }
+      {
+        day: 'Tues.',
+        time: '9:30 &ndash; 11:30am',
+        location: 'KEC Atrium',
+      }
     ]
   },
   {
--- src/data/courses/f22/cs290/tas.js
+++ src/data/courses/f22/cs290/tas.js
@@ -2,25 +2,35 @@ const taInfo = [
   {
     name: 'Vinoothna Ginjala',
     email: 'ginjalav@oregonstate.edu',
+    videoConferenceLink: 'https://oregonstate.zoom.us/my/cs290officehours?pwd=bVl2VTFnWU5PeWNpS1E1T3Z1c2tKZz09',
   //   appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNvbXM2NHpwNkg5fGRlZmF1bHR8M2Y0YjhlNTRiOTE3ZTNiNzNhNzY0NzIyNDJiMjY0ZGI',
     officeHours: [
-  //     {
-  //       day: 'Mon.',
-  //       time: '2:00 &ndash; 5:00pm',
-  //       location: 'KEC Atrium'
-  //     },
-  //     {
-  //       day: 'Wed.',
-  //       time: '2:00 &ndash; 5:00pm',
-  //       location: 'KEC Atrium'
-  //     },
+      {
+        day: 'Tues.',
+        time: '8:00am &ndash; 12:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/my/cs290officehours?pwd=bVl2VTFnWU5PeWNpS1E1T3Z1c2tKZz09'
+      },
+      {
+        day: 'Thurs.',
+        time: '8:00 &ndash; 10:00am',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/my/cs290officehours?pwd=bVl2VTFnWU5PeWNpS1E1T3Z1c2tKZz09'
+      }
     ],
     gradingHours: [
-  //     {
-  //       day: 'Fri.',
-  //       time: '9:30am &ndash; 12:40pm',
-  //       location: 'KEC Atrium',
-  //     },
+      {
+        day: 'Wed.',
+        time: '11:00am &ndash; 1:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/my/cs290officehours?pwd=bVl2VTFnWU5PeWNpS1E1T3Z1c2tKZz09'
+      },
+      {
+        day: 'Thurs.',
+        time: '11:00am &ndash; 1:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/my/cs290officehours?pwd=bVl2VTFnWU5PeWNpS1E1T3Z1c2tKZz09'
+      }
     ]
   },
   {
@@ -87,18 +97,33 @@ const taInfo = [
     email: 'taylzach@oregonstate.edu',
     // appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUNpRlRYWkpQVUpGfGRlZmF1bHR8ZjNlMDg2OWZmZTI0Nzk4ZDIxNTc1YjY3M2RjNGQ3OGU',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '11:00am &ndash; 12:00pm',
-      //   location: 'KEC Atrium',
-      // }
+      {
+        day: 'Mon.',
+        time: '12:00 &ndash; 1:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Wed.',
+        time: '12:00 &ndash; 1:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Fri.',
+        time: '12:00 &ndash; 1:00pm',
+        location: 'KEC Atrium',
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Mon',
-      //   time: '12:00 &ndash; 2:00pm',
-      //   location: 'KEC Atrium',
-      // }
+      {
+        day: 'Wed.',
+        time: '1:00 &ndash; 3:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Fri.',
+        time: '1:00 &ndash; 3:00pm',
+        location: 'KEC Atrium',
+      }
     ]
   }
 ];
