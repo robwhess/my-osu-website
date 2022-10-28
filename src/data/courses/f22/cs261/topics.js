df4d45f6d2d58f00eea8d7ff09b010e9fb731cfd
Rob Hess <hessro@oregonstate.edu>
2022-10-28 12:30:46 -0700

    Update CS 261 topics.

--- src/data/courses/f22/cs261/topics.js
+++ src/data/courses/f22/cs261/topics.js
@@ -179,7 +179,6 @@ const topics = [
   {
     title: 'Stacks, Queues, and Deques',
     weeks: [ 5 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -261,6 +260,7 @@ const topics = [
   {
     title: 'Binary Search Trees',
     weeks: [ 6, 7 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
