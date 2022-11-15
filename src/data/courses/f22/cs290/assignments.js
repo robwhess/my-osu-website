491652b325d93f5c4a033d38302dbb0048254a76
Rob Hess <hessro@oregonstate.edu>
2022-11-15 10:53:11 -0800

    Publish CS 290 assignment 5.

--- src/data/courses/f22/cs290/assignments.js
+++ src/data/courses/f22/cs290/assignments.js
@@ -49,18 +49,18 @@ const assignments = {
         '**This assignment will not be demoed.  It will be graded offline.**'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
-    //   due: {
-    //     day: 'Wednesday, 12/1/2021',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Code Blog link due **ON CANVAS** by assignment due date',
-    //     'Demo due by 11:59pm on Friday, 12/10/2021'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/FYlqUZiz',
+      title: 'Assignment 5 &ndash; Express serving and Handlebars templating',
+      due: {
+        day: 'Monday, 11/28/2022',
+        time: '11:59pm'
+      },
+      notes: [
+        'Code Blog link due **ON CANVAS** by assignment due date',
+        'Demo due by 11:59pm on Friday, 12/9/2022'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
