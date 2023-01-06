09a8e880ba6826e295171a0a53da22e019fd995f
Rob Hess <hessro@oregonstate.edu>
2023-01-06 13:56:11 -0800

    Publish TAs for w23 courses.

--- src/data/courses/w23/cs492/tas.js
+++ src/data/courses/w23/cs492/tas.js
@@ -1,23 +1,42 @@
 const tas = [
-  // {
-  //   name: '',
-  //   email: '@oregonstate.edu',
-  //   appointmentsLink: '',
-  //   officeHours: [
-  //     // {
-  //     //   day: 'Tues.',
-  //     //   time: '10:00 &ndash; 11:30am',
-  //     //   location: 'KEC Atrium'
-  //     // },
-  //   ],
-  //   gradingHours: [
-  //     // {
-  //     //   day: 'Mon.',
-  //     //   time: '11:00am &ndash; 1:00pm',
-  //     //   location: 'KEC Atrium'
-  //     // },
-  //   ]
-  // }
+  {
+    name: 'Supreeth Suresh Avadhani',
+    email: 'sureshas@oregonstate.edu',
+    // appointmentsLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '10:00 &ndash; 11:30am',
+      //   location: 'KEC Atrium'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 1:00pm',
+      //   location: 'KEC Atrium'
+      // },
+    ]
+  },
+  {
+    name: 'Rohit Nair',
+    email: 'nairro@oregonstate.edu',
+    // appointmentsLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '10:00 &ndash; 11:30am',
+      //   location: 'KEC Atrium'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 1:00pm',
+      //   location: 'KEC Atrium'
+      // },
+    ]
+  }
 ];
 
 export default tas;
--- src/data/courses/w23/cs494/tas.js
+++ src/data/courses/w23/cs494/tas.js
@@ -1,24 +1,24 @@
 const tas = [
-  // {
-  //   name: '',
-  //   email: '@oregonstate.edu',
-  //   appointmentsLink: '',
-  //   videoConferenceLink: '',
-  //   officeHours: [
-  //     // {
-  //     //   day: 'Mon.',
-  //     //   time: '9:30 &ndash; 11:30am',
-  //     //   location: 'KEC Atrium'
-  //     // },
-  //   ],
-  //   gradingHours: [
-  //     // {
-  //     //   day: 'Mon.',
-  //     //   time: '4:00 &ndash; 6:00pm',
-  //     //   location: 'KEC Atrium'
-  //     // },
-  //   ]
-  // }
+  {
+    name: 'Sumer Patel',
+    email: 'patelsum@oregonstate.edu',
+    // appointmentsLink: '',
+    // videoConferenceLink: '',
+    officeHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '9:30 &ndash; 11:30am',
+      //   location: 'KEC Atrium'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '4:00 &ndash; 6:00pm',
+      //   location: 'KEC Atrium'
+      // },
+    ]
+  }
 ];
 
 export default tas;
