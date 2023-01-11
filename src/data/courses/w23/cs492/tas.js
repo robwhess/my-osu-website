0f6967536245bf620f01c62e0e49aea59ef57b77
Rob Hess <hessro@oregonstate.edu>
2023-01-11 12:31:12 -0800

    Add Subramanya as a CS 492 TA.

--- src/data/courses/w23/cs492/tas.js
+++ src/data/courses/w23/cs492/tas.js
@@ -19,6 +19,25 @@ const tas = [
     ]
   },
   {
+    name: 'Subramanya Keshavamurthy',
+    email: 'keshavas@oregonstate.edu',
+    // appointmentsLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '10:00 &ndash; 11:30am',
+      //   location: 'KEC Atrium'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Mon.',
+      //   time: '11:00am &ndash; 1:00pm',
+      //   location: 'KEC Atrium'
+      // },
+    ]
+  },
+  {
     name: 'Rohit Nair',
     email: 'nairro@oregonstate.edu',
     // appointmentsLink: '',
