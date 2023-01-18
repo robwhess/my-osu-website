b9b3af16c278c369ec30e4475e3a376e906ccef5
Rob Hess <hessro@oregonstate.edu>
2023-01-17 19:08:59 -0800

    Publish CS 492 assignment 1.

--- src/data/courses/w23/cs492/assignments.js
+++ src/data/courses/w23/cs492/assignments.js
@@ -1,17 +1,17 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; A basic RecyclerView-based weather app',
-    //   due: {
-    //     day: 'Monday, 1/24/2022',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Monday 2/7/2022'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/dCtEQLk4',
+      title: 'Assignment 1 &ndash; A basic RecyclerView-based weather app',
+      due: {
+        day: 'Monday, 1/30/2022',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 2/13/2022'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 2 &ndash; A weather app with internet connectivity and Intents',
