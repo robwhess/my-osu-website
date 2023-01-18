92f33e7760f1a0d867dd949e4436f7a113ecf181
Rob Hess <hessro@oregonstate.edu>
2023-01-18 12:23:50 -0800

    Post Sumer's TA hours for CS 494.

--- src/data/courses/w23/cs494/tas.js
+++ src/data/courses/w23/cs494/tas.js
@@ -2,21 +2,41 @@ const tas = [
   {
     name: 'Sumer Patel',
     email: 'patelsum@oregonstate.edu',
-    // appointmentsLink: '',
+    appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZROFBnTkR6Z090fGRlZmF1bHR8MmQ2N2IzMTNjYjEwZjE3M2VkOGE4NTgzOWY0NjgyZGU',
     // videoConferenceLink: '',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '9:30 &ndash; 11:30am',
-      //   location: 'KEC Atrium'
-      // },
+      {
+        day: 'Mon.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '9:00 &ndash; 10:00am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Fri.',
+        time: '2:00 &ndash; 3:00pm',
+        location: 'KEC Atrium'
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '4:00 &ndash; 6:00pm',
-      //   location: 'KEC Atrium'
-      // },
+      {
+        day: 'Thurs.',
+        time: '3:00 &ndash; 7:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Fri.',
+        time: '3:00 &ndash; 4:00pm',
+        location: 'KEC Atrium'
+      }
     ]
   }
 ];
