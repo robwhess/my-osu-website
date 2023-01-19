29ec3e66647ba277d488a51a52fdc5feb78e6404
Rob Hess <hessro@oregonstate.edu>
2023-01-19 11:14:16 -0800

    Update Sumer's hours for CS 494, add Zoom link.

--- src/data/courses/w23/cs494/tas.js
+++ src/data/courses/w23/cs494/tas.js
@@ -3,39 +3,45 @@ const tas = [
     name: 'Sumer Patel',
     email: 'patelsum@oregonstate.edu',
     appointmentsLink: 'https://calendar.google.com/calendar/u/0/selfsched?sstoken=UUZROFBnTkR6Z090fGRlZmF1bHR8MmQ2N2IzMTNjYjEwZjE3M2VkOGE4NTgzOWY0NjgyZGU',
-    // videoConferenceLink: '',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
     officeHours: [
       {
         day: 'Mon.',
         time: '9:00 &ndash; 11:00am',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       },
       {
         day: 'Wed.',
         time: '9:00 &ndash; 11:00am',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       },
       {
         day: 'Thurs.',
         time: '9:00 &ndash; 10:00am',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       },
       {
         day: 'Fri.',
         time: '2:00 &ndash; 3:00pm',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       }
     ],
     gradingHours: [
       {
         day: 'Thurs.',
         time: '3:00 &ndash; 7:00pm',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       },
       {
         day: 'Fri.',
         time: '3:00 &ndash; 4:00pm',
-        location: 'KEC Atrium'
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/4637552622?pwd=V1lZMm1GRlpKMzlGREdFb2dsMkdYdz09',
       }
     ]
   }
