0a20fbbe2a87c5e127bfefe01377010fe5459321
Rob Hess <hessro@oregonstate.edu>
2023-02-10 11:09:51 -0800

    Replace deprecated call to url.parse().

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import url from 'url';
 
 import App from './App';
 import pkg from '../package.json';
@@ -10,7 +9,7 @@ import pkg from '../package.json';
  * Figure out base pathname based on "homepage" property in package.json.
  */
 const siteURL = pkg.homepage;
-const basename = siteURL ? url.parse(siteURL).pathname : '';
+const basename = siteURL ? new URL(siteURL).pathname : '';
 
 ReactDOM.render(
   <BrowserRouter basename={basename}>
