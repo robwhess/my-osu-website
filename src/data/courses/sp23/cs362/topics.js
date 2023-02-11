4d83cc6607ae784a88f44e7242d192ca0514fc4a
Rob Hess <hessro@oregonstate.edu>
2023-02-11 11:12:42 -0800

    Add more CS 362 material.

--- src/data/courses/sp23/cs362/topics.js
+++ src/data/courses/sp23/cs362/topics.js
@@ -33,19 +33,29 @@ const topics = [
                 description: 'Adam Bender (Chapter 11 of Software Engineering at Google)'
             },
             {
-                title: 'Testing Implementation Details',
-                link: 'https://kentcdodds.com/blog/testing-implementation-details',
+                title: 'Test Behavior, Not Implementation',
+                link: 'https://testing.googleblog.com/2013/08/testing-on-toilet-test-behavior-not.html',
+                description: 'Andrew Trenk (Google Testing Blog)'
+            },
+            {
+                title: 'Different types of testing and the testing pyramid',
+                link: 'https://learn.microsoft.com/en-us/training/modules/visual-studio-test-concepts/4-different-types-of-testing',
+                description: 'Microsoft Training'
+            },
+            {
+                title: 'TestPyramid',
+                link: 'https://martinfowler.com/bliki/TestPyramid.html',
+                description: 'Martin Fowler'
+            },
+            {
+                title: 'Write Tests.  Not too many.  Mostly integration.',
+                link: 'https://kentcdodds.com/blog/write-tests',
                 description: 'Kent C. Dodds'
             },
             {
-                title: '',
-                link: '',
-                description: ''
-            },
-            {
-                title: '',
-                link: '',
-                description: ''
+                title: 'Test-Driven Development',
+                link: 'https://www.jamesshore.com/v2/books/aoad2/test-driven_development',
+                description: 'James Shore and Shane Warden (from The Art of Agile Development)'
             }
         ]
     }
