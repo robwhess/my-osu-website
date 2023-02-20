6a714c722921b51a5aa7207793efd10b78e22bfc
Rob Hess <hessro@oregonstate.edu>
2023-02-20 10:46:47 -0800

    Add Redux code link to CS 494.  Update CS 494 topics.

--- src/data/courses/w23/cs494/topics.js
+++ src/data/courses/w23/cs494/topics.js
@@ -213,7 +213,6 @@ const topics = [
   {
     title: 'Styling Components with Emotion',
     weeks: [ 6 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -266,17 +265,18 @@ const topics = [
 
   {
     title: 'Centralizing Application State with Redux',
-    weeks: [ 6, 7 ],
+    weeks: [ 7 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1nYBJTdCKoAbP_pKxjBfSqjozDAPqH5ppiOyh6ZjUjPs/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs499-w22/using-redux',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs494-w23/using-redux',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
