441e9c71c41a02e2ce7527802bbf50ef23428213
Rob Hess <hessro@oregonstate.edu>
2023-02-28 12:27:57 -0800

    Post Room code link to CS 492.  Update CS 492 topics.

--- src/data/courses/w23/cs492/topics.js
+++ src/data/courses/w23/cs492/topics.js
@@ -335,7 +335,6 @@ const topics = [
   {
     title: 'Storing Settings with the Preference Library',
     weeks: [ 7 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -374,16 +373,17 @@ const topics = [
   {
     title: 'Storing Data using the Room Persistence Library',
     weeks: [ 8 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1PXJW9mwYRW1_bVG54bXhm4o8YQu8KxFjIFAP1WPz7Ms/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs492-w22/GitHubSearchWithRoom',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs492-w23/GitHubSearchWithRoom',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
