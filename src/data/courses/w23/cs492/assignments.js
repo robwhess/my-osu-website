0548c8116e0ab973e2835cd6a017ef1886e776f8
Rob Hess <hessro@oregonstate.edu>
2023-02-28 11:40:54 -0800

    Publish CS 492 assignment 4.

--- src/data/courses/w23/cs492/assignments.js
+++ src/data/courses/w23/cs492/assignments.js
@@ -34,17 +34,17 @@ const assignments = {
         'Demo due by 11:59pm, Monday 3/13/2023'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; A weather app with stored data and a navigation drawer',
-    //   due: {
-    //     day: '~~Monday, 3/7/2022~~ **Wednesday, 3/9/2022**',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Friday 3/18/2022'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/TXazjpJY',
+      title: 'Assignment 4 &ndash; A weather app with stored data and a navigation drawer',
+      due: {
+        day: 'Monday, 3/13/2023',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Friday 3/24/2023'
+      ]
+    }
   ],
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
 };
