928d13c64c58f6ee59dfdcc84baf1f6ebf9fb1d3
Rob Hess <hessro@oregonstate.edu>
2023-03-31 16:55:47 -0700

    Publish office hours for spring term.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -1,18 +1,18 @@
 const officeHours = [
   {
     hours: [
-      // {
-      //   day: 'Wed.',
-      //   time: '10:30am&ndash;12:30pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Thurs.',
-      //   time: '10:30&ndash;11:30am',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
+      {
+        day: 'Wed.',
+        time: '10:30am&ndash;12:00pm',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Thurs.',
+        time: '10:00&ndash;11:30am',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      }
     ]
   }
 ];
