ca7b13607e93c458dddc99f6334888828f9dcef7
Rob Hess <hessro@oregonstate.edu>
2023-04-04 10:04:53 -0700

    Post CS 362 assignment 1.

--- src/data/courses/sp23/cs362/assignments.js
+++ src/data/courses/sp23/cs362/assignments.js
@@ -1,17 +1,17 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; ',
-    //   due: {
-    //     day: 'Monday, 4/18/2022',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm on Monday, 5/2/2022'
-    //   ]
-    // }
+    {
+      link : 'https://docs.google.com/document/d/1UWmHHgnvPowSBN8bc_UAhZ3YBkyS-Wg5YQyGRYInW20/edit?usp=sharing',
+      title: 'Assignment 1 &ndash; Using Git and GitHub',
+      due: {
+        day: 'Monday, 4/17/2023',
+        time: '11:59pm'
+      },
+      notes: [
+        'Grading demo due by 11:59pm on Monday, 5/1/2023'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
