55587cd1d1e5cc46242b78fbbeae9cd304003d05
Rob Hess <hessro@oregonstate.edu>
2023-04-26 13:31:30 -0700

    Update CS 480 topics.  Add Bison code link.

--- src/data/courses/sp23/cs480/topics.js
+++ src/data/courses/sp23/cs480/topics.js
@@ -107,16 +107,17 @@ const topics = [
   {
     title: 'The Bison Parser Generator',
     weeks: [ 4, 5 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/19LCYNojTimpgpvrZu-pawIj0wfRXKZCKHVapwS2BxUI/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs480-sp22/bison',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs480-sp23/bison',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
