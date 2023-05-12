37c2147136929b3467083cc0923cdcfd767a0d42
Rob Hess <hessro@oregonstate.edu>
2023-05-11 17:00:56 -0700

    Publish CS 493 final project.

--- src/data/courses/sp23/cs493/finalProject.js
+++ src/data/courses/sp23/cs493/finalProject.js
@@ -3,14 +3,14 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1wWgiGouapjrf3Jw8j6tD1XJ2NzJDhfMrDLTrDfMwDWg/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1A-i0wsm3nTcKpkzOjcajy1OpDV1ZXcUPYJeo1vJ9tjQ/edit?usp=sharing',
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: 'https://docs.google.com/document/d/1dWXr_vJyXkq1Gc0nCpB__C5M8Hz8wqn8JwqDY3oerf4/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1Jfqp3Kfb1xm8SNc7Xmj33PniBbePLDCoCCqCz-FaY50/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Wednesday, 5/11/2022',
+        day: 'Wednesday, 5/24/2023',
         location: 'on Canvas'
       }
     },
@@ -27,29 +27,29 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/1Vdlsi8qLSPLA_jQsvtkK4udcSGKwug5ogjz9-0cg0RU/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1UXWOtWWogK4mZqdwUIBct57iIcJ_9EEDEAIxurLPAAs/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 6/10/2022',
+        day: 'Friday, 6/16/2023',
         location: 'on Canvas'
       },
       notes: [
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    {
-      link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to run your project from one of your team\'s computers.',
-        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
-        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
-      ]
-    }
+    // {
+    //   link : 'https://calendar.google.com/calendar/selfsched?sstoken=UU5lX0wwMmRLWll3fGRlZmF1bHR8YTY3MWY4YzUxZWQ4ZmMzY2ZiY2Y1NmM0ZjkxYjJjODQ',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to run your project from one of your team\'s computers.',
+    //     'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+    //     'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete RESTful API.  You can find more info below.'
