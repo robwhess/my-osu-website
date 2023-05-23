8d68cfc9be950ff81ddbeb6604feac4f950d292e
Rob Hess <hessro@oregonstate.edu>
2023-05-23 14:30:49 -0700

    Publish CS 493 assignment 4.

--- src/data/courses/sp23/cs493/assignments.js
+++ src/data/courses/sp23/cs493/assignments.js
@@ -37,17 +37,17 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; File uploads and offline work',
-    //   due: {
-    //     day: 'Tuesday, 5/31/2022',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 5:00pm on Friday, 6/10/2022'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/qI7pKtXC',
+      title: 'Assignment 4 &ndash; File uploads and offline work',
+      due: {
+        day: 'Monday, 6/5/2023',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm on Friday, 6/16/2023'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
