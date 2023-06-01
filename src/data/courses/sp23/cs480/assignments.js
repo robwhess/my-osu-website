9c9f7d7412f93269e0041b5257857e5b6b8dd5d0
Rob Hess <hessro@oregonstate.edu>
2023-05-31 21:52:58 -0700

    Publish CS 480 assignment 4.

--- src/data/courses/sp23/cs480/assignments.js
+++ src/data/courses/sp23/cs480/assignments.js
@@ -37,19 +37,19 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS480@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash;Code generation with LLVM',
-    //   due: {
-    //     day: 'Friday, 6/10/2022',
-    //     time: '5:00pm'
-    //   },
-    //   notes: [
-    //     '**This assignment will not be demoed.**',
-    //     '**Your grade for the assignment will be based completely on the results of the included auto-grading tests.**',
-    //     '**This assignment is now optional.**  If the grade you receive based on the autograding tests is higher than any of your other assignment grades, I will use it to replace your lowest grade from assignments 1-3.'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/0v9XLRTv',
+      title: 'Assignment 4 &ndash; Code generation with LLVM',
+      due: {
+        day: 'Friday, 6/16/2023',
+        time: '5:00pm'
+      },
+      notes: [
+        '**This assignment will not be demoed.**',
+        '**Your grade for the assignment will be based completely on the results of the included auto-grading tests.**',
+        // '**This assignment is now optional.**  If the grade you receive based on the autograding tests is higher than any of your other assignment grades, I will use it to replace your lowest grade from assignments 1-3.'
+      ]
+    }
   ],
 
   preamble: "Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline."
