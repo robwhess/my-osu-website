f94a6a24c5efad01e79297aafbed92b9354ee0ab
Rob Hess <hessro@oregonstate.edu>
2023-09-28 14:02:31 -0700

    Post CS 261 assignment 1.

--- src/data/courses/f23/cs261/assignments.js
+++ src/data/courses/f23/cs261/assignments.js
@@ -1,17 +1,17 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
-    //   due: {
-    //     day: 'Monday, 10/17/2022',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Monday 10/31/2022'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/HhGtXyLp',
+      title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
+      due: {
+        day: 'Monday, 10/23/2023',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Monday 11/6/2023'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 2 &ndash; Stacks and queues',
