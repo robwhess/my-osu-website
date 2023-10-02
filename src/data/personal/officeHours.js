4212ba2e1550ab3eca98f985d76fe075ede745c2
Rob Hess <hessro@oregonstate.edu>
2023-10-02 09:46:38 -0700

    Publish office hours.  Publish CS 261 week 1 recitation.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,20 +2,17 @@ const officeHours = [
   {
     hours: [
       {
-        day: "TBD"
+        day: 'Wed.',
+        time: '11:05am&ndash;12:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Thurs.',
+        time: '2:00&ndash;3:30am',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       }
-      // {
-      //   day: 'Wed.',
-      //   time: '10:30am&ndash;12:00pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Thurs.',
-      //   time: '10:00&ndash;11:30am',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
     ]
   }
 ];
