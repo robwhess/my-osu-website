c9259a98c2681f2aac22fd30eabb6caf255b784f
Rob Hess <hessro@oregonstate.edu>
2023-10-03 17:45:41 -0700

    Update CS 290 TA info.

--- src/data/courses/f23/cs290/tas.js
+++ src/data/courses/f23/cs290/tas.js
@@ -1,23 +1,44 @@
 const taInfo = [
   {
-    name: 'Sagar Karki',
-    email: 'karkisa@oregonstate.edu',
-    // videoConferenceLink: 'https://oregonstate.zoom.us/j/3974128885?pwd=OXh3cGZXRFIrRzc0SmVrVVJZRzhaZz09',
+    name: 'Disha Kanavikar',
+    email: 'kanavikd@oregonstate.edu',
+    // videoConferenceLink: '',
     officeHours: [
       // {
       //   day: 'Tues.',
-      //   time: '10:00am &ndash; 2:00pm',
+      //   time: '12:00 &ndash; 4:00pm',
       //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/3974128885?pwd=OXh3cGZXRFIrRzc0SmVrVVJZRzhaZz09'
+      //   videoConferenceLink: ''
       // }
     ],
     gradingHours: [
       // {
-      //   day: '',
-      //   time: ' &ndash; ',
-      //   appointmentsLink: '',
+      //   day: 'Thurs.',
+      //   time: '12:00 &ndash; 4:00pm',
+      //   location: 'Zoom',
       //   videoConferenceLink: ''
-      // },
+      // }
+    ]
+  },
+  {
+    name: 'Sagar Karki',
+    email: 'karkisa@oregonstate.edu',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/3974128885?pwd=OXh3cGZXRFIrRzc0SmVrVVJZRzhaZz09',
+    officeHours: [
+      {
+        day: 'Tues.',
+        time: '12:00 &ndash; 4:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3974128885?pwd=OXh3cGZXRFIrRzc0SmVrVVJZRzhaZz09'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'Thurs.',
+        time: '12:00 &ndash; 4:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3974128885?pwd=OXh3cGZXRFIrRzc0SmVrVVJZRzhaZz09'
+      }
     ]
   },
   {
@@ -44,11 +65,21 @@ const taInfo = [
     email: 'nguyemi4@oregonstate.edu',
     // appointmentsLink: '',
     officeHours: [
-      // {
-      //   day: 'Fri.',
-      //   time: '11:00am &ndash; 2:00pm',
-      //   location: 'KEC Atrium',
-      // }
+      {
+        day: 'Tues.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Thurs.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Fri.',
+        time: '3:00 &ndash; 5:00pm',
+        location: 'KEC Atrium',
+      }
     ],
     gradingHours: [
       // {
