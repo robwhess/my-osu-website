b78fa2e260771a17762f49a523ee2759e719c3af
Rob Hess <hessro@oregonstate.edu>
2023-11-17 18:08:55 -0800

    Publish CS 261 assignment 4 and week 8 recitation.

--- src/data/courses/f23/cs261/assignments.js
+++ src/data/courses/f23/cs261/assignments.js
@@ -37,17 +37,18 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; Priority queues',
-    //   due: {
-    //     day: 'Monday, 11/28/2022',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 11:59pm, Friday 12/9/2022',
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/bL1hR0D7',
+      title: 'Assignment 4 &ndash; Priority queues',
+      due: {
+        day: 'Monday, 12/4/2023',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 11:59pm, Friday 12/15/2023',
+        '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/)'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
--- src/data/courses/f23/cs261/recitations.js
+++ src/data/courses/f23/cs261/recitations.js
@@ -126,7 +126,6 @@ const recitations = {
 
     {
       title: 'Week 7: Recursion',
-      isCurrent: true,
       resources: [
         {
           title: 'Recitation exercise worksheet',
@@ -140,6 +139,14 @@ const recitations = {
       ]
     },
 
+    {
+      title: 'Week 8: No Recitation',
+      isCurrent: true,
+      notes: [
+        'No recitation this week due to the Thanksgiving holiday.'
+      ]
+    },
+
     // {
     //   title: 'Week 8: BST and AVL Tree Comparison',
     //   resources: [
@@ -147,13 +154,6 @@ const recitations = {
     //       title: 'Recitation exercise worksheet',
     //       link: 'https://docs.google.com/document/d/1pNBpZePXE3-Ml5xoc5uMkuleGdkhS42e43HSOwJ4UuI/edit?usp=sharing'
     //     }
-    //   ]
-    // },
-
-    // {
-    //   title: 'Week 9: No Recitation',
-    //   notes: [
-    //     'No recitation this week due to the Thanksgiving holiday.'
     //   ]
     // },
 
