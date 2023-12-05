237155ae3850d840694832560f379d9476832da4
Rob Hess <hessro@oregonstate.edu>
2023-12-05 15:32:17 -0800

    Publish CS 261 final exam.

--- src/data/courses/f23/cs261/finalExam.js
+++ src/data/courses/f23/cs261/finalExam.js
@@ -2,11 +2,11 @@ const finalExam = {
   title: 'Final Exam',
   assignments: [
     {
-      link : '',
+      link : 'https://docs.google.com/document/d/15XGlBTrNXJJmcxsXF7f89W5XpYftB9hePNRrftgd_lE/edit?usp=sharing',
       title: 'Final Exam',
       due: {
         time: '11:59pm',
-        day: 'Tuesday, 12/6/2022',
+        day: 'Wednesday, 12/13/2023',
         location: 'on Canvas'
       }
     }
