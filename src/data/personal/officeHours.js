241344ecfe92bbccc2ac8906d462c4202b507aa8
Rob Hess <hessro@oregonstate.edu>
2024-01-11 11:53:10 -0800

    Post office hours for w24.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -2,20 +2,17 @@ const officeHours = [
   {
     hours: [
       {
-        day: "TBD"
+        day: 'Wed.',
+        time: '10:30am&ndash;12:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Thurs.',
+        time: '11:00am&ndash;12:30pm',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       }
-      // {
-      //   day: 'Wed.',
-      //   time: '11:05am&ndash;12:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Thurs.',
-      //   time: '2:00&ndash;3:30pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
     ]
   }
 ];
