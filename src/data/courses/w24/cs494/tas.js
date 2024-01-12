a9ab67856555aad1d6af6431c63036fb7ceeb869
Rob Hess <hessro@oregonstate.edu>
2024-01-12 11:56:29 -0800

    Publish CS 494 TA office hours.

--- src/data/courses/w24/cs494/tas.js
+++ src/data/courses/w24/cs494/tas.js
@@ -5,20 +5,25 @@ const tas = [
     // appointmentsLink: '',
     // videoConferenceLink: '',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '9:00 &ndash; 11:00am',
-      //   location: 'Zoom',
-      //   videoConferenceLink: '',
-      // },
+      {
+        day: 'Tues.',
+        time: '9:00am &ndash; 1:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99724023326?pwd=aVBuWWVNamdrL0RMeHZ1QzN2TzNTQT09',
+      },
     ],
     gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '3:00 &ndash; 5:00pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: '',
-      // },
+      {
+        day: 'Thurs.',
+        time: '9:00am &ndash; 1:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/99045390205?pwd=TENCRnBobERoV3ZNRG5aNVlTSWJiQT09',
+      },
+      {
+        day: 'Fri.',
+        time: '1:00 &ndash; 3:00pm',
+        location: 'KEC Atrium',
+      }
     ]
   },
   {
@@ -27,12 +32,18 @@ const tas = [
     // appointmentsLink: '',
     // videoConferenceLink: '',
     officeHours: [
-      // {
-      //   day: 'Mon.',
-      //   time: '9:00 &ndash; 11:00am',
-      //   location: 'Zoom',
-      //   videoConferenceLink: '',
-      // },
+      {
+        day: 'Tues.',
+        time: '4:30 &ndash; 6:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+      },
+      {
+        day: 'Thurs.',
+        time: '4:30 &ndash; 6:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+      },
     ],
     gradingHours: [
       // {
