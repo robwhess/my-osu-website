78af43a4a524f47196815d3b34153c61e1b67913
Rob Hess <hessro@oregonstate.edu>
2024-01-30 12:54:32 -0800

    Update CS 492 topics.  Add RecyclerView code link.

--- src/data/courses/w24/cs492/topics.js
+++ src/data/courses/w24/cs492/topics.js
@@ -103,7 +103,6 @@ const topics = [
   {
     title: 'Getting Started with Android Studio',
     weeks: [ 2 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -126,7 +125,6 @@ const topics = [
   {
     title: 'Our First Android App: To-Dos',
     weeks: [ 2 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -160,16 +158,17 @@ const topics = [
   {
     title: 'RecyclerView',
     weeks: [ 3 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1OGRgzRL0KA3xdhmNnI8d5pcW9J1CO18tYxYFicghHH0/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs492-w23/RecyclerViewToDos',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs492-599-w24/RecyclerViewToDos',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
