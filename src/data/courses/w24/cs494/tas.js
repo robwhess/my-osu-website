673130b091fab4ae3f91e2332b11b03963ee9997
Rob Hess <hessro@oregonstate.edu>
2024-01-29 16:28:59 -0800

    Update Samisha's grading hours link for CS 494.

--- src/data/courses/w24/cs494/tas.js
+++ src/data/courses/w24/cs494/tas.js
@@ -50,13 +50,13 @@ const tas = [
         day: 'Wed.',
         time: '10:00am &ndash; 1:00pm',
         location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/93096057397?pwd=WjJ2SDRTdU1LWlYyMDY2VUhoZk5aUT09',
       },
       {
         day: 'Fri.',
         time: '10:00am &ndash; 1:00pm',
         location: 'Zoom',
-        videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/93096057397?pwd=WjJ2SDRTdU1LWlYyMDY2VUhoZk5aUT09',
       }
     ]
   }
