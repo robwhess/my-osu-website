494609e8794d755aa415fd4fa56a55c43011e62c
Rob Hess <hessro@oregonstate.edu>
2024-02-13 13:05:09 -0800

    Update CS 492 topics.  Add ViewModel code link.

--- src/data/courses/w24/cs492/topics.js
+++ src/data/courses/w24/cs492/topics.js
@@ -240,16 +240,17 @@ const topics = [
   {
     title: 'The Activity Lifecycle and the ViewModel Architecture',
     weeks: [ 5 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/18PdfWxcWt8LwIKByggbWQvK8JyR9KRcS5JJBdFed-4c/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs492-w23/LifecycleAwareGitHubSearch',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs492-599-w24/ViewModelGitHubSearch',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
