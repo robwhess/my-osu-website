91e92598d4ab033334b5470b08fe244baa1ca41e
Rob Hess <hessro@oregonstate.edu>
2024-02-16 09:52:05 -0800

    Publish CS 494 final project info.

--- src/data/courses/w24/cs494/finalProject.js
+++ src/data/courses/w24/cs494/finalProject.js
@@ -3,20 +3,24 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1n92AhtzF-rgXGoBcFR-c58P84qk1-L8_LBhYByZrlEM/edit?usp=sharing'
+      link: 'https://docs.google.com/document/d/1DW5ts7pzhZWh8eAdtZBEEx2W_FeGxrA2vW4ttJ9X8IU/edit?usp=sharing'
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: 'https://docs.google.com/document/d/1cMVJeF2fpwf8S6cMwEjDwM-GKwS0KtJPnDot9KuzcFE/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1krznXS51EIflwz-gafbm3cFde5ph5s_RzRIECG3H-J8/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Friday, 2/24/2023',
+        day: 'Friday, 3/1/2024',
         location: 'on Canvas'
-      }
+      },
+      notes: [
+        'Use [this Ed thread]() to help connect with potential teammates.',
+        'Submit [this form]() if you just want to be assigned to a random team.'
+      ]
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/a/xRupV__C',
+      link: 'https://classroom.github.com/a/kfyCixq_',
       due: {
         time: 'at the time of your grading demo',
         day: 'during finals week'
@@ -27,29 +31,29 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/1ZckPMFKYmj6SByScxoNXYrJ750UYJFiwA74Vg3ZSL6Q/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1eVfHzcenc8cUgrFEMb3nRK1tOm5iWnOtgGhFBMO-0UE/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/24/2023',
+        day: 'Friday, 3/22/2024',
         location: 'on Canvas'
       },
       notes: [
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    {
-      link : 'https://outlook.office365.com/owa/calendar/CS494FinalProjectDemos@OregonStateUniversity.onmicrosoft.com/bookings/s/qD4cHizkNkmuEh3Cf2cxKw2',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo unless prior arrangements have been made with Hess.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to run your project from one of your team\'s computers.',
-        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
-        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
-      ]
-    }
+    // {
+    //   link : '',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo unless prior arrangements have been made with Hess.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to run your project from one of your team\'s computers.',
+    //     'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+    //     'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete React-based web application that serves dynamic, interactive data from an API.  You can find more info below.'
