76ebfb49900a736fdf590f06e69b9dc2aa55e6d6
Rob Hess <hessro@oregonstate.edu>
2024-02-27 18:41:35 -0800

    Publish CS 494 assignment 4.

--- src/data/courses/w24/cs494/assignments.js
+++ src/data/courses/w24/cs494/assignments.js
@@ -48,17 +48,18 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS494@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; A Redux-powered shopping app',
-    //   due: {
-    //     day: 'Monday, 3/6/2023',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Demo due by 5:00pm, Friday 3/24/2023'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/VEKDmqz2',
+      title: 'Assignment 4 &ndash; A Redux-powered shopping app',
+      due: {
+        day: 'Monday, 3/11/2024',
+        time: '11:59pm'
+      },
+      notes: [
+        'Demo due by 5:00pm, Friday 3/22/2024',
+        '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS494@OregonStateUniversity.onmicrosoft.com/bookings/)'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
