76b9ece8ea163d413a5eba574d841fce9916e26d
Rob Hess <hessro@oregonstate.edu>
2024-03-29 14:41:47 -0700

    Update CS 362 TAs.

--- src/data/courses/sp24/cs362/tas.js
+++ src/data/courses/sp24/cs362/tas.js
@@ -1,5 +1,26 @@
 const taInfo = [
   {
+    name: 'Soumiki Chattopadhyay',
+    email: 'chattoso@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
+    // videoConferenceLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '4:30 &ndash; 6:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: ''
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '1:00 &ndash; 4:30pm',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  },
+  {
     name: 'Apurva Kokate',
     email: 'kokatea@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
@@ -44,27 +65,6 @@ const taInfo = [
   {
     name: 'Yunhan Qiao',
     email: 'qiaoy@oregonstate.edu',
-    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
-    // videoConferenceLink: '',
-    officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '4:30 &ndash; 6:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: ''
-      // }
-    ],
-    gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '1:00 &ndash; 4:30pm',
-      //   location: 'KEC Atrium'
-      // }
-    ]
-  },
-  {
-    name: 'Avani Sathe',
-    email: 'sathea@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
     // videoConferenceLink: '',
     officeHours: [
