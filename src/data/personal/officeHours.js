a167e89aec13a69e0860afc5e46e3e2236e5f081
Rob Hess <hessro@oregonstate.edu>
2024-04-04 10:35:36 -0700

    Post my office hours for sp24.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -1,19 +1,18 @@
 const officeHours = [
   {
     hours: [
-      { day: "TBD" }
-      // {
-      //   day: 'Wed.',
-      //   time: '10:30am&ndash;12:00pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Thurs.',
-      //   time: '11:00am&ndash;12:30pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
+      {
+        day: 'Wed.',
+        time: '2:00&ndash;3:30pm',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Thurs.',
+        time: '10:30am&ndash;12:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      }
     ]
   }
 ];
