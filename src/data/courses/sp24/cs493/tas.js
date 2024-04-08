edc612d5e36b6ed9424398e17a7d6e804ead538c
Rob Hess <hessro@oregonstate.edu>
2024-04-08 13:05:18 -0700

    Post CS 493 TA hours.

--- src/data/courses/sp24/cs493/tas.js
+++ src/data/courses/sp24/cs493/tas.js
@@ -2,66 +2,90 @@ const taInfo = [
   {
     name: 'Jun Chen',
     email: 'chenju3@oregonstate.edu',
-    // videoConferenceLink: '',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/3377073373?pwd=MTViK0pIQTk5YklKQWpBSjhrYis2QT09',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/',
-    // officeHours: [
-    //   {
-    //     day: 'Tues.',
-    //     time: '10:00am &ndash; 12:00pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   }
-    // ],
-    // gradingHours: [
-    //   {
-    //     day: 'Mon.',
-    //     time: '9:00am &ndash; 1:00pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   }
-    // ]
+    officeHours: [
+      {
+        day: 'Tues.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3377073373?pwd=MTViK0pIQTk5YklKQWpBSjhrYis2QT09'
+      },
+      {
+        day: 'Wed.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3377073373?pwd=MTViK0pIQTk5YklKQWpBSjhrYis2QT09'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'Mon.',
+        time: '4:00 &ndash; 7:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3377073373?pwd=MTViK0pIQTk5YklKQWpBSjhrYis2QT09'
+      },
+      {
+        day: 'Fri.',
+        time: '4:00 &ndash; 7:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/3377073373?pwd=MTViK0pIQTk5YklKQWpBSjhrYis2QT09'
+      }
+    ]
   },
   {
     name: 'Disha Kanavikar',
     email: 'kanavikd@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/',
     // videoConferenceLink: '',
-    // officeHours: [
-    //   {
-    //     day: 'Mon.',
-    //     time: '4:00 &ndash; 6:00pm',
-    //     location: 'KEC Atrium'
-    //   }
-    // ],
-    // gradingHours: [
-    //   {
-    //     day: 'Tues.',
-    //     time: '8:00am &ndash; 12:00pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   }
-    // ]
+    officeHours: [
+      {
+        day: 'Tues.',
+        time: '8:00 &ndash; 10:00pm',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Wed.',
+        time: '8:00 &ndash; 10:00pm',
+        location: 'KEC Atrium'
+      }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '8:00am &ndash; 12:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: ''
+      // }
+    ]
   },
   {
     name: 'Samisha Khurana',
     email: 'khuranas@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/',
-    // videoConferenceLink: '',
-    // officeHours: [
-    //   {
-    //     day: 'Mon.',
-    //     time: '4:00 &ndash; 6:00pm',
-    //     location: 'KEC Atrium'
-    //   }
-    // ],
-    // gradingHours: [
-    //   {
-    //     day: 'Tues.',
-    //     time: '8:00am &ndash; 12:00pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   }
-    // ]
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/92689414193?pwd=ZXlHRGJsY0xxNmVtNlQ2dXNraDB4QT09',
+    officeHours: [
+      {
+        day: 'Wed.',
+        time: '3:00 &ndash; 5:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92689414193?pwd=ZXlHRGJsY0xxNmVtNlQ2dXNraDB4QT09'
+      },
+      {
+        day: 'Fri.',
+        time: '3:00 &ndash; 5:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/92689414193?pwd=ZXlHRGJsY0xxNmVtNlQ2dXNraDB4QT09'
+      }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '8:00am &ndash; 12:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: ''
+      // }
+    ]
   }
 ];
 
