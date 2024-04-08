975fa82f15901689a0ca45791309ebfcba37537c
Rob Hess <hessro@oregonstate.edu>
2024-04-08 12:58:50 -0700

    Post CS 362 TA hours.

--- src/data/courses/sp24/cs362/tas.js
+++ src/data/courses/sp24/cs362/tas.js
@@ -3,21 +3,34 @@ const taInfo = [
     name: 'Soumiki Chattopadhyay',
     email: 'chattoso@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
-    // videoConferenceLink: '',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '4:30 &ndash; 6:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '1:00 &ndash; 2:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      },
+      {
+        day: 'Thurs.',
+        time: '2:30 &ndash; 3:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '1:00 &ndash; 4:30pm',
-      //   location: 'KEC Atrium'
-      // }
+      {
+        day: 'Wed.',
+        time: '6:00 &ndash; 8:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      },
+      {
+        day: 'Thurs.',
+        time: '6:00 &ndash; 7:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      }
     ]
   },
   {
@@ -26,12 +39,18 @@ const taInfo = [
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
     // videoConferenceLink: '',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '4:30 &ndash; 6:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '5:00 &ndash; 7:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/97072944498?pwd=eEZGVjhYYUFEUWh2S3lWc2w4cDZiZz09'
+      },
+      {
+        day: 'Fri.',
+        time: '5:00 &ndash; 7:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/97072944498?pwd=eEZGVjhYYUFEUWh2S3lWc2w4cDZiZz09'
+      }
     ],
     gradingHours: [
       // {
@@ -68,19 +87,33 @@ const taInfo = [
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
     // videoConferenceLink: '',
     officeHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '4:30 &ndash; 6:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: ''
-      // }
+      {
+        day: 'Tues.',
+        time: '3:00 &ndash; 5:00pm',
+        location: 'KEC Atrium',
+      },
+      {
+        day: 'Thurs.',
+        time: '3:00 &ndash; 5:00pm',
+        location: 'KEC Atrium',
+      }
     ],
     gradingHours: [
-      // {
-      //   day: 'Tues.',
-      //   time: '1:00 &ndash; 4:30pm',
-      //   location: 'KEC Atrium'
-      // }
+      {
+        day: 'Tues.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Thurs.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'KEC Atrium'
+      },
+      {
+        day: 'Fri.',
+        time: '9:00 &ndash; 11:00am',
+        location: 'KEC Atrium'
+      }
     ]
   }
 ];
