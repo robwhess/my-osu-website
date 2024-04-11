3a0b960641bc23f3378c6be7d238f3b9cea3680e
Rob Hess <hessro@oregonstate.edu>
2024-04-11 10:47:23 -0700

    Publish Ryan's TA hours for CS 480.

--- src/data/courses/sp24/cs480/tas.js
+++ src/data/courses/sp24/cs480/tas.js
@@ -2,24 +2,48 @@ const taInfo = [
   {
     name: 'Ryan Roberts',
     email: 'roberyan@oregonstate.edu',
-    // videoConferenceLink: '',
+    videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS480@OregonStateUniversity.onmicrosoft.com/bookings/',
-    // officeHours: [
-    //   {
-    //     day: 'Wed.',
-    //     time: '9:00am &ndash; 12:00pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   }
-    // ],
-    // gradingHours: [
-    //   {
-    //     day: 'Tues.',
-    //     time: '9:00am &ndash; 12:30pm',
-    //     location: 'Zoom',
-    //     videoConferenceLink: ''
-    //   },
-    // ]
+    officeHours: [
+      {
+        day: 'Wed.',
+        time: '2:30 &ndash; 4:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      },
+      {
+        day: 'Fri.',
+        time: '4:00 &ndash; 6:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      }
+    ],
+    gradingHours: [
+      {
+        day: 'Mon.',
+        time: '2:30 &ndash; 4:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      },
+      {
+        day: 'Tues.',
+        time: '4:30 &ndash; 6:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      },
+      {
+        day: 'Wed.',
+        time: '12:00 &ndash; 12:30pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      },
+      {
+        day: 'Thurs.',
+        time: '10:30am &ndash; 12:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/2983687190?pwd=SHdzL1Z0MVlYYlVYZG9FZXpraTFGQT09'
+      }
+    ]
   }
 ];
 
