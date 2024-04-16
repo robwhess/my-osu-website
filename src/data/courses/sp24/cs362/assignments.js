fa820b04f655b716f2874c815f9e460f77e85b81
Rob Hess <hessro@oregonstate.edu>
2024-04-16 10:13:08 -0700

    Publish CS 362 assignment 2.

--- src/data/courses/sp24/cs362/assignments.js
+++ src/data/courses/sp24/cs362/assignments.js
@@ -13,18 +13,18 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 2 &ndash; Unit Testing and TDD',
-    //   due: {
-    //     day: 'Monday, 5/1/2023',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Grading demo due by 11:59pm on Monday, 5/15/2023',
-    //     '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/cgCdNk39',
+      title: 'Assignment 2 &ndash; Unit Testing and TDD',
+      due: {
+        day: 'Monday, 4/29/2024',
+        time: '11:59pm'
+      },
+      notes: [
+        'Grading demo due by 11:59pm on Monday, 5/13/2024',
+        '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 3 &ndash; Ingegration tests for UI-based apps',
