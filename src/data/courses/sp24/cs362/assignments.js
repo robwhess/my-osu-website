f77a2e0aa3589f6eff5b738f41225adefb6f66fe
Rob Hess <hessro@oregonstate.edu>
2024-04-30 08:56:40 -0700

    Publish CS 480 assignment 2 and CS 362 assignment 3.

--- src/data/courses/sp24/cs362/assignments.js
+++ src/data/courses/sp24/cs362/assignments.js
@@ -25,18 +25,18 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 3 &ndash; Ingegration tests for UI-based apps',
-    //   due: {
-    //     day: 'Monday, 5/15/2023',
-    //     time: '11:59pm'
-    //   },
-    //   notes: [
-    //     'Grading demo due by 11:59pm on Monday, 5/29/2023',
-    //     '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/-EtVVtgg',
+      title: 'Assignment 3 &ndash; Ingegration tests for UI-based apps',
+      due: {
+        day: 'Monday, 5/13/2024',
+        time: '11:59pm'
+      },
+      notes: [
+        'Grading demo due by 11:59pm on Monday, 5/27/2024',
+        '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/)'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 4 &ndash; End-to-end tests for a complete application',
