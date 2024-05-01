ecd5f4a8b9861bf4a006c82b8c2ba4f9761bbb14
Rob Hess <hessro@oregonstate.edu>
2024-05-01 12:38:53 -0700

    Update CS 362 topics.  Add test doubles code link.

--- src/data/courses/sp24/cs362/topics.js
+++ src/data/courses/sp24/cs362/topics.js
@@ -175,7 +175,6 @@ const topics = [
     {
         title: 'UI-Based Integration Testing',
         weeks: [ 4, 5 ],
-        isCurrent: true,
         resources: [
             {
                 title: 'Lecture notes',
@@ -221,17 +220,18 @@ const topics = [
 
     {
         title: 'Test Doubles',
-        weeks: [ 6, 7 ],
+        weeks: [ 5, 6 ],
+        isCurrent: true,
         resources: [
             {
                 title: 'Lecture notes',
                 link: 'https://docs.google.com/document/d/1NB775R2seFx5YmMeDeiCEnTCw3uGoEzDKGcHjkYAAvI/edit?usp=sharing'
             },
-            // {
-            //     title: 'Example code',
-            //     link: 'https://github.com/osu-cs362-sp23/test-doubles',
-            //     description: 'code written during lecture'
-            // }
+            {
+                title: 'Example code',
+                link: 'https://github.com/organizations/osu-cs362-sp24/repositories/new',
+                description: 'code written during lecture'
+            }
         ],
         readings: [
             {
