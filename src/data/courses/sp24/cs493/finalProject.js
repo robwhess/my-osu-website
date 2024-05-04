64fa8f7757bbafc27ae402ce78c28e5eea6952d4
Rob Hess <hessro@oregonstate.edu>
2024-05-03 17:02:57 -0700

    Publish CS 493/599 final project.

--- src/data/courses/sp24/cs493/finalProject.js
+++ src/data/courses/sp24/cs493/finalProject.js
@@ -3,53 +3,66 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: '',
+      link: 'https://docs.google.com/document/d/16oSDO0w1YrwdLy7KFsJLlFCngRaatwzYCxNJlRayB2I/edit?usp=sharing',
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: '',
+      link: 'https://docs.google.com/document/d/16U2uLCTHKwX7Cik-vzGQzh7TndFNNvej8DUD0Tq9fHc/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: '~~Wednesday, 5/24/2023~~ **Friday, 5/26/2023**',
+        day: 'Friday, 5/17/2024',
         location: 'on Canvas'
       }
     },
     {
       title: 'Deliverable #2: Code',
-      link: '',
+      link: 'https://classroom.github.com/a/OjlFa0Io',
       due: {
         time: 'at the time of your grading demo',
-        day: 'during finals week'
+        day: 'during finals week',
+        location: 'on GitHub'
       },
       notes: [
         'Use the link above to create a repo on GitHub for your team to use for the final project.'
       ]
     },
     {
+      title: 'Deliverable #2b: Public API URL (CS 599 only)',
+      link: 'https://canvas.oregonstate.edu/courses/1970824/assignments/9675384',
+      due: {
+        time: 'at the time of your grading demo',
+        day: 'during finals week',
+        location: 'on Canvas'
+      },
+      notes: [
+        'Grad teams must submit the public URL for their deployed API on Canvas.'
+      ]
+    },
+    {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: '',
+      link: 'https://docs.google.com/document/d/19EnHAvRkPQ5xLhGDXMvLf47DMxCRH9OtOslHHabMmwY/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 6/16/2023',
+        day: 'Friday, 6/14/2024',
         location: 'on Canvas'
       },
       notes: [
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    {
-      link : 'https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/s/XZlMAYOPmUybTIBR57AJ5w2',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo unless prior arrangements have been made with Hess.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to run your project from one of your team\'s computers.',
-        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
-        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
-      ]
-    }
+    // {
+    //   link : 'https://outlook.office365.com/owa/calendar/CS493@OregonStateUniversity.onmicrosoft.com/bookings/s/XZlMAYOPmUybTIBR57AJ5w2',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo unless prior arrangements have been made with Hess.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to run your project from one of your team\'s computers.',
+    //     'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+    //     'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please ask at least 24 hours in advance.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete RESTful API.  You can find more info below.'
