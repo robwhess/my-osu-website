d31aabf518c6e5c3872a490aaa688d832fa72881
Rob Hess <hessro@oregonstate.edu>
2024-05-07 19:41:54 -0700

    Publish CS 362 final project.

--- src/data/courses/sp24/cs362/finalProject.js
+++ src/data/courses/sp24/cs362/finalProject.js
@@ -1,46 +1,46 @@
 const finalProject = {
   title: 'Final Project',
   assignments: [
-    // {
-    //   title: 'Final Project Details',
-    //   link: ''
-    // },
-    // {
-    //   title: 'Deliverable #1: Final Project Team Formation',
-    //   link: '',
-    //   due: {
-    //     time: '11:59pm',
-    //     day: 'Monday, 5/22/2023',
-    //     location: 'on Canvas'
-    //   },
-    //   notes: [
-    //     'Use [this Ed thread]() to help connect with potential teammates.',
-    //     'Submit [this form]() if you just want to be assigned to a random team.'
-    //   ]
-    // },
-    // {
-    //   title: 'Deliverable #2: Code',
-    //   link: '',
-    //   due: {
-    //     time: 'at the time of your grading demo',
-    //     day: 'during finals week'
-    //   },
-    //   notes: [
-    //     'Use the link above to create a repo on GitHub for your team to use for the final project.'
-    //   ]
-    // },
-    // {
-    //   title: 'Deliverable #3: Final Project Team Evaluation',
-    //   link: '',
-    //   due: {
-    //     time: '5:00pm',
-    //     day: 'Friday, 6/16/2023',
-    //     location: 'on Canvas'
-    //   },
-    //   notes: [
-    //     'Note that this must be completed individually by every member of your team.'
-    //   ]
-    // },
+    {
+      title: 'Final Project Details',
+      link: 'https://docs.google.com/document/d/1nIxMUtNhLRL8CgnKtLTzn2oSitZQGPXjPQ-iImyYgaQ/edit?usp=sharing'
+    },
+    {
+      title: 'Deliverable #1: Final Project Team Formation',
+      link: 'https://docs.google.com/document/d/1MCZeCqP9ghF9k4FE06BgT9usf6b8Mcs9_hx1KG7ehtk/edit?usp=sharing',
+      due: {
+        time: '11:59pm',
+        day: 'Wednesday, 5/22/2024',
+        location: 'on Canvas'
+      },
+      notes: [
+        'Use [this Ed thread](https://edstem.org/us/courses/57635/discussion/4927602) to help connect with potential teammates.',
+        'Submit [this form](https://forms.gle/7FzZ5fKn5hYbUZpu7) if you just want to be assigned to a random team.'
+      ]
+    },
+    {
+      title: 'Deliverable #2: Code',
+      link: 'https://classroom.github.com/a/EbH1uor2',
+      due: {
+        time: 'at the time of your grading demo',
+        day: 'during finals week'
+      },
+      notes: [
+        'Use the link above to create a repo on GitHub for your team to use for the final project.'
+      ]
+    },
+    {
+      title: 'Deliverable #3: Final Project Team Evaluation',
+      link: 'https://docs.google.com/document/d/1R7F9Wl9cyznp1QMrToISyTOrsM1dzqJl3Ft5YZezw58/edit?usp=sharing',
+      due: {
+        time: '5:00pm',
+        day: 'Friday, 6/14/2024',
+        location: 'on Canvas'
+      },
+      notes: [
+        'Note that this must be completed individually by every member of your team.'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Final Project Demo Signup',
