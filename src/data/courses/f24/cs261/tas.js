3975a9a4eb2632917c7e9b6e6b02c18fc215d786
Rob Hess <hessro@oregonstate.edu>
2024-09-25 16:52:29 -0700

    Update CS 261 TAs.

--- src/data/courses/f24/cs261/tas.js
+++ src/data/courses/f24/cs261/tas.js
@@ -42,27 +42,6 @@ const taInfo = [
     ]
   },
   {
-    name: 'Kyla Daskalos',
-    email: 'daskalok@oregonstate.edu',
-    // videoConferenceLink: 'https://oregonstate.zoom.us/j/94167515520?pwd=ZC9mM3pXVnJBRHUrdmJpUmY5VUVMUT09',
-    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/',
-    officeHours: [
-      // {
-      //   day: 'Thurs.',
-      //   time: '8:00 &ndash; 11:00am',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94167515520?pwd=ZC9mM3pXVnJBRHUrdmJpUmY5VUVMUT09'
-      // }
-    ],
-    gradingHours: [
-      // {
-      //   day: 'Fri.',
-      //   time: '5:00 &ndash; 8:00',
-      //   location: 'KEC Atrium'
-      // }
-    ]
-  },
-  {
     name: 'Gabriel de Leon',
     email: 'deleong@oregonstate.edu',
     // videoConferenceLink: 'https://oregonstate.zoom.us/j/94167515520?pwd=ZC9mM3pXVnJBRHUrdmJpUmY5VUVMUT09',
@@ -125,6 +104,27 @@ const taInfo = [
       // }
     ]
   },
+  {
+    name: 'Riley Rice',
+    email: 'riceri@oregonstate.edu',
+    // videoConferenceLink: 'https://oregonstate.zoom.us/j/94167515520?pwd=ZC9mM3pXVnJBRHUrdmJpUmY5VUVMUT09',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/',
+    officeHours: [
+      // {
+      //   day: 'Thurs.',
+      //   time: '8:00 &ndash; 11:00am',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/94167515520?pwd=ZC9mM3pXVnJBRHUrdmJpUmY5VUVMUT09'
+      // }
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Fri.',
+      //   time: '5:00 &ndash; 8:00',
+      //   location: 'KEC Atrium'
+      // }
+    ]
+  }
 ];
 
 export default taInfo;
