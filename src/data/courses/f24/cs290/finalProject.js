863d9a98bdc7ec2e922f7cf48d22a29b4fe3fb85
Rob Hess <hessro@oregonstate.edu>
2024-11-05 10:19:59 -0800

    Publish CS 290 final project.

--- src/data/courses/f24/cs290/finalProject.js
+++ src/data/courses/f24/cs290/finalProject.js
@@ -3,24 +3,24 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1n_Emy40KNpSWkplRqWsLWnkoBrG5WHCax4yl1kr01Yo/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1Olh7d9fDRk0vKl1KJCzJ07Jzfw9yRtswt-06oAUX_rw/edit?usp=sharing',
     },
     {
       title: 'Deliverable #1: Final Project Team Formation and Project Ideas',
-      link: 'https://docs.google.com/document/d/19mbHKAFUKLNFDyK5F_jZaZjZCZq4kmJJAIB4PyQv_ak/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/12weNGWWYzaUZQtiKcFHIzMeva3HohRrIUFe-G-4ws5k/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Friday, 11/17/2023',
+        day: 'Friday, 11/15/2024',
         location: 'on Canvas'
       },
       notes: [
-        'Use [this Ed thread](https://edstem.org/us/courses/47913/discussion/3790885) to help connect with potential teammates.',
-        'Submit [this form](https://forms.gle/sSWs4VsCZH1EbyEE7) if you just want to be assigned to a random team.'
+        'Use [this Ed thread](https://edstem.org/us/courses/67738/discussion/5649474) to help connect with potential teammates.',
+        'Submit [this form](https://forms.gle/NuB23QfLFMdox6W17) if you just want to be assigned to a random team.'
       ]
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/a/Is1VF4_N',
+      link: 'https://classroom.github.com/a/KeEhiTY7',
       due: {
         time: 'at the time of your grading demo',
         day: 'during finals week'
@@ -31,29 +31,29 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/13WE4iOBHii3keSQR1U5C_WlrqSYmKhz0U1EVgbjXTDI/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1l4D-fICM7Xgk6ncRE07MnANcHH2g52DJRck_c8ay3HY/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 12/15/2023',
+        day: 'Friday, 12/13/2024',
         location: 'on Canvas'
       },
       notes: [
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    {
-      link : 'https://outlook.office365.com/owa/calendar/CS290@OregonStateUniversity.onmicrosoft.com/bookings/s/nbUAk1bWB0iTS5zaAVCMtg2',
-      title: 'Final Project Demo Signup',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.',
-        'All team members must attend your demo unless prior arrangements have been made with Hess.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to run your project from one of your team\'s computers.',
-        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
-        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please *email* at least 24 hours in advance.  **Do not rely only on requesting a remote demo by leaving a note when you sign up through Microsoft Bookings!**  These notes are buried and I won\'t see them.  Please email instead.'
-      ]
-    }
+    // {
+    //   link : 'https://outlook.office365.com/owa/calendar/CS290@OregonStateUniversity.onmicrosoft.com/bookings/s/nbUAk1bWB0iTS5zaAVCMtg2',
+    //   title: 'Final Project Demo Signup',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.',
+    //     'All team members must attend your demo unless prior arrangements have been made with Hess.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to run your project from one of your team\'s computers.',
+    //     'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+    //     'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please *email* at least 24 hours in advance.  **Do not rely only on requesting a remote demo by leaving a note when you sign up through Microsoft Bookings!**  These notes are buried and I won\'t see them.  Please email instead.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete web application that serves dynamic, interactive data that is stored in a database on the back end.  You can find more info below.'
