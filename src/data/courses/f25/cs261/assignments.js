5638bbc5e12e66b6702a7be99c3428beccde9cff
Rob Hess <hessro@oregonstate.edu>
2025-09-25 15:51:42 -0700

    Publish CS 261 assignment 1 and update topics.

--- src/data/courses/f25/cs261/assignments.js
+++ src/data/courses/f25/cs261/assignments.js
@@ -1,20 +1,20 @@
 const assignments = {
   title: 'Assignments',
   assignments: [
-    // {
-    //   link : '',
-    //   title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
-    //   // due: {
-    //   //   day: 'Monday, 10/23/2023',
-    //   //   time: '11:59pm'
-    //   // },
-    //   notes: [
-    //     'Preferred deadline: **Monday, 10/21/2024, 11:59pm**',
-    //     'Extension deadline: **Wednesday, 10/23/2024, 11:59pm** (no submissions after this)',
-    //     'Grading demo due: **Monday 11/4/2024, 11:59pm**',
-    //     '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/)'
-    //   ]
-    // },
+    {
+      link : 'https://classroom.github.com/a/44TYFKxn',
+      title: 'Assignment 1 &ndash; Dynamic arrays and linked lists',
+      // due: {
+      //   day: 'Monday, 10/23/2023',
+      //   time: '11:59pm'
+      // },
+      notes: [
+        'Preferred deadline: **Monday, 10/20/2025, 11:59pm**',
+        'Extension deadline: **Wednesday, 10/22/2025, 11:59pm** (no submissions after this)',
+        'Grading demo due: **Monday 11/3/2025, 11:59pm**',
+        // '[Sign up for a grading demo here](https://outlook.office365.com/owa/calendar/CS261@OregonStateUniversity.onmicrosoft.com/bookings/)'
+      ]
+    },
     // {
     //   link : '',
     //   title: 'Assignment 2 &ndash; Stacks and queues',
