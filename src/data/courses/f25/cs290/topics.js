e7c41b849ff27dc48b73a9bc213f7b6ba25f8eae
Rob Hess <hessro@oregonstate.edu>
2025-10-06 11:10:30 -0700

    Update CS 290 topics.

--- src/data/courses/f25/cs290/topics.js
+++ src/data/courses/f25/cs290/topics.js
@@ -56,7 +56,6 @@ const topics = [
   {
     title: 'HTML',
     weeks: [ 1 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -85,16 +84,17 @@ const topics = [
   {
     title: 'CSS',
     weeks: [ 2, 3, 4 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1DTpjDB2K4EF2sz4MwhPOYfC8CUhpZn9ykEVkwNZY8wM/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-f24/css',
-      //   description: 'GitHub repo for code written in lecture'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-f25/css',
+        description: 'GitHub repo for code written in lecture'
+      }
     ],
     readings: [
       {
