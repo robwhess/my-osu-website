708d385fd660a7b45b4b74225e504fba7332fb2c
Rob Hess <hessro@oregonstate.edu>
2025-11-17 12:55:44 -0800

    Publish CS 290 assignment 4.

--- src/data/courses/f25/cs290/assignments.js
+++ src/data/courses/f25/cs290/assignments.js
@@ -47,37 +47,21 @@ const assignments = {
         '[Sign up for a grading demo here](https://outlook.office.com/book/CS290@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
       ]
     },
-    // {
-    //   link : '',
-    //   title: 'Assignment 4 &ndash; Simple Node.js-based server',
-    //   // due: {
-    //   //   day: 'Monday, 11/20/2023',
-    //   //   time: '11:59pm'
-    //   // },
-    //   notes: [
-    //     '**This assignment is completely optional and will not be graded.**'
-    //     // 'Preferred deadline: **Tuesday, 11/12/2024, 11:59pm**',
-    //     // 'Extension deadline: ~~Wednesday, 11/13/2024~~ **Thursday, 11/14/2024, 11:59pm** (no submissions after this)',
-    //     // 'Assignment and Code Blog entry due at the same time.  **Submit Code Blog link on Canvas.**',
-    //     // 'Grading demo due: **Monday 11/25/2024, 11:59pm**',
-    //     // '[Sign up for a grading demo here](https://outlook.office.com/book/CS290@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
-    //   ]
-    // },
-    // {
-    //   link : '',
-    //   title: 'Assignment 5 &ndash; Web server using Express and Handlebars',
-    //   // due: {
-    //   //   day: 'Monday, 12/4/2023',
-    //   //   time: '11:59pm'
-    //   // },
-    //   notes: [
-    //     'Preferred deadline: **Monday, 12/2/2024, 11:59pm**',
-    //     'Extension deadline: **Wednesday, 12/4/2024, 11:59pm** (no submissions after this)',
-    //     'Assignment and Code Blog entry due at the same time.  **Submit Code Blog link on Canvas.**',
-    //     'Grading demo due: **Friday 12/13/2024, 11:59pm**',
-    //     '[Sign up for a grading demo here](https://outlook.office.com/book/CS290@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
-    //   ]
-    // }
+    {
+      link : 'https://classroom.github.com/a/hKvJJpUv',
+      title: 'Assignment 4 &ndash; Web server using Express and EJS',
+      // due: {
+      //   day: 'Monday, 12/4/2023',
+      //   time: '11:59pm'
+      // },
+      notes: [
+        'Preferred deadline: **Monday, 12/1/2025, 11:59pm**',
+        'Extension deadline: **Wednesday, 12/3/2025, 11:59pm** (no submissions after this)',
+        'Assignment and Code Blog entry due at the same time.  **Submit Code Blog link on Canvas.**',
+        'Grading demo due: **Friday 12/12/2025, 11:59pm**',
+        '[Sign up for a grading demo here](https://outlook.office.com/book/CS290@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled)'
+      ]
+    }
   ],
 
   preamble: 'Programming assignments will be managed via GitHub Classroom.  Following the links below will prompt you to sign in to GitHub and to create an assignment repository for yourself.  The assignment repository will at a minimum contain a `README.md` file containing the assignment description.  There may also be additional skeleton files in the repository to help you get started with the assignment.  To submit an assignment, make sure you have committed your solution code and pushed it to your GitHub repository before the assignment deadline.'
