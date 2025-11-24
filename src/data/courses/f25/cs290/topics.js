d76be0b345eda94b95ca97de5e5bf7d66418e5a2
Rob Hess <hessro@oregonstate.edu>
2025-11-24 13:20:49 -0800

    Update CS 290 topics.

--- src/data/courses/f25/cs290/topics.js
+++ src/data/courses/f25/cs290/topics.js
@@ -336,7 +336,6 @@ const topics = [
   {
     title: 'Server-Side JS: Serving with Express',
     weeks: [ 7, 8 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -424,16 +423,17 @@ const topics = [
   {
     title: 'Asynchronous Client-Server Communication and User-Generated Content',
     weeks: [ 10 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1mG9PAN18vuKIXPZXE0P4EVkOBtU9zjz4UqaIv6DnDfY/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs290-f24/user-content',
-      //   description: 'GitHub repo for code written in lecture'
-      // }
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs290-f25/user-content',
+        description: 'GitHub repo for code written in lecture'
+      }
     ],
     readings: [
       {
