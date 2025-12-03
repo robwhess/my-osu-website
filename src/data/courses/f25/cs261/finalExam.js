ccaad27be914f38f33d5acf13e3d44c664077ab6
Rob Hess <hessro@oregonstate.edu>
2025-12-02 20:35:03 -0800

    Publish CS 261 final exam interview signup info.

--- src/data/courses/f25/cs261/finalExam.js
+++ src/data/courses/f25/cs261/finalExam.js
@@ -12,6 +12,15 @@ const finalExam = {
       notes: [
         '**There is no extension deadline for the final exam.**  All submissions must be made by the date specified above.'
       ]
+    },
+    {
+      link : 'https://outlook.office.com/book/CS261@OregonStateUniversity.onmicrosoft.com/s/TWeB97vg5ku6JscUBqrsCw2?ismsaljsauthenabled',
+      title: 'Final Exam Grading Interview Signup',
+      notes: [
+        'Sign up for a final exam grading interview appointment using the link above.',
+        'Interviews will be held in person in Hess\'s office (KEC 1109).',
+        'Remote attendance at a final exam grading interview is possible *by request only*.  If you\'d like to attend your interview remotely via Zoom, please [submit this form](https://docs.google.com/forms/d/e/1FAIpQLSdj0b-GWy7_z8GVfFwuGt73bHFfmTI_LbfpS2McvrWOxmYMRA/viewform?usp=publish-editor) at least 24 hours in advance.'
+      ]
     }
   ],
 
