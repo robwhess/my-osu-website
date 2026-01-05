bd50a90500a96771f06e39bdc5c917bbfff0687e
Rob Hess <hessro@oregonstate.edu>
2026-01-05 14:13:29 -0800

    Post TA info for CS 492 and 494.

--- src/data/courses/w26/cs492/tas.js
+++ src/data/courses/w26/cs492/tas.js
@@ -1,26 +1,61 @@
 const tas = [
   {
-    name: 'TBD'
+    name: 'Beniamin Condrea',
+    email: 'condreab@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/',
+    officeHours: [
+    //   {
+    //     day: 'Tues.',
+    //     time: '12:00 &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ],
+    gradingHours: [
+    //   {
+    //     day: 'Wed.',
+    //     time: '11:00am &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ]
+  },
+  {
+    name: 'Elliot Foley',
+    email: 'foleelli@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/',
+    officeHours: [
+    //   {
+    //     day: 'Tues.',
+    //     time: '12:00 &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ],
+    gradingHours: [
+    //   {
+    //     day: 'Wed.',
+    //     time: '11:00am &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ]
+  },
+  {
+    name: 'Shubhangi Pandey',
+    email: 'pandeys@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/',
+    officeHours: [
+    //   {
+    //     day: 'Tues.',
+    //     time: '12:00 &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ],
+    gradingHours: [
+    //   {
+    //     day: 'Wed.',
+    //     time: '11:00am &ndash; 2:00pm',
+    //     location: 'KEC Atrium'
+    //   }
+    ]
   }
-  // {
-  //   name: '',
-  //   email: '@oregonstate.edu',
-  //   appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '12:00 &ndash; 2:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Wed.',
-  //       time: '11:00am &ndash; 2:00pm',
-  //       location: 'KEC Atrium'
-  //     }
-  //   ]
-  // }
 ];
 
 export default tas;
--- src/data/courses/w26/cs494/tas.js
+++ src/data/courses/w26/cs494/tas.js
@@ -1,29 +1,48 @@
 const tas = [
   {
-    name: 'TBD'
+    name: 'Beniamin Condrea',
+    email: 'condreab@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS494@OregonStateUniversity.onmicrosoft.com/bookings/',
+    // videoConferenceLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '4:30 &ndash; 6:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Wed.',
+      //   time: '10:00am &ndash; 1:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/93096057397?pwd=WjJ2SDRTdU1LWlYyMDY2VUhoZk5aUT09',
+      // },
+    ]
+  },
+  {
+    name: 'Pranav Vasist',
+    email: 'vasistp@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS494@OregonStateUniversity.onmicrosoft.com/bookings/',
+    // videoConferenceLink: '',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '4:30 &ndash; 6:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Wed.',
+      //   time: '10:00am &ndash; 1:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/93096057397?pwd=WjJ2SDRTdU1LWlYyMDY2VUhoZk5aUT09',
+      // },
+    ]
   }
-  // {
-  //   name: '',
-  //   email: '@oregonstate.edu',
-  //   appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS494@OregonStateUniversity.onmicrosoft.com/bookings/',
-  //   // videoConferenceLink: '',
-  //   officeHours: [
-  //     {
-  //       day: 'Tues.',
-  //       time: '4:30 &ndash; 6:30pm',
-  //       location: 'Zoom',
-  //       videoConferenceLink: 'https://oregonstate.zoom.us/j/92597238153?pwd=NHN1Sy9jU0d6VWhXTGNoalpYRmZVZz09',
-  //     },
-  //   ],
-  //   gradingHours: [
-  //     {
-  //       day: 'Wed.',
-  //       time: '10:00am &ndash; 1:00pm',
-  //       location: 'Zoom',
-  //       videoConferenceLink: 'https://oregonstate.zoom.us/j/93096057397?pwd=WjJ2SDRTdU1LWlYyMDY2VUhoZk5aUT09',
-  //     },
-  //   ]
-  // }
 ];
 
 export default tas;
