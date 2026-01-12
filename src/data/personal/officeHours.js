7514ca72bb7d25c1b0f456ecfc6aef635324ad6f
Rob Hess <hessro@oregonstate.edu>
2026-01-12 09:06:41 -0800

    Publish my office hours for w26.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -1,24 +1,18 @@
 const officeHours = [
-  /*
-   * Winter office hours: Wed 2:10-3:30 (KEC), Tues: 10:30-12:00 (Zoom)
-   */
   {
     hours: [
       {
-        day: 'TBD'
+        day: 'Tues.',
+        time: '10:30am&ndash;12:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Wed.',
+        time: '2:45&ndash;4:00pm',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
       }
-      // {
-      //   day: 'Wed.',
-      //   time: '11:10am&ndash;12:30pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Thurs.',
-      //   time: '2:00&ndash;3:30pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
     ]
   }
 ];
