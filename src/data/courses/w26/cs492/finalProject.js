4083f1f7e7d9d517085ceabce8afab6fb47e6924
Rob Hess <hessro@oregonstate.edu>
2026-02-06 14:00:48 -0800

    Publish CS 492 final project info.

--- src/data/courses/w26/cs492/finalProject.js
+++ src/data/courses/w26/cs492/finalProject.js
@@ -3,24 +3,24 @@ const finalProject = {
   assignments: [
     {
       title: 'Final Project Details',
-      link: 'https://docs.google.com/document/d/1USGmi6qFNl4UHrhU-IEg-e97woaiL_LuKUR_5h_juFc/edit?usp=sharing'
+      link: 'https://docs.google.com/document/d/1g4jmquZwUglUQAKP8ulnuRFaitn_4s1wLC1RZX5cwlQ/edit?usp=sharing'
     },
     {
       title: 'Deliverable #1: Final Project Proposal',
-      link: 'https://docs.google.com/document/d/1vr07ZPFye2mSFeVXdmuoB_u-LmNbDKWedz1jt3DZgo4/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1wVBb1SLEwsVpB7sxm1k3c92BR9hHnAO7uyDjaE3ql8g/edit?usp=sharing',
       due: {
         time: '11:59pm',
-        day: 'Friday, 2/23/2024',
+        day: 'Friday, 2/20/2026',
         location: 'on Canvas'
       },
       notes: [
-        'Use [this Ed thread](https://edstem.org/us/courses/51456/discussion/4338548) to help connect with potential teammates.',
-        'Submit [this form](https://forms.gle/5MvD3oTdV91cnCcy9) if you just want to be assigned to a random team.'
+        'Use [this Ed thread](https://edstem.org/us/courses/90406/discussion/7630313) to help connect with potential teammates.',
+        'Submit [this form](https://forms.gle/kM2kEubZFmgU8N8bA) if you just want to be assigned to a random team.'
       ]
     },
     {
       title: 'Deliverable #2: Code',
-      link: 'https://classroom.github.com/a/pCPqpX7x',
+      link: 'https://classroom.github.com/a/0D_bTaoG',
       due: {
         time: 'at the time of your grading demo or project presentation',
         day: 'during finals week'
@@ -31,43 +31,29 @@ const finalProject = {
     },
     {
       title: 'Deliverable #3: Final Project Team Evaluation',
-      link: 'https://docs.google.com/document/d/149t4DcQLhblPIxR72Pip6k9KEY0teazfguoNvY8oLHs/edit?usp=sharing',
+      link: 'https://docs.google.com/document/d/1pBMJvgwJv-uE0Q1uGCO_IUvv731EZM_onCWNJ-kgoA4/edit?usp=sharing',
       due: {
         time: '5:00pm',
-        day: 'Friday, 3/22/2024',
+        day: 'Friday, 3/20/2026',
         location: 'on Canvas'
       },
       notes: [
         'Note that this must be completed individually by every member of your team.'
       ]
     },
-    {
-      title: 'CS 599 Only - Final Project Presentations',
-      link: 'https://docs.google.com/document/d/1GISmn9TuZkfKhfeKNs2UD5eaHVJa_rfSDpi6X78Ufpo/edit?usp=sharing',
-      due: {
-        time: '2:00 &ndash; 3:50pm',
-        day: 'Wednesday, 3/20/2024',
-        location: 'KEC 1003'
-      },
-      notes: [
-        '**All graduate teams** must present their project during the presentation session scheduled at the time listed above.',
-        'Please see the document linked above for presentation requirements, logistics, etc.',
-        'Any undergraduates who are interested are welcome to attend the presentations.'
-      ]
-    },
-    {
-      title: 'CS 492 Only - Final Project Demo Signup',
-      link : 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/s/vrFkIux3QUqayZSedUXzbw2',
-      notes: [
-        'Sign up for a final project demo appointment using the link above.',
-        'Sign up for one demo appointment per team.  **Only undergraduate teams should sign up for a project demo.**',
-        'All team members must attend your demo unless prior arrangements have been made with Hess.',
-        'Code is due on GitHub at the time of your demo.',
-        'Come to the demo prepared to run your project from one of your team\'s computers.',
-        'Final project demos will be held in person in Hess\'s office (KEC 1109).',
-        'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please *email* at least 24 hours in advance.  **Do not rely only on requesting a remote demo by leaving a note when you sign up through Microsoft Bookings!**  These notes are buried and I won\'t see them.  Please email instead.'
-      ]
-    }
+    // {
+    //   title: 'Final Project Demo Signup',
+    //   link : 'https://outlook.office365.com/owa/calendar/CS492@OregonStateUniversity.onmicrosoft.com/bookings/s/vrFkIux3QUqayZSedUXzbw2',
+    //   notes: [
+    //     'Sign up for a final project demo appointment using the link above.',
+    //     'Sign up for one demo appointment per team.  **Only undergraduate teams should sign up for a project demo.**',
+    //     'All team members must attend your demo unless prior arrangements have been made with Hess.',
+    //     'Code is due on GitHub at the time of your demo.',
+    //     'Come to the demo prepared to run your project from one of your team\'s computers.',
+    //     'Final project demos will be held in person in Hess\'s office (KEC 1109).',
+    //     'Remote attendance at a final project demo is possible *by request only*.  If you\'d like to attend your demo remotely via Zoom, please *email* at least 24 hours in advance.  **Do not rely only on requesting a remote demo by leaving a note when you sign up through Microsoft Bookings!**  These notes are buried and I won\'t see them.  Please email instead.'
+    //   ]
+    // }
   ],
 
   preamble: 'For the final project in this course, you will work in teams to implement a complete Android app that gets data from a third-party API.  The project will have three deliverables: a proposal, where you outline the app you\'d like to write; an implementation of your app; and a brief evaluation of your project team.  Follow the links below to find out more about each of these deliverables.'
