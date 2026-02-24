b57eb5421255981f941cfb99ed3860a67e984e6d
Rob Hess <hessro@oregonstate.edu>
2026-02-24 13:26:00 -0800

    Update CS 494 topics.

--- src/data/courses/w26/cs494/topics.js
+++ src/data/courses/w26/cs494/topics.js
@@ -273,7 +273,6 @@ const topics = [
   {
     title: 'Centralizing Application State with Redux',
     weeks: [ 7 ],
-    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
@@ -384,17 +383,18 @@ const topics = [
 
   {
     title: 'API Queries with GraphQL',
-    weeks: [ 9 ],
+    weeks: [ 8 ],
+    isCurrent: true,
     resources: [
       {
         title: 'Lecture notes',
         link: 'https://docs.google.com/document/d/1i02SdUAS96QYayvf4KtDa0HmEwZNOLZZb7jZBfbWWl8/edit?usp=sharing'
       },
-      // {
-      //   title: 'Example code',
-      //   link: 'https://github.com/osu-cs494-w24/graphql-with-apollo',
-      //   description: 'GitHub repo for code written in lecture'
-      // },
+      {
+        title: 'Example code',
+        link: 'https://github.com/osu-cs494-w26/graphql-with-apollo',
+        description: 'GitHub repo for code written in lecture'
+      },
     ],
     readings: [
       {
