f33d0a58a1d8c0e896c0feafdd5eec70c8dc0dcf
Rob Hess <hessro@oregonstate.edu>
2026-03-13 09:19:44 -0700

    Add TAs to CS 362.

--- src/data/courses/sp26/cs362/tas.js
+++ src/data/courses/sp26/cs362/tas.js
@@ -22,8 +22,52 @@ const taInfo = [
     ]
   },
   {
+    name: 'Logan Bachman',
+    email: 'bachmanl@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
+    // videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '2:30 &ndash; 3:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Wed.',
+      //   time: '6:00 &ndash; 8:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      // },
+    ]
+  },
+  {
     name: 'Stanley Eng',
     email: 'engst@oregonstate.edu',
+    // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
+    // videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09',
+    officeHours: [
+      // {
+      //   day: 'Tues.',
+      //   time: '2:30 &ndash; 3:30pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      // },
+    ],
+    gradingHours: [
+      // {
+      //   day: 'Wed.',
+      //   time: '6:00 &ndash; 8:00pm',
+      //   location: 'Zoom',
+      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09'
+      // },
+    ]
+  },
+  {
+    name: 'Anthony Silva',
+    email: 'silvanth@oregonstate.edu',
     // appointmentsLink: 'https://outlook.office365.com/owa/calendar/CS362@OregonStateUniversity.onmicrosoft.com/bookings/',
     // videoConferenceLink: 'https://oregonstate.zoom.us/j/8746802238?pwd=MzdyYWpkYlFTZmxiQW5PbjQ5Slk0dz09',
     officeHours: [
