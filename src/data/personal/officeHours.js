35f83ee3110fbf57eff39115f0e70227dea98d20
Rob Hess <hessro@oregonstate.edu>
2026-04-03 10:26:29 -0700

    Post my own office hours.

--- src/data/personal/officeHours.js
+++ src/data/personal/officeHours.js
@@ -1,19 +1,18 @@
 const officeHours = [
   {
     hours: [
-      { day: "TBD" }
-      // {
-      //   day: 'Tues.',
-      //   time: '10:30am&ndash;12:00pm',
-      //   location: 'Zoom',
-      //   videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // },
-      // {
-      //   day: 'Wed.',
-      //   time: '2:45&ndash;4:00pm',
-      //   location: 'KEC 1109',
-      //   // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
-      // }
+      {
+        day: 'Tues.',
+        time: '1:30 &ndash; 3:00pm',
+        location: 'Zoom',
+        videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      },
+      {
+        day: 'Wed.',
+        time: '2:15 &ndash; 3:30pm',
+        location: 'KEC 1109',
+        // videoConferenceLink: 'https://oregonstate.zoom.us/j/589990808?pwd=ViszY2Z4eGtoUDh4NEw0QWkzckJMQT09'
+      }
     ]
   }
 ];
