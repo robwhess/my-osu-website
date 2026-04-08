6dd35eeb961e8a82c6f20011e9232e47dbb30645
Rob Hess <hessro@oregonstate.edu>
2026-04-08 15:00:41 -0700

    Update CS 362 and CS 493 topics.

--- src/data/courses/sp26/cs362/topics.js
+++ src/data/courses/sp26/cs362/topics.js
@@ -20,7 +20,6 @@ const topics = [
     {
         title: 'Git and GitHub: An Individual\'s Perspective',
         weeks: [ 1, 2 ],
-        isCurrent: true,
         resources: [
             {
                 title: 'Lecture notes',
