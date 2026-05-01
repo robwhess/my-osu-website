7d754f1ba7184f8e7e10433edce3e09ae2d27baa
Rob Hess <hessro@oregonstate.edu>
2026-05-01 15:32:34 -0700

    Update base URL handling for Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import pkg from './package.json'
+
 // https://vite.dev/config/
 export default defineConfig({
+  /*
+   * Figure out base pathname based on "homepage" property in package.json.
+   */
+  base: pkg.homepage ? new URL(pkg.homepage).pathname : undefined,
   plugins: [react({
     jsxImportSource: "@emotion/react"
   })],
