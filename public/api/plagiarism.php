ee2273e0a8b53d2439b9a4547156ac1ae2000281
Rob Hess <hessro@oregonstate.edu>
2026-05-01 18:29:25 -0700

    Add "plagiarism" API endpoint (in PHP!).

---
+++ public/api/plagiarism.php
@@ -0,0 +1,42 @@
+<?php
+    /*
+     * This is a simple "API endpoint" that simulates the computation of a
+     * plagiarism score on a submitted
+     */
+
+    header('Content-Type: application/json');
+
+    /*
+     * Only accept POST requests
+     */
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    	http_response_code(405);
+    	die(json_encode(['err' => 'Only POST requests are accepted']));
+    }
+
+    /*
+     * Make sure file was uploaded.
+     */
+    if (empty($_FILES) || !isset($_FILES['submission'])) {
+        http_response_code(400);
+	    die(json_encode(['err' => '`submission` file not specified']));
+    }
+
+    /*
+     * Make sure uploaded file is not empty.
+     */
+    if ($_FILES['submission']['size'] <= 0) {
+        http_response_code(400);
+	    die(json_encode(['err' => '`submission` file is empty']));
+    }
+
+    /*
+     * Compute random plagiarism score between 1 and 100 and random delay
+     * between 1 and 5 seconds.
+     */
+    $score = rand(0, 100);
+    $delay = rand(1, 5);
+    sleep($delay);
+    http_response_code(200);
+    die(json_encode(['score' => $score]));
+?>
