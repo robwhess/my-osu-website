d993d5ac7e6327441c56e5c15e27140ed7ebba04
Rob Hess <hessro@oregonstate.edu>
2026-07-12 14:28:42 -0400

    Generate needed id's for Markdown TOC.

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -1,6 +1,7 @@
 import Markdown from "react-markdown"
 import remarkGfm from "remark-gfm"
 import remarkToc from "remark-toc"
+import rehypeSlug from "rehype-slug"
 
 export default function MarkdownRenderer({
     markdown
@@ -9,7 +10,10 @@ export default function MarkdownRenderer({
 }) {
     return (
         <div className="prose prose-lead:leading-normal prose-li:marker:text-base-content">
-            <Markdown remarkPlugins={[ remarkGfm, remarkToc ]}>
+            <Markdown
+                remarkPlugins={[ remarkGfm, remarkToc ]}
+                rehypePlugins={[ rehypeSlug ]}
+            >
                 {markdown}
             </Markdown>
         </div>
