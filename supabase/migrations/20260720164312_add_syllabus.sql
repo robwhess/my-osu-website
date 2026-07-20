3ed61937dbdfa2777ee2448c257ae0db965fddca
Rob Hess <hessro@oregonstate.edu>
2026-07-20 09:50:43 -0700

    Add migration for syllabus and update DB seed data.

---
+++ supabase/migrations/20260720164312_add_syllabus.sql
@@ -0,0 +1,4 @@
+alter table "public"."course_term" add column "syllabus" text;
+
+
+
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict LeSFxYSvbXpLeALEHiCcIJmthbd6vcBFQkaDz3gjIu3kM6peMwtyDQoXddbyzRb
+-- \restrict av4FRIDT9XUBw4p1FIj7Dnq9S18c9hs9zTrIdYmfJyAhnCcLFwZqfPeg810yZsf
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -22,161 +22,17 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Data for Name: audit_log_entries; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: flow_state; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: identities; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: instances; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: oauth_clients; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: sessions; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: mfa_amr_claims; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: mfa_factors; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: mfa_challenges; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: oauth_authorizations; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: oauth_client_states; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: oauth_consents; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: one_time_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: refresh_tokens; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: sso_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: saml_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: saml_relay_states; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: sso_domains; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: webauthn_challenges; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
--- Data for Name: webauthn_credentials; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
---
-
-
-
---
 -- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO "public"."course" ("id", "number", "title", "terms", "description", "created_at") VALUES
-	('cs362', 'CS 362', 'Software Engineering II', '{spring}', 'Processes and techniques for maintaining high quality software', '2024-10-15 22:36:28.898808+00'),
-	('cs480', 'CS 480', 'Translators', '{spring}', 'How to build a modern compiler', '2024-10-15 22:37:08.595432+00'),
-	('cs494', 'CS 494', 'Advanced Web Development', '{winter}', 'Modern tools and techniques for developing web clients', '2024-10-15 22:39:10.871521+00'),
-	('cs261', 'CS 261', 'Data Structures', '{fall}', 'Techniques for storing and organizing data in a program', '2024-10-15 22:33:20.527808+00'),
-	('cs290', 'CS 290', 'Web Development', '{fall}', 'Fundamentals of full-stack web app development', '2024-10-15 22:35:40.088099+00'),
-	('cs492', 'CS 492', 'Mobile Software Development', '{winter}', 'Development of native mobile clients', '2024-10-15 22:37:49.346802+00'),
-	('cs493', 'CS 493', 'Cloud Application Development', '{spring}', 'Tools and techniques for building an HTTP API', '2024-10-15 22:38:32.614224+00');
-
-
---
--- Data for Name: course_term; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO "public"."course_term" ("id", "created_at", "course_id", "term", "year") VALUES
-	('cs290-sp22', '2024-11-04 20:55:04.991756+00', 'cs290', 'spring', 2022),
-	('cs290-f22', '2024-11-04 20:54:35.563564+00', 'cs290', 'fall', 2022),
-	('cs290-f23', '2024-11-04 20:52:09.04387+00', 'cs290', 'fall', 2023),
-	('cs290-f24', '2024-10-30 20:28:35.075182+00', 'cs290', 'fall', 2024),
-	('cs493-sp26', '2026-06-01 21:08:26.390458+00', 'cs493', 'spring', 2026);
+INSERT INTO "public"."course" ("id", "number", "title", "description", "created_at", "terms") VALUES
+	('cs261', 'CS 261', 'Data Structures', 'Techniques for storing and organizing data in a program', '2024-10-15 22:33:20.527808+00', '{fall}'),
+	('cs290', 'CS 290', 'Web Development', 'Fundamentals of full-stack web app development', '2024-10-15 22:35:40.088099+00', '{fall}'),
+	('cs362', 'CS 362', 'Software Engineering II', 'Processes and techniques for maintaining high quality software', '2024-10-15 22:36:28.898808+00', '{spring}'),
+	('cs480', 'CS 480', 'Translators', 'How to build a modern compiler', '2024-10-15 22:37:08.595432+00', '{}'),
+	('cs492', 'CS 492', 'Mobile Software Development', 'Development of native mobile clients', '2024-10-15 22:37:49.346802+00', '{winter}'),
+	('cs493', 'CS 493', 'Cloud Application Development', 'Tools and techniques for building an HTTP API', '2024-10-15 22:38:32.614224+00', '{spring}'),
+	('cs494', 'CS 494', 'Advanced Web Development', 'Modern tools and techniques for developing web clients', '2024-10-15 22:39:10.871521+00', '{winter}');
 
 
 --
@@ -185,6 +41,15 @@ INSERT INTO "public"."course_term" ("id", "created_at", "course_id", "term", "ye
 
 INSERT INTO "public"."person" ("id", "created_at", "name", "email") VALUES
 	(1, '2024-10-15 22:26:40.078632+00', 'Rob Hess', 'hessro@oregonstate.edu');
+
+
+--
+-- Data for Name: course_term; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO "public"."course_term" ("id", "created_at", "course_id", "year", "term", "instructor_id", "links", "syllabus") VALUES
+	('cs362-sp26', '2026-07-01 02:49:33.625699+00', 'cs362', 2026, 'spring', 1, '{"{\"url\": \"https://outlook.office.com/book/CS362@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled\", \"title\": \"CS 362 grading demo appointments\", \"description\": \"Use this page to sign up for grading demos for all assignments.\"}","{\"url\": \"https://canvas.oregonstate.edu/courses/2039368\", \"title\": \"CS 362 on Canvas\"}","{\"url\": \"https://oregonstate.zoom.us/j/99350551124?pwd=dHqez0vKdPnABmJ7aMl4dNawhgtf7f.1\", \"title\": \"CS 362 on Zoom\", \"description\": \"All in-person lectures will be broadcast in this Zoom meeting.\"}","{\"url\": \"https://edstem.org/us/courses/96641\", \"title\": \"CS 362 on Ed\", \"description\": \"We will use Ed for course Q & A – you should be automatically enrolled if you''re enrolled for this course.\"}","{\"url\": \"https://github.com/osu-cs362-sp26\", \"title\": \"CS 362 on GitHub\", \"description\": \"Find all lecture code repos and your own private assignment repos here.\"}"}', 'cs362/cs362-sp26.md'),
+	('cs493-sp26', '2026-06-01 21:08:26.390458+00', 'cs493', 2026, 'spring', 1, '{"{\"url\": \"https://outlook.office.com/book/CS493@OregonStateUniversity.onmicrosoft.com/?ismsaljsauthenabled\", \"title\": \"CS 493 grading demo appointments\", \"description\": \"Use this page to sign up for grading demos for all assignments.\"}","{\"url\": \"https://canvas.oregonstate.edu/courses/2066268\", \"title\": \"CS 493 on Canvas\"}","{\"url\": \"https://oregonstate.zoom.us/j/96017875949?pwd=RPj7neChAcucSbjAvp1wGdquaynQUk.1\", \"title\": \"CS 493 on Zoom\", \"description\": \"All in-person lectures will be broadcast in this Zoom meeting.\"}","{\"url\": \"https://edstem.org/us/courses/96642\", \"title\": \"CS 493 on Ed\", \"description\": \"We will use Ed for course Q & A – you should be automatically enrolled if you''re enrolled for this course.\"}","{\"url\": \"https://github.com/osu-cs493-sp26\", \"title\": \"CS 493 on GitHub\", \"description\": \"Find all lecture code repos and your own private assignment repos here.\"}"}', 'cs493/cs493-sp26.md');
 
 
 --
@@ -197,52 +62,20 @@ INSERT INTO "public"."hours" ("id", "created_at", "day", "start", "end", "locati
 
 
 --
--- Data for Name: buckets; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+-- Data for Name: lecture; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-
-
---
--- Data for Name: buckets_analytics; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
+INSERT INTO "public"."lecture" ("id", "created_at", "section", "day", "start", "end", "location", "videoconference_link", "extra_info", "location_link", "course_term_id", "crn") VALUES
+	(2, '2026-07-01 02:51:48.924317+00', '001', 'Mon, Wed', '16:00:00', '17:50:00', 'LPSC 125', 'https://oregonstate.zoom.us/j/99350551124?pwd=dHqez0vKdPnABmJ7aMl4dNawhgtf7f.1', NULL, 'https://map.oregonstate.edu/?id=2243#!m/1027734?share', 'cs362-sp26', 52342),
+	(1, '2026-06-30 20:01:07.080816+00', '001', 'Mon, Wed', '12:00:00', '13:50:00', 'ROG 230', 'https://oregonstate.zoom.us/j/96017875949?pwd=RPj7neChAcucSbjAvp1wGdquaynQUk.1', NULL, 'https://map.oregonstate.edu/?id=2243#!m/1027606?share', 'cs493-sp26', 61444);
 
 
 --
--- Data for Name: buckets_vectors; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
+-- Data for Name: textbook; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-
-
---
--- Data for Name: objects; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: s3_multipart_uploads; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: s3_multipart_uploads_parts; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Data for Name: vector_indexes; Type: TABLE DATA; Schema: storage; Owner: supabase_storage_admin
---
-
-
-
---
--- Name: refresh_tokens_id_seq; Type: SEQUENCE SET; Schema: auth; Owner: supabase_auth_admin
---
-
-SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
+INSERT INTO "public"."textbook" ("id", "created_at", "title", "author", "link", "description", "course_term_id") VALUES
+	(1, '2026-07-04 15:36:49.372502+00', 'No textbook for this course', NULL, NULL, 'Readings will come from a curated collection of articles and documentation from the internet.', 'cs493-sp26');
 
 
 --
@@ -253,6 +86,13 @@ SELECT pg_catalog.setval('"public"."hours_id_seq"', 2, true);
 
 
 --
+-- Name: lecture_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."lecture_id_seq"', 2, true);
+
+
+--
 -- Name: person_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -260,9 +100,16 @@ SELECT pg_catalog.setval('"public"."person_id_seq"', 1, true);
 
 
 --
+-- Name: textbook_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('"public"."textbook_id_seq"', 1, true);
+
+
+--
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict LeSFxYSvbXpLeALEHiCcIJmthbd6vcBFQkaDz3gjIu3kM6peMwtyDQoXddbyzRb
+-- \unrestrict av4FRIDT9XUBw4p1FIj7Dnq9S18c9hs9zTrIdYmfJyAhnCcLFwZqfPeg810yZsf
 
 RESET ALL;
