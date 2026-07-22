3911b818e0e7566dfc8e062c75593e420449a8bc
Rob Hess <hessro@oregonstate.edu>
2026-07-22 16:57:11 -0700

    Add TAs page.

---
+++ app/courses/[course]/[term]/tas/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { use } from "react"
+import { useQuery } from "@supabase-cache-helpers/postgrest-swr"
+
+import { createClient } from "@/lib/supabase/browser"
+import EventCard from "@/components/EventCard"
+
+export default function TAsPage({
+    params
+}: Readonly<{
+    params: Promise<{ course: string, term: string }>
+}>) {
+    const { course, term } = use(params)
+    const courseTermId = `${course}-${term}`
+    const supabase = createClient()
+    const { data, isLoading } = useQuery(
+        supabase
+            .from("person")
+            .select("*,hours!inner(*,videoConferenceLink:videoconference_link,extraInfo:extra_info)")
+            .eq("hours.course_term_id", courseTermId)
+    )
+
+    return (
+        <div className="flex flex-col">
+            <h3 className="text-lg font-medium">TAs</h3>
+            <ul>
+                {isLoading && (
+                    <li className="md:px-4 pt-6">
+                        <div className="skeleton h-8 w-40 my-2"></div>
+                        <div className="skeleton h-4 w-48 my-2"></div>
+                    </li>
+                )}
+                {data?.map(person => (
+                    <li key={person.id} className="flex flex-col md:flex-row gap-4 md:px-4 py-12 first:pt-6 not-first:border-t border-base-300">
+                        <div className="flex-2">
+                            <h4 className="text-lg font-medium text-gray-500">{person.name}</h4>
+                            <p className="text-sm"><a className="link" href={`mailto:${person.email}`}>{person.email}</a></p>
+                        </div>
+                        <div className="flex-3">
+                            <h4 className="mb-1 md:pl-1 font-medium uppercase text-gray-500">Office Hours</h4>
+                            {person.hours.filter(hours => hours.type === "office").map(hours => (
+                                <div key={hours.id} className="mb-1 ml-4 md:ml-0">
+                                    <EventCard tight small {...hours} />
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex-3">
+                            <h4 className="mb-1 md:pl-1 font-medium uppercase text-gray-500">Grading Hours</h4>
+                            {person.hours.filter(hours => hours.type === "grading").map(hours => (
+                                <div key={hours.id} className="mb-1 ml-4 md:ml-0">
+                                    <EventCard tight small {...hours} />
+                                </div>
+                            ))}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
