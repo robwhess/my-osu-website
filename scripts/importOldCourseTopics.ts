54ee448a82d9e1ae182ef3c479df6e5672a3ff5b
Rob Hess <hessro@oregonstate.edu>
2026-07-31 13:13:20 -0700

    Add script for importing course topics from old website.

---
+++ scripts/importOldCourseTopics.ts
@@ -0,0 +1,59 @@
+/**
+ * Import course topics from old course website.  Usage:
+ *
+ * node importOldCourseTopics.ts <course> <term>
+ *
+ * e.g. node importOldCourseTopics.ts cs493 sp26
+ */
+
+import path from "node:path"
+import dotenv from "dotenv"
+import { createClient } from "@supabase/supabase-js"
+
+import { type Database, type Json } from "../generated/supabase/types.ts"
+
+dotenv.config({ path: ".env.local" })
+
+interface resource {
+    title: string
+    link?: string
+    description?: string
+}
+
+interface topic {
+    title: string
+    weeks: number[]
+    resources: resource[]
+    readings?: resource[]
+}
+
+const COURSE_DATA_DIR = "/Users/hessro/my-osu-website/src/data/courses"
+const term = process.argv[3]
+const course = process.argv[2]
+
+const topicsFile = path.join(COURSE_DATA_DIR, term, course, "topics.js")
+
+const { default: topics }: { default: topic[] } = await import(topicsFile)
+
+const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SECRET_KEY!
+)
+
+topics.forEach(async topic => {
+    const { error } = await supabase
+        .from("topic")
+        .upsert({
+            title: topic.title,
+            weeks: topic.weeks,
+            materials: topic.resources as unknown as Json[],
+            readings: (topic.readings || []) as unknown as Json[],
+            course_term_id: `${course}-${term}`
+        })
+        .select()
+    if (error) {
+        console.error(`== Error inserting ${topic.title}:`, error)
+    } else  {
+        console.log("== Successfully imported", topic.title)
+    }
+})
