f4ab1bc6db6ed9184797a6f2626c219fb06c60cb
Rob Hess <hessro@oregonstate.edu>
2026-08-16 11:54:04 -0700

    Add jump links to assignments page.

--- app/courses/[course]/[term]/assignments/page.tsx
+++ app/courses/[course]/[term]/assignments/page.tsx
@@ -3,6 +3,8 @@
 import { use } from "react"
 import { useQuery } from "@supabase-cache-helpers/postgrest-swr"
 import { MdErrorOutline, MdOutlineEditOff } from "react-icons/md"
+import slugify from "slugify"
+import Link from "next/link"
 
 import { createClient } from "@/lib/supabase/browser"
 
@@ -26,6 +28,11 @@ export default function AssignmentsPage({
             .order("created_at", { referencedTable: "assignment", ascending: true })
     )
 
+    const slugs = data?.map(group => ({
+        title: group.title,
+        slug: slugify(group.title, { lower: true })
+    })) || []
+
     return (
         <div className="flex flex-col">
             <h3 className="text-lg font-medium">Assignments</h3>
@@ -37,8 +44,10 @@ export default function AssignmentsPage({
             )}
             {isLoading && (
                 <div className="m-4">
-                    <div className="skeleton h-2 w-24 my-2"></div>
-                    <div className="skeleton h-6 w-72 my-2"></div>
+                    <div className="skeleton h-6 w-64 mt-6"></div>
+                    <div className="skeleton h-2 w-84 my-2"></div>
+                    <div className="skeleton h-2 w-72 my-2"></div>
+                    <div className="skeleton h-2 w-80 my-2"></div>
                 </div>
             )}
             {!error && !isLoading && !data?.length && (
@@ -47,13 +56,30 @@ export default function AssignmentsPage({
                     <span>No assignments available.</span>
                 </div>
             )}
-            {data?.map(group => (
+            {slugs.length > 1 && (
+                <nav className="pt-6">
+                    <h5 className="font-semibold">Jump to...</h5>
+                    <ul className="list-disc list-outside text-sm pl-6">
+                        {slugs.map(slug => (
+                            <li key={slug.slug} className="list-item">
+                                <Link className="link" href={`#${slug.slug}`}>{slug.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            )}
+            {data?.map((group, idx) => (
                 <div
                     key={group.id}
-                    className="md:px-4 py-12 first:pt-6 not-first-of-type:border-t border-base-300"
+                    className="md:px-4 py-12 first-of-type:pt-6 not-last-of-type:border-b border-base-300"
                 >
                     <div className="flex-2">
-                        <h4 className="text-lg font-medium uppercase text-gray-700">{group.title}</h4>
+                        <h4
+                            id={slugs[idx].slug}
+                            className="text-lg font-medium uppercase text-gray-700"
+                        >
+                            {group.title}
+                        </h4>
                         {group.description && (
                             <MarkdownRenderer small markdown={group.description} />
                         )}
